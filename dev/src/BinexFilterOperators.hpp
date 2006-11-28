67e40105d3ae016c22e3ca6b861d291eb36fe464
Ben Harris <pben@whoknows.com>
2006-11-28 22:06:24 +0000

    Forget to add the actual library code in the last submission.

---
+++ dev/src/BinexFilterOperators.hpp
@@ -0,0 +1,41 @@
+#pragma ident "$Id: //depot/msn/main/code/shared/gpstk/BinexFilterOperators.hpp#1 $"
+
+/**
+ * @file BinexFilterOperators.hpp
+ * Operators for FileFilter using Binex data
+ */
+
+#ifndef GPSTK_BINEXFILTEROPERATORS_HPP
+#define GPSTK_BINEXFILTEROPERATORS_HPP
+
+#include "DayTime.hpp"
+#include "FileFilter.hpp"
+#include "BinexData.hpp"
+
+#include <set>
+
+namespace gpstk
+{
+   /** @addtogroup Binex */
+   //@{
+
+   typedef std::binary_function<BinexData, BinexData, bool> BinexDataBinaryOperator;
+
+
+      /// Determine if two BinexData objects are equal.
+   struct BinexDataOperatorEquals : 
+      public BinexDataBinaryOperator
+   {
+   public:
+      bool operator()(const BinexData& l,
+                      const BinexData& r) const
+         {
+            return (l == r);
+         }
+   };
+
+   //@}
+
+}
+
+#endif // GPSTK_BINEXFILTEROPERATORS_HPP
---
+++ dev/src/BinexStream.hpp
@@ -0,0 +1,55 @@
+#pragma ident "$Id: //depot/msn/main/code/shared/gpstk/BinexStream.hpp#1 $"
+
+/**
+ * @file BinexStream.hpp
+ * File stream for RINEX meterological files
+ */
+
+#ifndef GPSTK_BINEXSTREAM_HPP
+#define GPSTK_BINEXSTREAM_HPP
+
+#include "FFBinaryStream.hpp"
+
+namespace gpstk
+{
+   /** @addtogroup Binex */
+   //@{
+
+      /**
+       * This class performs file i/o on a BINEX file for the 
+       * BinexData classes.
+       *
+       * @sa binex_read_write.cpp for an example.
+       * @sa binex_test.cpp for an example.
+       * @sa BinexData.
+       *
+       */
+   class BinexStream : public FFBinaryStream
+   {
+   public:
+         /// Destructor
+      virtual ~BinexStream() {}
+      
+         /// Default constructor
+      BinexStream() {}
+      
+         /** Constructor 
+          * Opens a file named \a fn using ios::openmode \a mode.
+          */
+      BinexStream(const char* fn,
+                  std::ios::openmode mode=std::ios::in | std::ios::binary)
+            : FFBinaryStream(fn, mode) {};
+
+         /// Opens a file named \a fn using ios::openmode \a mode.
+      virtual void
+      open(const char* fn, std::ios::openmode mode)
+      { 
+         FFBinaryStream::open(fn, mode); 
+      }
+   };
+
+   //@}
+
+} // namespace gpstk
+
+#endif // GPSTK_BINEXSTREAM_HPP
