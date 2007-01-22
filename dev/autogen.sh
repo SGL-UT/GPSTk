2d712d343628bdf4d8b5fe34fe099cca3f8e3918
Martin Vermeer <martin.vermeer@aalto.fi>
2007-01-22 11:15:17 +0000

    Chris Kuethe: makefile fixes for make install

--- dev/autogen.sh
+++ dev/autogen.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+export AUTOCONF_VERSION=2.60
+export AUTOMAKE_VERSION=1.9
+
 autoreconf
 
+exit
+env LDFLAGS="-lstdc++ -lm" CFLAGS="-O0 -fPIC" CXXFLAGS="-O0 -fPIC" ./configure --prefix=/foo/gpstk
