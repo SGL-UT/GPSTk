a8211b535cd98d4c141b73d82ae0cc53a0ac45e2
Tracie Conn <tconn@whoknows.com>
2007-03-20 15:48:00 +0000

    Correcting small typo in options.

--- dev/apps/reszilla/ordEdit.cpp
+++ dev/apps/reszilla/ordEdit.cpp
@@ -78,7 +78,7 @@ OrdEdit::OrdEdit() throw()
    : OrdApp("ordEdit", "Edits an ord file based on various criteria."),
      elMask(0),clkResidLimit(0),
      ephSourceOpt('e',"be-file","Remove data for unhealthy SVs by "
-                  "providing broadcast ephemeris source: RINEX nav or"
+                  "providing broadcast ephemeris source: RINEX nav or "
                   "FIC file." ),           
      elvOpt('m',"elev","Remove data for SVs below a given elevation mask."),
      clkOpt('k',"clock-est", "Remove ords that do not have corresponding "
