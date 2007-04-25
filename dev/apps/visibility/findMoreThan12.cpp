19c65df815a34afea80fcbbfaea80fd1d48dc233
Tracie Conn <tconn@whoknows.com>
2007-04-25 17:52:04 +0000

    Now uses EphReader - much faster.

--- dev/apps/visibility/findMoreThan12.cpp
+++ dev/apps/visibility/findMoreThan12.cpp
@@ -1,18 +1,46 @@
+//============================================================================
+//
+//  This file is part of GPSTk, the GPS Toolkit.
+//
+//  The GPSTk is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published
+//  by the Free Software Foundation; either version 2.1 of the License, or
+//  any later version.
+//
+//  The GPSTk is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with GPSTk; if not, write to the Free Software Foundation,
+//  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  
+//  Copyright 2004, The University of Texas at Austin
+//
+//============================================================================
+
+//============================================================================
+//
+//This software developed by Applied Research Laboratories at the University of
+//Texas at Austin, under contract to an agency or agencies within the U.S. 
+//Department of Defense. The U.S. Government retains all rights to use,
+//duplicate, distribute, disclose, or release this software. 
+//
+//Pursuant to DoD Directive 523024 
+//
+// DISTRIBUTION STATEMENT A: This software has been approved for public 
+//                           release, distribution is unlimited.
+//
+//=============================================================================
+
 #include <iostream>
 #include <iomanip>
 
 #include "DayTime.hpp"
 #include "CommandOption.hpp"
 #include "CommandOptionParser.hpp"
-#include "WGS84Geoid.hpp"
-#include "SP3EphemerisStore.hpp"
-#include "RinexObsBase.hpp"
-#include "RinexObsData.hpp"
-#include "RinexObsHeader.hpp"
-#include "RinexObsStream.hpp"
-#include "RinexNavData.hpp"
-#include "RinexEphemerisStore.hpp"
-#include "BCEphemerisStore.hpp"
+#include "EphReader.hpp"
 
 using namespace std;
 using namespace gpstk;
@@ -24,36 +52,33 @@ int main(int argc, char *argv[])
     verboseOption('v',"verbose","Increase verbosity.");
 
   CommandOptionWithAnyArg 
-    apcFile('a',"apc-file","Precise ephemeris file in SP3 format. User may enter multiple files."),
-    beFile('b',"be-file","RINEX navigation file. User may enter multiple files."),
+    ephFiles('e',"eph-files","Ephemeris source file(s). Can be RINEX nav, SP3, or FIC.",true),
     antennaPosition('p',"position","Antenna position in ECEF (x,y,z) coordinates.  Format as a string: \"X Y Z\".",true);
     
   CommandOptionWithNumberArg 
-    minElev('e',"elev","Give an integer for the elevation (degrees) above which you want to find more than 12 SVs at a given time.", true);     
+    minElev('m',"min-elev","Give an integer for the elevation (degrees) above which you want to find more than 12 SVs at a given time.", true);     
       
   CommandOptionParser cop("Finds when there are simultaneously more than 12 SVs above a given elevation. Although the -a and -b arguments are listed as optional below, one must be specified. An ephemeris source must be given.");
     cop.parseOptions(argc, argv);
+    
   // verify that the correct input was given
-  if (helpOption.getCount() || cop.hasErrors())
+  if (helpOption.getCount())
   {
-    if (cop.hasErrors())
+    cop.displayUsage(cout);
+    exit(0);
+  }
+  
+  if (cop.hasErrors())
+  {
     cop.dumpErrors(cout);
     cop.displayUsage(cout);
     exit(0);
   }
-  if ( (apcFile.getCount())&&(beFile.getCount()) )
-  {
-    cout << "Enter a precise OR broadcast ephemeris source, but not both." << endl;
-    exit(0);
-  }
-  if ( (apcFile.getCount()==0)&&(beFile.getCount()==0) )
-  {
-    cout << "Please enter an ephemeris source file." << endl;
-    exit(0);
-  }
+
   // output intermediate info?
   bool verbose = false;
   if (verboseOption.getCount()) { verbose = true; }
+  
   // get the minimum elevation
   int minEl = gpstk::StringUtils::asInt((minElev.getValue())[0]);
   if (minEl < 0 )
@@ -61,54 +86,61 @@ int main(int argc, char *argv[])
     cout << "Please enter a positive elevation. " << endl;
     exit(0);
   }
+  
   // get the ephemeris source(s)
-  SP3EphemerisStore peEphList;
-  RinexEphemerisStore beEphList;
-  if (apcFile.getCount()) {peEphList.loadFiles(apcFile.getValue());}
-  if (beFile.getCount()) {beEphList.loadFiles(beFile.getValue());}
+  EphReader ephReader;
+  ephReader.verboseLevel = verbose;
+  for (int i=0; i<ephFiles.getCount(); i++)
+      ephReader.read(ephFiles.getValue()[i]);
+  
+  gpstk::EphemerisStore& ephStore = *ephReader.eph;
+  
   // get the antenna position
   Xvt antPVT;
   double x,y,z;
   sscanf(antennaPosition.getValue().front().c_str(),"%lf %lf %lf", &x, &y, &z);
   antPVT.x[0] = x; antPVT.x[1] = y; antPVT.x[2] = z;
   ECEF antPos = Triple(antPVT.x);
-  Xvt peXVT;
+  
   // get initial and final times for analysis. extra code b/c sscanf reads in int's but setYMDHMS needs shorts
   short year, month, day, hour, minute;
   double seconds;
-  DayTime tStartDT, tEndDT, tstart, tend, t;
-  if (apcFile.getCount()) {tStartDT = peEphList.getInitialTime(); }
-  if (beFile.getCount()) {tStartDT = beEphList.getInitialTime(); }
+
+  DayTime tStartDT = ephStore.getInitialTime();
   year = tStartDT.year();
   month = tStartDT.month();
   day = tStartDT.day();
   hour = tStartDT.hour();
   minute = tStartDT.minute();
   seconds = tStartDT.second();
+  DayTime tstart;
   tstart.setYMDHMS(year,month,day,hour,minute,seconds);
-  if (apcFile.getCount()) {tEndDT = peEphList.getFinalTime(); }
-  if (beFile.getCount()) {tEndDT = beEphList.getFinalTime(); }
+  
+  DayTime tEndDT = ephStore.getFinalTime();
   year = tEndDT.year();
   month = tEndDT.month();
   day = tEndDT.day();
   hour = tEndDT.hour();
   minute = tEndDT.minute();
   seconds = tEndDT.second();
+  DayTime tend;
   tend.setYMDHMS(year,month,day,hour,minute,seconds);
-  // start at the first ephemeris time and crank through. evaluate every minute
-  t = tstart;
+  
+  DayTime t = tstart;
+
   cout << "Start Time: " << tstart << " End Time: " << tend << endl;
+  
   while (t < tend)
   {
     short numSVsAboveElv = 0;
     short prn = 1;
-    while (prn < 33)
+    while (prn <= gpstk::MAX_PRN)
     {
+      Xvt peXVT;
       bool NoEph = false;
-      try 
+      try
       {
-        if (apcFile.getCount()) {peXVT = peEphList.getPrnXvt(prn,t);}
-        if (beFile.getCount()) {peXVT = beEphList.getPrnXvt(prn,t);}
+        peXVT = ephStore.getPrnXvt(prn,t);
       }
       catch(gpstk::Exception& e) 
       {
@@ -128,13 +160,13 @@ int main(int argc, char *argv[])
     { 
       cout << "Found " << numSVsAboveElv << " SVs above " << minEl << " degrees at " << t << endl;
       prn = 1;
-      while (prn < 33)
+      while (prn < gpstk::MAX_PRN)
       {
         bool NoEph = false;
+        Xvt peXVT;
         try 
         {
-          if (apcFile.getCount()) {peXVT = peEphList.getPrnXvt(prn,t);}
-          if (beFile.getCount()) {peXVT = beEphList.getPrnXvt(prn,t);}
+          peXVT = ephStore.getPrnXvt(prn,t);
         }
         catch(gpstk::Exception& e) {NoEph = true;}
         double elvAngle = 0;
