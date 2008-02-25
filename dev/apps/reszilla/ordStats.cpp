bb199d1afbfd903fa6fed1b29fca67198423e04c
Jon C Little <littlej@arlut.utexas.edu>
2008-02-25 04:24:54 +0000

    Changing an error message to go to stderr instead of stdout

--- dev/apps/reszilla/ordStats.cpp
+++ dev/apps/reszilla/ordStats.cpp
@@ -190,7 +190,7 @@ void OrdStats::process()
         }
       }
       catch (gpstk::Exception &exc)
-      { cout << "# Error caught in ordStats - probably missing clock offset data\n"; }
+      { cerr << "Error caught in ordStats - probably missing clock offset data\n"; }
    }  
    
    
