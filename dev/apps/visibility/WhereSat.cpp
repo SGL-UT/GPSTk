3b9d1d41bd263c053dd90b35d6f286bada46d570
Tracie Conn <tconn@whoknows.com>
2008-02-29 20:51:03 +0000

    - Got rid of "MATLAB-suitable" output option.  - Results now output in a tabular format only.  - Can process more than one SV, or all SVs.  - Added time format option.  - Azimuth/elevation only output when SV is above horizon.  - Output clock correction in ms, not seconds.  - Better exception handling, i.e.:
    clau:visibility :)./wheresat -e temp.png 
    # time, PRN, X(m), Y(m), Z(m), Clock Correction(ms)
    terminate called after throwing an instance of 'gpstk::DayTime::DayTimeException'
    Aborted

--- dev/apps/visibility/WhereSat.cpp
+++ dev/apps/visibility/WhereSat.cpp
@@ -44,6 +44,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <set>
 
 #include "DayTime.hpp"
 #include "CommandOption.hpp"
@@ -53,234 +54,185 @@
 
 using namespace std;
 using namespace gpstk;
+using namespace gpstk::StringUtils;
  
 
 int main(int argc, char *argv[])
 {
-  CommandOptionNoArg
-    helpOption('h',"help", "Print help usage.");
+	CommandOptionNoArg
+		helpOption('h',"help", "Print help usage.");
 
- CommandOptionWithAnyArg 
-    ephFiles('e',"eph-files","Ephemeris source file(s). Can be RINEX nav, SP3, or FIC.",true),
-    PositionOption('u',"position","Antenna position in ECEF (x,y,z) coordinates.  Format as a string: "
-            "\"X Y Z\". Used to give user-centered data (SV range, azimuth & elevation)."),
-    StartTimeOption('s',"start","Starting time for analysis. Format as string: \"MO/DD/YYYY HH:MM:SS\"."
-            " If no start time is specified, the default is end of                  file."),
-    EndTimeOption('f',"end", "End time for analysis.  Format as string: \"MO/DD/YYYY HH:MM:SS\". If no"
-            " end time is specified, the default is beginning of file."),
-    MatlabOption('o',"output-filename", "Filename for results to be output in MATLAB suitable file.");
+	CommandOptionWithAnyArg 
+		ephFiles('e',"eph-files","Ephemeris source file(s). Can be RINEX nav, "
+					"SP3, or FIC.",true),
+		positionOpt('u',"position","Antenna position in ECEF (x,y,z) "
+					"coordinates.  Format as a string: \"X Y Z\". Used to give "
+					"user-centered data (SV range, azimuth & elevation) when SV "
+					"is in view."),
+		startTimeOpt('\0',"start","Ignore data before this time. Format as "
+					"string: \"MO/DD/YYYY HH:MM:SS\"."),
+		endTimeOpt('\0',"end", "Ignore data after this time. Format as string: "
+					"\"MO/DD/YYYY HH:MM:SS\"."),
+		formatOpt('f',"time-format","DayTime format specifier used for times "
+					"in the output. The default is \"%4Y %3j %02H:%02M:%04.1f\".");
 
- CommandOptionWithNumberArg 
-    MyPrnOption('p',"prn","Which SV to analyze.",true),
-    TimeIncrementOption('t',"time","Time increment for ephemeris calculation.  Enter increment in "
-            "seconds.  Default is 900 (15 min).");     
+	CommandOptionWithNumberArg 
+		prnOpt('p',"prn","Which SVs to analyze. Repeat option for multiple"
+					" satellites. If this option is not specified, all ephemeris "
+					"data will be processed."),
+		incrementOpt('t',"time","Time increment for ephemeris calculation. "
+					"Enter increment in seconds. Default is 900 (15 min).");     
 
- CommandOptionParser cop("Computes the ephemeris data for a single prn.");
- cop.parseOptions(argc, argv);
+	CommandOptionParser cop("Computes ephemeris data.");
+	cop.parseOptions(argc, argv);
 
- if (helpOption.getCount())
- {
-    cop.displayUsage(cout);
-    exit(0);
- }
+	if (helpOption.getCount())
+	{
+		cop.displayUsage(cerr);
+		exit(0);
+	}
  
- if (cop.hasErrors())
- {
-    cop.dumpErrors(cout);
-    cop.displayUsage(cout);
-    exit(0);
- }
+	if (cop.hasErrors())
+	{
+		cop.dumpErrors(cerr);
+		cop.displayUsage(cerr);
+		exit(0);
+	}
  
- short myprn = StringUtils::asInt((MyPrnOption.getValue())[0]); 
- if ((myprn > gpstk::MAX_PRN) || (myprn < 1))
- {
-   cerr << "Enter a valid PRN number.\n\n";
-   exit(0);
- }
+	set<int> prnSet;
+	for (int index = 0; index < prnOpt.getCount(); index++)
+	{
+		int prn = asInt(prnOpt.getValue()[index]);
+		if ((prn > gpstk::MAX_PRN) || (prn < 1))
+		{
+			cerr<< "An invalid PRN number was entered.\n";
+			exit(0);
+		}
+		else
+		{
+			prnSet.insert(prn);
+		}
+	}
+	
+	std::string timeFormat;
+	if (formatOpt.getCount())
+      timeFormat = formatOpt.getValue()[0];
+	else
+		timeFormat = "%4Y %3j %02H:%02M:%04.1f";
+	
+	cout << "# time, PRN, X(m), Y(m), Z(m), Clock Correction(ms)";
+	
+	Xvt antXvt;	
+	if (positionOpt.getCount())
+	{
+		double x, y, z;
+		sscanf(positionOpt.getValue().front().c_str(),"%lf %lf %lf", &x, &y, &z);
+		antXvt.x[0] = x; antXvt.x[1] = y; antXvt.x[2] = z;
+		cout << ", Elevation(deg), Azimuth(deg), Range(m)";
+	}
+	cout << endl;
+	
+	int incr=900;
+	if (incrementOpt.getCount())
+		incr = asInt(incrementOpt.getValue()[0]);
  
- std::ofstream ofs;
- if (MatlabOption.getCount())
- {
-    const string fn=MatlabOption.getValue()[0];
-      ofs.open(fn.c_str(), ios::out);
-    if (PositionOption.getCount()) 
-      ofs << "#10-bit GPS week, GPS seconds of week,X,Y,Z(m),Clock Corr.(sec), azimuth (deg), "
-          << "elevation (deg), range (m)" << endl; 
- }
- else 
-    ofs << "#10-bit GPS week, GPS seconds of week,X,Y,Z(m),Clock Corr.(sec)" << endl;
- 
- int incr=900;
- if (TimeIncrementOption.getCount())
-     incr = gpstk::StringUtils::asInt((TimeIncrementOption.getValue())[0]);
- 
- Xvt ANTPVT;
- if (PositionOption.getCount())
- {
-    double x, y, z;
-    sscanf(PositionOption.getValue().front().c_str(),"%lf %lf %lf", &x, &y, &z);
-    ANTPVT.x[0] = x; ANTPVT.x[1] = y; ANTPVT.x[2] = z;
-    cout << endl << endl << setw(20) << left << " Antenna Position: " << ANTPVT.x[0] 
-         << "  " << ANTPVT.x[1] << "  " << ANTPVT.x[2] << endl;
- }
- 
- // get the ephemeris source(s)
-  EphReader ephReader;
-  for (int i=0; i<ephFiles.getCount(); i++)
-      ephReader.read(ephFiles.getValue()[i]);
+	// get the ephemeris source(s)
+	EphReader ephReader;
+	for (int i=0; i<ephFiles.getCount(); i++)
+	ephReader.read(ephFiles.getValue()[i]);
+	XvtStore<SatID>& ephStore = *ephReader.eph;
   
-  XvtStore<SatID>& ephStore = *ephReader.eph;
-  
-  DayTime tStart,tEnd;
-  if (StartTimeOption.getCount())
-  {
-      double ss;
-      int mm,dd,yy,hh,minu; 
-      sscanf(StartTimeOption.getValue().front().c_str(), "%d/%d/%d %d:%d:%lf",&mm,&dd,&yy,&hh,&minu,&ss);
-      tStart.setYMDHMS((short)yy, (short)mm, (short)dd, (short)hh, (short)minu, (double)ss);
-  }	
-  else
-  {
-      DayTime tFile(ephStore.getInitialTime());	  //extra code b/c sscanf reads in int's but setYMDHMS needs shorts
-      short year = tFile.year();
-      short month = tFile.month();
-      short day = tFile.day();
-      short hour = tFile.hour();
-      short minute = tFile.minute();
-      double ss = tFile.second();
-      tStart.setYMDHMS(year,month,day,hour,minute,ss);
-  }
+	DayTime tStart,tEnd;
+	if (startTimeOpt.getCount())
+	{
+		double ss;
+		int mm,dd,yy,hh,minu; 
+		sscanf(startTimeOpt.getValue().front().c_str(), 
+				"%d/%d/%d %d:%d:%lf",&mm,&dd,&yy,&hh,&minu,&ss);
+		tStart.setYMDHMS((short)yy, (short)mm, (short)dd, (short)hh, 
+							  (short)minu, (double)ss);
+	}	
+	else
+	{
+		//extra code b/c sscanf reads in int's but setYMDHMS needs shorts
+		DayTime tFile(ephStore.getInitialTime());	  
+		short year = tFile.year();
+		short month = tFile.month();
+		short day = tFile.day();
+		short hour = tFile.hour();
+		short minute = tFile.minute();
+		double ss = tFile.second();
+		tStart.setYMDHMS(year,month,day,hour,minute,ss);
+	}
  
-  if (EndTimeOption.getCount())
-  {
-      double ss;
-      int mm,dd,yy,hh,minu; 
-      sscanf(EndTimeOption.getValue().front().c_str(), "%d/%d/%d %d:%d:%lf",&mm,&dd,&yy,&hh,&minu,&ss);
-      tEnd.setYMDHMS((short)yy, (short)mm, (short)dd, (short)hh, (short)minu, (double)ss);
-  }
-  else
-  {
-      DayTime tFile(ephStore.getFinalTime());		//extra code b/c sscanf reads in int's but setYMDHMS needs shorts
-      short year = tFile.year();
-      short month = tFile.month();
-      short day = tFile.day();
-      short hour = tFile.hour();
-      short minute = tFile.minute();
-      double ss = tFile.second();
-      tEnd.setYMDHMS(year,month,day,hour,minute,ss);
-  }
+	if (endTimeOpt.getCount())
+	{
+		double ss;
+		int mm,dd,yy,hh,minu; 
+		sscanf(endTimeOpt.getValue().front().c_str(), 
+				"%d/%d/%d %d:%d:%lf",&mm,&dd,&yy,&hh,&minu,&ss);
+		tEnd.setYMDHMS((short)yy, (short)mm, (short)dd, (short)hh, (short)minu, 
+							(double)ss);
+	}
+	else
+	{
+		//extra code b/c sscanf reads in int's but setYMDHMS needs shorts
+		DayTime tFile(ephStore.getFinalTime());	
+		short year = tFile.year();
+		short month = tFile.month();
+		short day = tFile.day();
+		short hour = tFile.hour();
+		short minute = tFile.minute();
+		double ss = tFile.second();
+		tEnd.setYMDHMS(year,month,day,hour,minute,ss);
+	}
 
-  cout  << setw(20) << left << " Start Time: "
-        << tStart << endl;
-  cout  << setw(20) << left << " End Time: "
-        << tEnd << endl;
-  cout  << setw(20) << left << " PRN: "
-        << myprn << endl << endl;
-  cout  << " Prn " << myprn 
-        << " Earth-fixed position and clock information:"
-        << endl << endl;
-  cout  << setw(20) << left << " Date       Time(UTC)  "
-        << setw(20) << " X (meters)"
-        << setw(20) << " Y (meters)"
-        << setw(20) << " Z (meters)"
-        << setw(20) << " Clock Correc (s)"
-        << endl;
-  cout  << setw(98) << setfill('=') << " =" << endl;
-  cout.width(0);
-  cout.fill(' '); 
-  
-  DayTime t = tStart;
+	DayTime t = tStart;
+	
+	while (t <= tEnd)
+	{
+		for(set<int>::iterator i = prnSet.begin(); i != prnSet.end(); i++ )
+		{
+			SatID thisSat(*i,SatID::systemGPS); 			
+			try
+			{
+				Xvt xvt = ephStore.getXvt(thisSat, t);
+				cout << t.printf(timeFormat) << right << fixed  << setprecision(3)  
+				     << " " << setw(3)  <<  *i
+				     << " " << setw(14) << xvt.x[0]
+				     << " " << setw(14) << xvt.x[1]
+				     << " " << setw(14) << xvt.x[2]
+				     << " " << setprecision(6) << setw(10)  << (xvt.dtime*1000);
+				     
+				WGS84Geoid geoid;
+				double correction = (xvt.dtime) * (geoid.c());    
+				
+				if ((abs(antXvt.x[0]) < 1)||(antXvt.x.elvAngle(xvt.x) < 0))
+				{
+					cout << right
+						  << " "  << setw(8) << "-"
+						  << " "  << setw(8) << "-"
+						  << " "  << setw(15) << "-" << endl;	
+				}
+				else
+				{
+					cout << right << fixed << setprecision(3)
+						  << " "  << setw(8) << antXvt.x.azAngle(xvt.x)
+						  << " "  << setw(8) << antXvt.x.elvAngle(xvt.x)
+						  << " "  << setw(15) 
+						  << xvt.preciseRho(antXvt.x, geoid, correction) << endl;
+				}
+			}
+			catch(...)
+			{
+				;
+			}
+		}
+		
+		t += incr;
+		
+	}
 
-  SatID sat(myprn,SatID::systemGPS);
-  
-  int count = 0;
-  while (t < tEnd)
-  {
-      try
-      {
-        Xvt BCPVT = ephStore.getXvt(sat, t);
-        cout  << " "  << left << setw(20) << t
-              << fixed
-              << " "  << left << setw(20) << BCPVT.x[0]
-              << " "  << left << setw(20) << BCPVT.x[1]
-              << " "  << left << setw(20) << BCPVT.x[2]
-              << " "  << left << setw(20) << BCPVT.dtime << endl;
-        count++;
-      }
-      catch (InvalidRequest& e) { ; }
-      t += incr;
-  }
-  if (PositionOption.getCount())
-  {
-      ECEF rxPos = Triple(ANTPVT.x);
-      cout  << endl << endl; 
-      cout  << " Data for user reference frame:" << endl << endl;;
-      cout  << setw(20) << left << " Date       Time(UTC)  "
-            << setw(15) << " Azimuth"
-            << setw(15) << " Elevation"
-            << setw(15) << " Range to SV (m)"
-            << endl;
-      cout  << setw(70) << setfill('=') << " =" << endl;
-      cout.width(0);
-      cout.fill(' '); 
-      t = tStart;
-      
-      while (t < tEnd)
-      {
-        try 
-        {
-          Xvt BCPVT = ephStore.getXvt(sat, t);
-          WGS84Geoid geoid;
-          double correction = (BCPVT.dtime) * (geoid.c());
-          cout  << fixed
-                << " "  << left << setw(20) <<t
-                << " "  << left << setw(15) << ANTPVT.x.azAngle(BCPVT.x)
-                << " "  << left << setw(15) << ANTPVT.x.elvAngle(BCPVT.x)
-                << " "  << left << setw(15) << BCPVT.preciseRho(rxPos, geoid, correction)
-                << endl;
-        }
-        catch (gpstk::Exception& e) {;}
-        t += incr;
-      }
-    }
-  
-  if (MatlabOption.getCount())
-  {
-      t = tStart;
-      while (t < tEnd)
-      {
-        try 
-        {   
-          Xvt BCPVT = ephStore.getXvt(sat, t);
-          WGS84Geoid geoid;
-          double correction = (BCPVT.dtime) * (geoid.c());
-          
-          ofs << fixed
-              << left << setw(6) <<t.GPSfullweek()
-              << " "  << left << setw(8) << t.GPSsecond() 
-              << " "  << left << setw(20) << BCPVT.x[0]
-              << " "  << left << setw(20) << BCPVT.x[1]
-              << " "  << left << setw(20) << BCPVT.x[2]
-              << " "  << left << setw(10) << BCPVT.dtime;
-              if (PositionOption.getCount())
-              {
-                ofs << fixed
-                    << " "  << left << setw(15) << ANTPVT.x.azAngle(BCPVT.x)
-                    << " "  << left << setw(15) << ANTPVT.x.elvAngle(BCPVT.x)
-                    << " "  << left << setw(15) << BCPVT.preciseRho(Triple(ANTPVT.x), geoid, correction);
-              }
-              ofs << endl;
-        }
-        catch (gpstk::Exception& e) { ; }
-        t += incr;
-      }
-    }
-  
-  if (count==0) { cout << endl << endl << " No epochs for prn " << myprn << "!" << endl << endl; }
-  else { cout << endl <<  " Calculated " << count << " increments for prn " << myprn << " ." << endl<< endl << endl; }
-  
-  ofs.close();
-  exit(0);
-
+	exit(0);
 }
 
