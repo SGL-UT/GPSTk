4a28fa2ce12dc73edbd6a265278c6759bbbdc7bd
Dagoberto Salazar <architest@whoknows.com>
2008-03-20 23:22:49 +0000

    Added class 'CodeKalmanSolver'. It computes code-based solutions using a simple Kalman solver.

---
+++ dev/lib/procframe/CodeKalmanSolver.cpp
@@ -0,0 +1,369 @@
+#pragma ident "$Id: $"
+
+/**
+ * @file CodeKalmanSolver.cpp
+ * Class to compute the code-based solution using a simple Kalman solver.
+ */
+
+//============================================================================
+//
+//  This file is part of GPSTk, the GPS Toolkit.
+//
+//  The GPSTk is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published
+//  by the Free Software Foundation; either version 2.1 of the License, or
+//  any later version.
+//
+//  The GPSTk is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with GPSTk; if not, write to the Free Software Foundation,
+//  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  
+//  Dagoberto Salazar - gAGE ( http://www.gage.es ). 2008
+//
+//============================================================================
+
+
+#include "CodeKalmanSolver.hpp"
+#include "MatrixFunctors.hpp"
+
+
+namespace gpstk
+{
+
+      // Index initially assigned to this class
+   int CodeKalmanSolver::classIndex = 8000000;
+
+
+      // Returns an index identifying this object.
+   int CodeKalmanSolver::getIndex() const
+   { return index; }
+
+
+      // Returns a string identifying this object.
+   std::string CodeKalmanSolver::getClassName() const
+   { return "CodeKalmanSolver"; }
+
+
+      // Constant stochastic model
+   StochasticModel CodeKalmanSolver::constantModel;
+
+
+      // White noise stochastic model
+   WhiteNoiseModel CodeKalmanSolver::whitenoiseModel;
+
+
+      // Initializing method.
+   void CodeKalmanSolver::Init()
+   {
+         // Set the class index
+      setIndex();
+
+      numUnknowns = defaultEqDef.body.size();
+
+      Vector<double> initialState(numUnknowns, 0.0);
+      Matrix<double> initialErrorCovariance(numUnknowns, numUnknowns, 0.0);
+         // Fill the initialErrorCovariance matrix
+         // First, the coordinates
+      for (int i=0; i<3; i++)
+      {
+         initialErrorCovariance(i,i) = 100.0;
+      }
+         // Now, the receiver clock
+      initialErrorCovariance(3,3) = 9.0e10;
+
+      kFilter.Reset( initialState, initialErrorCovariance );
+
+         // Pointer to default coordinates stochastic model (constant)
+      pCoordStoModel = &constantModel;
+
+
+         // Pointer to default receiver clock stochastic model (white noise)
+      pClockStoModel = &whitenoiseModel;
+
+
+      solution.resize(numUnknowns);
+   }
+
+
+      // Default constructor.
+   CodeKalmanSolver::CodeKalmanSolver()
+   {
+         // First, let's define a set with the typical code-based unknowns
+      TypeIDSet tempSet;
+      tempSet.insert(TypeID::dx);
+      tempSet.insert(TypeID::dy);
+      tempSet.insert(TypeID::dz);
+      tempSet.insert(TypeID::cdt);
+
+         // Now, we build the default definition for a common GNSS 
+         // code-based equation
+      defaultEqDef.header = TypeID::prefitC;
+      defaultEqDef.body = tempSet;
+
+         // Call the initializing method
+      Init();
+
+   }
+
+
+      /* Explicit constructor. Sets the default equation definition 
+       * to be used when fed with GNSS data structures.
+       *
+       * @param eqDef     gnssEquationDefinition to be used
+       */
+   CodeKalmanSolver::CodeKalmanSolver(const gnssEquationDefinition& eqDef)
+   {
+      setDefaultEqDefinition(eqDef);
+      Init();
+   }
+
+
+      /* Compute the solution of the given equations set.
+       *
+       * @param prefitResiduals   Vector of prefit residuals
+       * @param designMatrix      Design matrix for the equation system
+       * @param weightVector      Vector of weights assigned to each
+       *                          satellite.
+       *
+       * @return
+       *  0 if OK
+       *  -1 if problems arose
+       */
+   int CodeKalmanSolver::Compute( const Vector<double>& prefitResiduals,
+                                    const Matrix<double>& designMatrix,
+                                    const Vector<double>& weightVector )
+      throw(InvalidSolver)
+   {
+         // First, check that everyting has a proper size
+      int wSize = static_cast<int>(weightVector.size());
+      int pSize = static_cast<int>(prefitResiduals.size());
+      if (!(wSize==pSize))
+      {
+         InvalidSolver e("prefitResiduals size does not match dimension \
+                          of weightVector");
+         GPSTK_THROW(e);
+      }
+
+      Matrix<double> wMatrix(wSize,wSize,0.0);  // Declare a weight matrix
+
+         // Fill the weight matrix diagonal with the content of 
+         // the weight vector
+      for (int i=0; i<wSize; i++)
+      {
+         wMatrix(i,i) = weightVector(i);
+      }
+
+         // Call the more general CodeKalmanSolver::Compute() method
+      return CodeKalmanSolver::Compute( prefitResiduals,
+                                          designMatrix,
+                                          wMatrix );
+   }
+
+
+      // Compute the solution of the given equations set.
+      //
+      // @param prefitResiduals   Vector of prefit residuals
+      // @param designMatrix      Design matrix for equation system
+      // @param weightMatrix      Matrix of weights
+      //
+      // @return
+      //  0 if OK
+      //  -1 if problems arose
+      //
+   int CodeKalmanSolver::Compute( const Vector<double>& prefitResiduals,
+                                    const Matrix<double>& designMatrix,
+                                    const Matrix<double>& weightMatrix )
+      throw(InvalidSolver)
+   {
+      if (!(weightMatrix.isSquare()))
+      {
+         InvalidSolver e("Weight matrix is not square");
+         GPSTK_THROW(e);
+      }
+
+      int wRow = static_cast<int>(weightMatrix.rows());
+      int pRow = static_cast<int>(prefitResiduals.size());
+      if (!(wRow==pRow))
+      {
+         InvalidSolver e("prefitResiduals size does not match dimension of \
+                          weightMatrix");
+         GPSTK_THROW(e);
+      }
+
+      int gCol = static_cast<int>(designMatrix.cols());
+
+      int gRow = static_cast<int>(designMatrix.rows());
+      if (!(gRow==pRow))
+      {
+         InvalidSolver e("prefitResiduals size does not match dimension \
+                          of designMatrix");
+         GPSTK_THROW(e);
+      }
+
+      if (!(phiMatrix.isSquare()))
+      {
+         InvalidSolver e("phiMatrix is not square");
+         GPSTK_THROW(e);
+      }
+
+      int phiRow = static_cast<int>(phiMatrix.rows());
+      if (!(phiRow==numUnknowns))
+      {
+         InvalidSolver e("prefitResiduals size does not match dimension \
+                          of phiMatrix");
+         GPSTK_THROW(e);
+      }
+
+      if (!(qMatrix.isSquare()))
+      {
+         InvalidSolver e("qMatrix is not square");
+         GPSTK_THROW(e);
+      }
+
+      int qRow = static_cast<int>(qMatrix.rows());
+      if (!(qRow==numUnknowns))
+      {
+         InvalidSolver e("prefitResiduals size does not match dimension \
+                          of qMatrix");
+         GPSTK_THROW(e);
+      }
+
+
+      try
+      {
+            // Call the Kalman filter object.
+         kFilter.Compute( phiMatrix,
+                          qMatrix,
+                          prefitResiduals,
+                          designMatrix,
+                          weightMatrix );
+      }
+      catch(InvalidSolver& e)
+      {
+         GPSTK_RETHROW(e);
+      }
+
+         // Store the solution
+      solution = kFilter.xhat;
+
+         // Compute the postfit residuals Vector
+      postfitResiduals = prefitResiduals - designMatrix * solution;
+
+         // If everything is fine so far, then the results should be valid
+      valid = true;
+
+      return 0;
+
+   }  // end CodeKalmanSolver::Compute()
+
+
+
+      /* Returns a reference to a gnnsSatTypeValue object after 
+       * solving the previously defined equation system.
+       *
+       * @param gData    Data object holding the data.
+       */
+   gnssSatTypeValue& CodeKalmanSolver::Process(gnssSatTypeValue& gData)
+      throw(InvalidSolver)
+   {
+         // Build a gnssRinex object and fill it with data
+      gnssRinex g1;
+      g1.header = gData.header;
+      g1.body = gData.body;
+
+         // Call the Process() method with the appropriate input object
+      Process(g1);
+
+         // Update the original gnssSatTypeValue object with the results
+      gData.body = g1.body;
+
+      return gData;
+   }
+
+
+      /* Returns a reference to a gnnsRinex object after solving 
+       * the previously defined equation system.
+       *
+       * @param gData     Data object holding the data.
+       */
+   gnssRinex& CodeKalmanSolver::Process(gnssRinex& gData)
+      throw(InvalidSolver)
+   {
+         // Number of measurements equals the number of visible satellites
+      numMeas = gData.numSats();
+
+         // State Transition Matrix (PhiMatrix)
+      phiMatrix.resize(numUnknowns, numUnknowns, 0.0);
+
+         // Noise covariance matrix (QMatrix)
+      qMatrix.resize(numUnknowns, numUnknowns, 0.0);
+
+         // Geometry matrix
+      hMatrix.resize(numMeas, numUnknowns, 0.0);
+
+         // Weights matrix
+      rMatrix.resize(numMeas, numMeas, 0.0);
+
+         // Measurements vector (Prefit-residuals)
+      measVector.resize(numMeas, 0.0);
+
+         // Build the vector of measurements
+      measVector = gData.getVectorOfTypeID(defaultEqDef.header);
+
+         // Generate the appropriate weights vector
+      Vector<double> weightsVector(gData.getVectorOfTypeID(TypeID::weight));
+      for (int i= 0; i<numMeas; i++)
+      {
+         rMatrix(i,i) = weightsVector(i);
+      }
+
+         // Generate the corresponding geometry/design matrix
+      hMatrix = gData.body.getMatrixOfTypes((*this).defaultEqDef.body);
+
+      SatID  dummySat;
+      TypeID dummyType;
+         // Now, let's fill the Phi and Q matrices
+         // First, the coordinates
+      pCoordStoModel->Prepare(dummyType, dummySat, gData);
+      for (int i=0; i<3; i++)
+      {
+         phiMatrix(i,i) = pCoordStoModel->getPhi();
+         qMatrix(i,i)   = pCoordStoModel->getQ();
+      }
+
+         // Now, the receiver clock
+      pClockStoModel->Prepare(dummyType, dummySat, gData);
+      phiMatrix(3,3) = pClockStoModel->getPhi();
+      qMatrix(3,3)   = pClockStoModel->getQ();
+
+      try
+      {
+            // Call the Compute() method with the defined equation model.
+            // This equation model MUST HAS BEEN previously set, usually when
+            // creating the CodeKalmanSolver object with the appropriate
+            // constructor.
+         Compute(measVector, hMatrix, rMatrix);
+      }
+      catch(InvalidSolver& e)
+      {
+         GPSTK_RETHROW(e);
+      }
+
+
+         // Now we have to add the new values to the data structure
+      if ( defaultEqDef.header == TypeID::prefitC )
+      {
+         gData.insertTypeIDVector(TypeID::postfitC, postfitResiduals);
+      }
+
+      return gData;
+
+   }   // End CodeKalmanSolver::Process()
+
+
+} // end namespace gpstk
---
+++ dev/lib/procframe/CodeKalmanSolver.hpp
@@ -0,0 +1,343 @@
+#pragma ident "$Id: $"
+
+/**
+ * @file CodeKalmanSolver.hpp
+ * Class to compute the code-based solution using a simple Kalman solver.
+ */
+
+#ifndef CODEKALMANSOLVER_HPP
+#define CODEKALMANSOLVER_HPP
+
+//============================================================================
+//
+//  This file is part of GPSTk, the GPS Toolkit.
+//
+//  The GPSTk is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published
+//  by the Free Software Foundation; either version 2.1 of the License, or
+//  any later version.
+//
+//  The GPSTk is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with GPSTk; if not, write to the Free Software Foundation,
+//  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  
+//  Dagoberto Salazar - gAGE ( http://www.gage.es ). 2008
+//
+//============================================================================
+
+
+#include "SolverLMS.hpp"
+#include "StochasticModel.hpp"
+#include "SimpleKalmanFilter.hpp"
+
+
+namespace gpstk
+{
+
+      /** @addtogroup GPSsolutions */
+      /// @ingroup math
+
+      //@{
+
+      /** This class computes the code-based solution using a simple
+       *  Kalman solver.
+       * 
+       * This class may be used either in a Vector- and Matrix-oriented way, 
+       * or with GNSS data structure objects from "DataStructures" class.
+       *
+       * A typical way to use this class with GNSS data structures follows:
+       *
+       * @code
+       *   RinexObsStream rin("ebre0300.02o");  // Data stream
+       *
+       *   // More declarations here: Ionospheric and tropospheric models, 
+       *   // ephemeris, etc.
+       *
+       *   // Declare the modeler object, setting all the parameters in one
+       *   // pass
+       *   ModelObs model(ionoStore, mopsTM, bceStore, TypeID::C1);
+       *   model.Prepare();     // Set initial position (Bancroft method)
+       *
+       *   // Declare a CodeKalmanSolver object
+       *   CodeKalmanSolver kSolver;
+       *
+       *   // This object will compute the appropriate MOPS weights
+       *   ComputeMOPSWeights mopsW(nominalPos, bceStore);
+       *
+       *   gnssRinex gRin;
+       *
+       *   while(rin >> gRin) {
+       *      gRin >> model >> mopsW >> kSolver;
+       *   }
+       * @endcode
+       *
+       * The "CodeKalmanSolver" object will extract all the data it needs
+       * from the GNSS data structure that is "gRin" and will try to solve the
+       * code-based system of equations using a simple Kalman filter. It will 
+       * also insert back postfit residual data into "gRin" if it successfully
+       * solves the equation system.
+       *
+       * Please note it needs some weights assigned to each satellite.
+       * This can be achieved with objects from classes such as
+       * "ComputeIURAWeights", "ComputeMOPSWeights", etc., but in any case 
+       * this is a mandatory step.
+       *
+       * By default, it will build the geometry matrix from the values of 
+       * coefficients dx, dy, dz and cdt, and the independent vector will be
+       * composed of the code prefit residuals (TypeID::prefitC) values.
+       *
+       * You may change the former by redefining the default equation
+       * definition to be used. For instance:
+       *
+       * @code
+       *   TypeIDSet unknownsSet;
+       *   unknownsSet.insert(TypeID::dLat);
+       *   unknownsSet.insert(TypeID::dLon);
+       *   unknownsSet.insert(TypeID::dH);
+       *   unknownsSet.insert(TypeID::cdt);
+       *
+       *   // Create a new equation definition
+       *   // newEq(independent value, set of unknowns)
+       *   gnssEquationDefinition newEq(TypeID::prefitC, unknownsSet);
+       *
+       *   // Reconfigure solver
+       *   kSolver.setDefaultEqDefinition(newEq);
+       * @endcode
+       *
+       *
+       * \warning Kalman filters are objets that store their internal
+       * state, so you MUST NOT use the SAME object to process DIFFERENT data
+       * streams.
+       *
+       * @sa SolverBase.hpp and SolverLMS for base classes.
+       *
+       */
+   class CodeKalmanSolver : public SolverLMS
+   {
+   public:
+
+         /// Default constructor.
+      CodeKalmanSolver();
+
+
+         /** Explicit constructor. Sets the default equation definition 
+          *  to be used when fed with GNSS data structures.
+          *
+          * @param eqDef     gnssEquationDefinition to be used
+          */
+      CodeKalmanSolver(const gnssEquationDefinition& eqDef);
+
+
+         /** Compute the PPP Solution of the given equations set.
+          *
+          * @param prefitResiduals   Vector of prefit residuals
+          * @param designMatrix      Design matrix for the equation system
+          * @param weightMatrix      Matrix of weights
+          *
+          * @return
+          *  0 if OK
+          *  -1 if problems arose
+          */
+      virtual int Compute(const Vector<double>& prefitResiduals,
+                          const Matrix<double>& designMatrix,
+                          const Matrix<double>& weightMatrix)
+         throw(InvalidSolver);
+
+
+         /** Compute the PPP Solution of the given equations set.
+          *
+          * @param prefitResiduals   Vector of prefit residuals
+          * @param designMatrix      Design matrix for the equation system
+          * @param weightVector      Vector of weights assigned to each
+          *                          satellite.
+          *
+          * @return
+          *  0 if OK
+          *  -1 if problems arose
+          */
+      virtual int Compute(const Vector<double>& prefitResiduals,
+                          const Matrix<double>& designMatrix,
+                          const Vector<double>& weightVector)
+         throw(InvalidSolver);
+
+
+         /** Returns a reference to a gnnsSatTypeValue object after 
+          *  solving the previously defined equation system.
+          *
+          * @param gData    Data object holding the data.
+          */
+      virtual gnssSatTypeValue& Process(gnssSatTypeValue& gData)
+         throw(InvalidSolver);
+
+
+         /** Returns a reference to a gnnsRinex object after solving 
+          *  the previously defined equation system.
+          *
+          * @param gData    Data object holding the data.
+          */
+      virtual gnssRinex& Process(gnssRinex& gData)
+         throw(InvalidSolver);
+
+
+         /// Get coordinates stochastic model pointer
+      StochasticModel* getCoordinatesModel() const
+      { return pCoordStoModel; };
+
+
+         /** Set coordinates stochastic model
+          *
+          * @param pModel      Pointer to StochasticModel associated with
+          *                    coordinates.
+          */
+      CodeKalmanSolver& setCoordinatesModel(StochasticModel* pModel)
+      { pCoordStoModel = pModel; return (*this); };
+
+
+         /// Get receiver clock stochastic model pointer
+      StochasticModel* getReceiverClockModel() const
+      { return pClockStoModel; };
+
+
+         /** Set receiver clock stochastic model
+          *
+          * @param pModel      Pointer to StochasticModel associated with
+          *                    receiver clock.
+          */
+      CodeKalmanSolver& setReceiverClockModel(StochasticModel* pModel)
+      { pClockStoModel = pModel; return (*this); };
+
+
+         /// Get the State Transition Matrix (phiMatrix)
+      Matrix<double> getPhiMatrix() const
+      { return phiMatrix; };
+
+
+         /** Set the State Transition Matrix (phiMatrix)
+          *
+          * @param pMatrix     State Transition matrix.
+          *
+          * \warning Process() methods set phiMatrix and qMatrix according to
+          * the stochastic models already defined. Therefore, you must use
+          * the Compute() methods directly if you use this method. 
+          *
+          */
+      CodeKalmanSolver& setPhiMatrix(const Matrix<double> & pMatrix)
+      { phiMatrix = pMatrix; return (*this); };
+
+
+         /// Get the Noise covariance matrix (QMatrix)
+      Matrix<double> getQMatrix() const
+      { return qMatrix; };
+
+
+         /** Set the Noise Covariance Matrix (QMatrix)
+          *
+          * @param pMatrix     Noise Covariance matrix.
+          *
+          * \warning Process() methods set phiMatrix and qMatrix according to
+          * the stochastic models already defined. Therefore, you must use
+          * the Compute() methods directly if you use this method. 
+          *
+          */
+      CodeKalmanSolver& setQMatrix(const Matrix<double> & pMatrix)
+      { qMatrix = pMatrix; return (*this); };
+
+
+         /// Returns an index identifying this object.
+      virtual int getIndex(void) const;
+
+
+         /// Returns a string identifying this object.
+      virtual std::string getClassName(void) const;
+
+
+         /** Sets the index to a given arbitrary value. Use with caution.
+          *
+          * @param newindex      New integer index to be assigned to 
+          *                      current object.
+          */
+      CodeKalmanSolver& setIndex(const int newindex)
+      { index = newindex; return (*this); };
+
+
+         /// Destructor.
+      virtual ~CodeKalmanSolver() {};
+
+
+   private:
+
+
+         /// Number of unknowns
+      int numUnknowns;
+
+
+         /// Number of measurements
+      int numMeas;
+
+
+         /// Pointer to stochastic model for coordinates
+      StochasticModel* pCoordStoModel;
+
+
+         /// Pointer to stochastic model for receiver clock
+      StochasticModel* pClockStoModel;
+
+
+         /// State Transition Matrix (PhiMatrix)
+      Matrix<double> phiMatrix;
+
+
+         /// Noise covariance matrix (QMatrix)
+      Matrix<double> qMatrix;
+
+
+         /// Geometry matrix
+      Matrix<double> hMatrix;
+
+
+         /// Weights matrix
+      Matrix<double> rMatrix;
+
+
+         /// Measurements vector (Prefit-residuals)
+      Vector<double> measVector;
+
+
+         /// General Kalman filter object
+      SimpleKalmanFilter kFilter;
+
+
+         /// Initializing method.
+      void Init(void); 
+
+
+         /// Constant stochastic model
+      static StochasticModel constantModel;
+
+
+         /// White noise stochastic model
+      static WhiteNoiseModel whitenoiseModel;
+
+
+         /// Initial index assigned to this class.
+      static int classIndex;
+
+         /// Index belonging to this object.
+      int index;
+
+         /// Sets the index and increment classIndex.
+      void setIndex(void)
+      { index = classIndex++; }; 
+
+
+   }; // class CodeKalmanSolver
+
+      //@}
+
+} // namespace
+#endif // CODEKALMANSOLVER_HPP
