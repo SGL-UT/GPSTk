d98362a25872190147777baab21c977030beefee
Dagoberto Salazar <architest@whoknows.com>
2008-06-30 14:44:37 +0000

    Now, class 'PhaseAmbiguityModel' expects satellite arcs by default, which implies using 'SatArcMarker' before calling 'PhaseAmbiguityModel' objects (this is usually done inside Kalman filter solvers). You may change this behavior using 'setWatchSatArc()' method, and use cycle slip flags directly.

--- dev/lib/procframe/StochasticModel.cpp
+++ dev/lib/procframe/StochasticModel.cpp
@@ -23,7 +23,7 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//  
+//
 //  Dagoberto Salazar - gAGE ( http://www.gage.es ). 2007, 2008
 //
 //============================================================================
