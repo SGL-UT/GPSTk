d99f1e37ff70af6c51059daaec78d8379d2a2954
Eric Hagen <ehagen@whoknows.com>
2009-01-09 21:32:00 +0000

    Fixed problem with JulianDate test, checks out now.

--- dev/lib/Rinex3/UnitTests/JulianDate/xJulianDate.cpp
+++ dev/lib/Rinex3/UnitTests/JulianDate/xJulianDate.cpp
@@ -58,17 +58,17 @@ void xJulianDate :: operatorTest (void)
 void xJulianDate :: resetTest (void)
 {
 
-	Rinex3::JulianDate Compare(13500000,gpstk::GPS);
+	Rinex3::JulianDate Compare(1350000,gpstk::GPS);
 
-//	CommonTime Test = Compare.convertToCommonTime();
+	CommonTime Test = Compare.convertToCommonTime();
 
-//	Rinex3::JulianDate Test2;
-//	Test2.convertFromCommonTime(Test);
+	Rinex3::JulianDate Test2;
+	Test2.convertFromCommonTime(Test);
 
-//	CPPUNIT_ASSERT_EQUAL(Test2,Compare);
+	CPPUNIT_ASSERT_EQUAL(Test2,Compare);
 
 	CPPUNIT_ASSERT_EQUAL(gpstk::GPS,Compare.getTimeSystem());
-	CPPUNIT_ASSERT_EQUAL(13500000,(int)Compare.jd);
+	CPPUNIT_ASSERT_EQUAL(1350000,(int)Compare.jd);
 
 	Compare.reset();
 	CPPUNIT_ASSERT_EQUAL(gpstk::Unknown,Compare.getTimeSystem());
@@ -79,18 +79,18 @@ void xJulianDate :: resetTest (void)
 void xJulianDate :: timeSystemTest (void)
 {
 
-	Rinex3::JulianDate GPS1(13500000,gpstk::GPS);
-	Rinex3::JulianDate GPS2(13400000,gpstk::GPS);
-	Rinex3::JulianDate UTC(13500000,gpstk::UTC);
-	Rinex3::JulianDate UNKNOWN(13500000,gpstk::Unknown);
-	Rinex3::JulianDate ANY(13500000,gpstk::Any);
+	Rinex3::JulianDate GPS1(1350000,gpstk::GPS);
+	Rinex3::JulianDate GPS2(1340000,gpstk::GPS);
+	Rinex3::JulianDate UTC(1350000,gpstk::UTC);
+	Rinex3::JulianDate UNKNOWN(1350000,gpstk::Unknown);
+	Rinex3::JulianDate ANY(1350000,gpstk::Any);
 
 	CPPUNIT_ASSERT(GPS1 != GPS2);
 	CPPUNIT_ASSERT_EQUAL(GPS1.getTimeSystem(),GPS2.getTimeSystem());
 	CPPUNIT_ASSERT(GPS1 != UTC);
 	CPPUNIT_ASSERT(GPS1 != UNKNOWN);
-//	CPPUNIT_ASSERT(GPS1.convertToCommonTime() > gpstk::CommonTime::BEGINNING_OF_TIME);
-//	CPPUNIT_ASSERT(gpstk::CommonTime::BEGINNING_OF_TIME < GPS1);
+	CPPUNIT_ASSERT(GPS1.convertToCommonTime() > gpstk::CommonTime::BEGINNING_OF_TIME);
+	CPPUNIT_ASSERT(gpstk::CommonTime::BEGINNING_OF_TIME < GPS1);
 	CPPUNIT_ASSERT_EQUAL(GPS1,ANY);
 	CPPUNIT_ASSERT_EQUAL(UTC,ANY);
 	CPPUNIT_ASSERT_EQUAL(UNKNOWN,ANY);
@@ -105,12 +105,12 @@ void xJulianDate :: timeSystemTest (void)
 
 void xJulianDate :: printfTest (void)
 {
-	Rinex3::JulianDate GPS1(13500000,gpstk::GPS);
-	Rinex3::JulianDate UTC(13500000,gpstk::UTC);
+	Rinex3::JulianDate GPS1(1350000,gpstk::GPS);
+	Rinex3::JulianDate UTC(1350000,gpstk::UTC);
 
 
-	CPPUNIT_ASSERT_EQUAL(GPS1.printf("%08J %02P"),(std::string)"13500000.000000 02");
-	CPPUNIT_ASSERT_EQUAL(UTC.printf("%08J %02P"),(std::string)"13500000.000000 03");
+	CPPUNIT_ASSERT_EQUAL(GPS1.printf("%08J %02P"),(std::string)"1350000.000000 02");
+	CPPUNIT_ASSERT_EQUAL(UTC.printf("%08J %02P"),(std::string)"1350000.000000 03");
 	CPPUNIT_ASSERT_EQUAL(GPS1.printError("%08J %02P"),(std::string)"ErrorBadTime ErrorBadTime");
 	CPPUNIT_ASSERT_EQUAL(UTC.printError("%08J %02P"),(std::string)"ErrorBadTime ErrorBadTime");
 }
