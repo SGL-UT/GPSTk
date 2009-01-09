984e253b7196f66d6b8a4d4e92a22f7cdec85742
Dave Rainwater <rain@arlut.utexas.edu>
2009-01-09 20:51:27 +0000

    Missed CivilTime's == operator. -DR

--- dev/lib/Rinex3/CivilTime.cpp
+++ dev/lib/Rinex3/CivilTime.cpp
@@ -291,7 +291,7 @@ namespace Rinex3
      /// Any (wildcard) type exception allowed, otherwise must be same time systems
       if ((timeSystem != Any && right.timeSystem != Any) &&
            timeSystem != right.timeSystem)
-         throw InvalidRequest("CommonTime objects not in same time system, cannot be compared");
+         return false;
 
       if( year   == right.year    &&
           month  == right.month   &&
