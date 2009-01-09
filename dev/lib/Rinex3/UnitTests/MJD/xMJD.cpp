7cb2b3952259b813783f1e51c733436583671719
Eric Hagen <ehagen@whoknows.com>
2009-01-09 21:37:20 +0000

    Added new tests to MJD

--- dev/lib/Rinex3/UnitTests/MJD/xMJD.cpp
+++ dev/lib/Rinex3/UnitTests/MJD/xMJD.cpp
@@ -20,10 +20,6 @@ void xMJD :: setFromInfoTest (void)
 	CPPUNIT_ASSERT(setFromInfo1.setFromInfo(Id));
 	Id.erase('Q');
 	CPPUNIT_ASSERT(setFromInfo2.setFromInfo(Id));
-	ofstream out("Logs/printfOutput");
-	
-	out << setFromInfo1 << endl;
-	out << setFromInfo2 << endl;
 }
 
 void xMJD :: operatorTest (void)
@@ -53,5 +49,61 @@ void xMJD :: operatorTest (void)
 	CPPUNIT_ASSERT(Compare >= CompareCopy);
 	
 	CPPUNIT_ASSERT(Compare.isValid());
+}
+
+void xMJD :: resetTest (void)
+{
+	Rinex3::MJD Compare(135000,gpstk::GPS);
+
+	CommonTime Test = Compare.convertToCommonTime();
+
+	Rinex3::MJD Test2;
+	Test2.convertFromCommonTime(Test);
+
+	CPPUNIT_ASSERT_EQUAL(Test2,Compare);
+
+	CPPUNIT_ASSERT_EQUAL(gpstk::GPS,Compare.getTimeSystem());
+	CPPUNIT_ASSERT_EQUAL(135000,(int)Compare.mjd);
+
+	Compare.reset();
+	CPPUNIT_ASSERT_EQUAL(gpstk::Unknown,Compare.getTimeSystem());
+	CPPUNIT_ASSERT_EQUAL(0,(int)Compare.mjd);
+}
+
+void xMJD :: timeSystemTest (void)
+{
+	Rinex3::MJD GPS1(135000,gpstk::GPS);
+	Rinex3::MJD GPS2(134000,gpstk::GPS);
+	Rinex3::MJD UTC(135000,gpstk::UTC);
+	Rinex3::MJD UNKNOWN(135000,gpstk::Unknown);
+	Rinex3::MJD ANY(135000,gpstk::Any);
+
+	CPPUNIT_ASSERT(GPS1 != GPS2);
+	CPPUNIT_ASSERT_EQUAL(GPS1.getTimeSystem(),GPS2.getTimeSystem());
+	CPPUNIT_ASSERT(GPS1 != UTC);
+	CPPUNIT_ASSERT(GPS1 != UNKNOWN);
+	CPPUNIT_ASSERT(GPS1.convertToCommonTime() > gpstk::CommonTime::BEGINNING_OF_TIME);
+	CPPUNIT_ASSERT(gpstk::CommonTime::BEGINNING_OF_TIME < GPS1);
+	CPPUNIT_ASSERT_EQUAL(GPS1,ANY);
+	CPPUNIT_ASSERT_EQUAL(UTC,ANY);
+	CPPUNIT_ASSERT_EQUAL(UNKNOWN,ANY);
+	CPPUNIT_ASSERT(GPS2 != ANY);
+	CPPUNIT_ASSERT(GPS2 < GPS1);
+	CPPUNIT_ASSERT(GPS2 < ANY);
+
+	UNKNOWN.setTimeSystem(gpstk::GPS);
+	CPPUNIT_ASSERT_EQUAL(UNKNOWN.getTimeSystem(),gpstk::GPS);
+}
+
+void xMJD :: printfTest (void)
+{
+	Rinex3::MJD GPS1(135000,gpstk::GPS);
+	Rinex3::MJD UTC(135000,gpstk::UTC);
+
+
+	CPPUNIT_ASSERT_EQUAL(GPS1.printf("%08Q %02P"),(std::string)"135000.000000 02");
+	CPPUNIT_ASSERT_EQUAL(UTC.printf("%08Q %02P"),(std::string)"135000.000000 03");
+	CPPUNIT_ASSERT_EQUAL(GPS1.printError("%08Q %02P"),(std::string)"ErrorBadTime ErrorBadTime");
+	CPPUNIT_ASSERT_EQUAL(UTC.printError("%08Q %02P"),(std::string)"ErrorBadTime ErrorBadTime");
 }
 
