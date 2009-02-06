bbf9d94758f30b352551749251ab2e643ce9de53
Dave Rainwater <rain@arlut.utexas.edu>
2009-02-06 00:26:34 +0000

    Had to move GPSEphStore methods up to KepEphStore. They are generic for GNSSes anyway.  -DR

--- dev/lib/Rinex3/KeplerianEphemerisStore.hpp
+++ dev/lib/Rinex3/KeplerianEphemerisStore.hpp
@@ -54,6 +54,9 @@
 #include "SatID.hpp"
 #include "CommonTime.hpp"
 #include "XvtStore.hpp"
+#include "EngEphemeris.hpp"
+#include "icd_200_constants.hpp"
+#include "CivilTime.hpp"
 
 using namespace gpstk;
 
@@ -68,18 +71,194 @@ namespace Rinex3
    class KeplerianEphemerisStore : public XvtStore<SatID>
    {
    public:
+      
+      KeplerianEphemerisStore()
+         throw()
+         : initialTime(CommonTime::END_OF_TIME), 
+           finalTime(CommonTime::BEGINNING_OF_TIME),
+           method(0)
+      {}
+
+
       virtual ~KeplerianEphemerisStore()
       {}
 
-      bool velocityIsPresent()
+      /// Returns the position, velocity, and clock offset of the indicated
+      /// satellite in ECEF coordinates (meters) at the indicated time.
+      /// @param[in] sat the SV's SatID
+      /// @param[in] t the time to look up
+      /// @return the Xvt of the SV at time
+      /// @throw InvalidRequest If the request can not be completed for any
+      ///    reason, this is thrown. The text may have additional
+      ///    information as to why the request failed.
+      virtual Xvt getXvt(const SatID sat, const CommonTime& t) const
+         throw( gpstk::InvalidRequest );
+      
+
+      /// A debugging function that outputs in human readable form,
+      /// all data stored in this object.
+      /// @param[in] s the stream to receive the output; defaults to cout
+      /// @param[in] detail the level of detail to provide
+      virtual void dump(std::ostream& s = std::cout, short detail = 0) const
+         throw();
+
+
+      /// Edit the dataset, removing data outside the indicated time interval
+      /// @param tmin defines the beginning of the time interval
+      /// @param tmax defines the end of the time interval
+      virtual void edit(const CommonTime& tmin, 
+                const CommonTime& tmax = CommonTime::END_OF_TIME )
+         throw();
+
+
+      /// Determine the earliest time for which this object can successfully 
+      /// determine the Xvt for any satellite.
+      /// @return The initial time
+      /// @throw InvalidRequest This is thrown if the object has no data.
+      virtual CommonTime getInitialTime() const
+         throw()
+      { return initialTime; }
+
+      
+      /// Determine the latest time for which this object can successfully 
+      /// determine the Xvt for any satellite.
+      /// @return The final time
+      /// @throw InvalidRequest This is thrown if the object has no data.
+      virtual CommonTime getFinalTime() const
+         throw()
+      { return finalTime; }
+
+      virtual bool velocityIsPresent()
          const throw()
       {return true;}
 
-      bool clockIsPresent()
+      virtual bool clockIsPresent()
          const throw()
       {return true;}
 
-   }; // end class XvtStore
+      //---------------------------------------------------------------
+      // Below are interfaces that are unique to this class (i.e. not 
+      // in the parent class)
+      //---------------------------------------------------------------
+
+      /// Returns the health of an SV for a particular time
+      /// @param sat the satellite's SatID
+      /// @param t the time to look up
+      /// @return the SV health bits
+      /// @throw InvalidRequest no matching ephemeris found in the store
+      short getSatHealth(const SatID& sat, const CommonTime& t) const
+         throw( gpstk::InvalidRequest );
+
+
+      /// Add an EngEphemeris object to this collection.
+      /// @param eph the EngEphemeris to add
+      /// @return true if ephemeris was added, false otherwise
+      bool addEphemeris(const EngEphemeris& eph)
+         throw();
+
+      /// Remove EngEphemeris objects older than t.
+      /// @param t remove EngEphemeris objects older than this
+      void wiper(const CommonTime& t)
+         throw()
+      {edit(t);}
+      
+      /// Remove all data from this collection.   
+      void clear()
+         throw()
+      {edit(CommonTime::END_OF_TIME);}
+      
+      /** This returns the pvt of the sv in ecef coordinates
+       * (units m, s, m/s, s/s) at the indicated time.
+       * @param sat the satellite's SatID
+       * @param t the time to look up
+       * @param ref a place to return the IODC for future reference.
+       * @return the Xvt of the SV at time t
+       */
+      Xvt getXvt(const SatID& sat, const CommonTime& t, short& ref) const
+         throw( gpstk::InvalidRequest);
+
+      /// Get the number of EngEphemeris objects in this collection.
+      /// @return the number of EngEphemeris records in the map
+      unsigned ubeSize() const
+         throw();
+
+      unsigned size() const
+         throw()
+      { return ubeSize(); };
+      
+      /// Find an ephemeris based upon the search method configured
+      /// by SearchNear/SearchPast
+      /// @param sat SatID of satellite of interest
+      /// @param t time with which to search for ephemeris
+      /// @return a reference to the desired ephemeris
+      /// @throw InvalidRequest object thrown when no ephemeris is found
+      const EngEphemeris& findEphemeris(const SatID& sat, const CommonTime& t) const
+         throw( gpstk::InvalidRequest );
+
+      /// Find an ephemeris for the indicated satellite at time t. The ephemeris
+      /// is chosen to be the one that 1) is within the fit interval
+      /// for the given time of interest, and 2) is the last ephemeris
+      /// transmitted before the time of interest (i.e. min(toi - HOW time)).
+      /// @param sat the SV of interest
+      /// @param t the time of interest
+      /// @return a reference to the desired ephemeris
+      /// @throw InvalidRequest object thrown when no ephemeris is found
+      const EngEphemeris& findUserEphemeris(const SatID& sat, const CommonTime& t) const
+         throw( gpstk::InvalidRequest );
+
+      /// Find an ephemeris for the indicated satellite at time t. The ephemeris
+      /// chosen is the one with HOW time closest to the time t, (i.e. with
+      /// smallest fabs(t-HOW), but still within the fit interval.
+      /// @param sat the SV of interest
+      /// @param t the time of interest
+      /// @return a reference to desired ephemeris
+      /// @throw InvalidRequest object thrown when no ephemeris is found
+      const EngEphemeris& findNearEphemeris(const SatID& sat, const CommonTime& t) const
+         throw( gpstk::InvalidRequest );
+
+      /// Add all ephemerides to an existing list<EngEphemeris>.
+      /// @return the number of ephemerides added.
+      int addToList(std::list<EngEphemeris>& v) const
+         throw();
+
+      /// use findNearEphemeris() in the getSat...() routines
+      void SearchNear(void)
+         throw()
+      {method = 1;}
+
+      /// use findEphemeris() in the getSat...() routines (the default)
+      void SearchPast(void)
+         throw()
+      {method = 0;}
+
+      /// This is intended to just store sets of unique EngEphemerides
+      /// for a single SV.  The key is the Toe - 1/2 the fit interval.
+      typedef std::map<CommonTime, EngEphemeris> EngEphMap;
+      
+      /// Returns a map of the ephemerides available for the specified
+      /// satellite.  Note that the return is specifically chosen as a 
+      /// const reference.  The intent is to provide "read only" access
+      /// for analysis.  If the map needs to be modified, see other methods.
+      const EngEphMap& getEphMap( const SatID& sat ) const
+         throw( gpstk::InvalidRequest );
+
+      protected:
+
+      /// This is intended to hold all unique EngEphemerides for each SV
+      /// The key is the prn of the SV.
+      typedef std::map<short, EngEphMap> UBEMap;
+      
+      /// The map where all EngEphemerides are stored.
+      UBEMap ube;
+      
+      CommonTime initialTime; //< Time of the first EngEphemeris
+      CommonTime finalTime;   //< Time of the last EngEphemeris
+      
+      /// flag indicating search method (find...Eph) to use in getSatXvt 
+      ///  and getSatHealth
+      int method;
+
+   }; // end class
 
    //@}
 
