7378d66e9e0f98fcdfc988a924b775bcd2a568d8
Troy Varney <tvarney@whoknows.com>
2009-07-27 15:44:10 +0000

    Addition of a few more tests to the CommonTimeTests

--- branches/rinex3_dev/FunctionalTests/CommonTimeTests.hpp
+++ branches/rinex3_dev/FunctionalTests/CommonTimeTests.hpp
@@ -1,15 +1,6 @@
 #ifndef COMMON_TIME_TESTS_HPP
 #define COMMON_TIME_TESTS_HPP
 
-#include <iostream>
 
-#include "CommonTime.hpp"
-#include "TimeSystem.hpp"
-#include "Exception.hpp"
-
-bool addDaysTest();
-bool addMillisecondsTest();
-bool addSecondsDoubleTest();
-bool addSecondsLongTest();
 
 #endif
