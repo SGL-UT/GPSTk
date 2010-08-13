78cfb3079d43426be76f46d4f9f22e18bfa7709f
Allen Farris <afarris@whoknows.com>
2010-08-13 17:22:09 +0000

    refixing bug from yesterday, my last commit didnt properly fix it

--- dev/src/MSCData.cpp
+++ dev/src/MSCData.cpp
@@ -134,23 +134,13 @@ namespace gpstk
          epoch = asDouble(currentLine.substr(19, 7));
          frac = modf(epoch, &intg);
          doy = (short)(frac * SEC_YEAR / gpstk::DayTime::SEC_DAY);
-         sod = (short)((frac * SEC_YEAR) - (doy * gpstk::DayTime::SEC_DAY));
-         while(sod < 0) //DayTime errors if input sod is less than 0, thus correct
-         {
-           doy -= 1;
-           sod += gpstk::DayTime::SEC_DAY;
-         }
+         sod = (frac * SEC_YEAR) - (doy * gpstk::DayTime::SEC_DAY);
          refepoch = gpstk::DayTime((short)intg, doy+1, sod);
          
          epoch = asDouble(currentLine.substr(26, 7));
          frac = modf(epoch, &intg);
          doy = (short)(frac * SEC_YEAR / gpstk::DayTime::SEC_DAY);
          sod = (frac * SEC_YEAR) - (doy * gpstk::DayTime::SEC_DAY);
-         while(sod < 0) //DayTime errors if input sod is less than 0, thus correct
-         {
-           doy -= 1;
-           sod += gpstk::DayTime::SEC_DAY;
-         }
          effepoch = gpstk::DayTime((short)intg, doy+1, sod);
          
          coordinates[0] = asDouble(currentLine.substr(33, 12));
