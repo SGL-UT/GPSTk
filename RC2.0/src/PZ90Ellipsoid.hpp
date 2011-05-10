f643815217b4f7740d35e5374dbc4775442519d9
Dagoberto Salazar <architest@whoknows.com>
2011-05-10 23:10:48 +0000

    Further adjustments in 'PZ90Ellipsoid.hpp' to update it to PZ90.02 ellipsoid (as defined in table 3.2 of GLONASS ICD 2008, v5.1).

--- RC2.0/src/PZ90Ellipsoid.hpp
+++ RC2.0/src/PZ90Ellipsoid.hpp
@@ -38,7 +38,7 @@
 
 /**
  * @file PZ90Ellipsoid.hpp
- * PZ90 model of the Ellipsoid
+ * PZ90.02 model of the Ellipsoid (as defined in table 3.2 of ICD-2008, v5.1).
  */
 
 #ifndef GPSTK_PZ90ELLIPSOID_HPP
@@ -54,59 +54,65 @@ namespace gpstk
    class PZ90Ellipsoid : public EllipsoidModel
    {
    public:
-   		 ///Defined in table 3.2 of ICD-2002e
+
+   		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return semi-major axis of Earth in meters.
       virtual double a() const throw()
       { return 6378136.0; }
 		 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return semi-major axis of Earth in km.
       virtual double a_km() const throw()
       { return a() / 1000.0; }
 
          /**
-          * Defined in table 3.2 of the GLONASS ICD-2002e
+          * Defined in table 3.2 of the GLONASS ICD-2008 (v5.1)
+          * @return inverse o flattening (ellipsoid parameter).
+          */
+      virtual double flatteningInverse() const throw()
+      { return 298.25784; }
+
+         /**
+          * Computed from inverse flattening value as given in table 3.2
+          * of the GLONASS ICD-2008 (v5.1)
           * @return flattening (ellipsoid parameter).
           */
       virtual double flattening() const throw()
-      { return 3.35280374302e-3; }
+      { return 3.35280373518e-3; }
      
-      virtual double flatteningInverse() const throw()
-      { return 298.257839303; }
-
 	//The eccentricity and eccSquared values were computed from the
 	//flattening value via the formula:
-	//ecc2 = 1 - (1 - f)^2 = 2f - f2 
+	//ecc2 = 1 - (1 - f)^2 = f*(2.0 - f)
 	//ecc = sqrt(ecc2)
          /// @return eccentricity (ellipsoid parameter).
       virtual double eccentricity() const throw()
-      { return 8.19563833937e-2; }
+      { return 8.1819106432923e-2; }
 
          /// @return eccentricity squared (ellipsoid parameter).
       virtual double eccSquared() const throw()
-      { return 6.71684877898e-3; }
+      { return 6.69436617748e-3; }
 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return angular velocity of Earth in radians/sec.
       virtual double angVelocity() const throw()
       { return 7.292115e-5; }
 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return geocentric gravitational constant in m**3 / s**2
       virtual double gm() const throw()
       { return 398600.4418e9; }
 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return geocentric gravitational constant in km**3 / s**2
       virtual double gm_km() const throw()
-      { return 398600.4419; }
+      { return 398600.4418; }
 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return Speed of light in m/s.
       virtual double c() const throw()
       { return 299792458; }
 
-		 ///Defined in table 3.2 of ICD-2002e
+		 ///Defined in table 3.2 of ICD-2008 (v5.1)
          /// @return Speed of light in km/s
       virtual double c_km() const throw()
       { return c()/1000.0; }
@@ -114,10 +120,10 @@ namespace gpstk
       /// Destructor.
       virtual ~PZ90Ellipsoid() throw() {};
 
-   }; // class PZ90Ellipsoid
+   }; // End of class 'PZ90Ellipsoid'
 
    //@}
 
-} // namespace
+}  // End of namespace gpstk
 
-#endif
+#endif   // GPSTK_PZ90ELLIPSOID_HPP
