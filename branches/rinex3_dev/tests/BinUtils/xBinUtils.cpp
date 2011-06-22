c56ad4100eddf22a99fb8ff21262b96cb32c5fc8
Nelson Wu <nwu@whoknows.com>
2011-06-22 20:03:09 +0000

    Continuing merge of r3 and RC2.0 branches: updated r3 version of xBinUtils.cpp

--- branches/rinex3_dev/tests/BinUtils/xBinUtils.cpp
+++ branches/rinex3_dev/tests/BinUtils/xBinUtils.cpp
@@ -18,7 +18,7 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//  
+//
 //  Copyright 2009, The University of Texas at Austin
 //
 //============================================================================
@@ -29,15 +29,15 @@ CPPUNIT_TEST_SUITE_REGISTRATION (xBinUtils);
 
 
 void xBinUtils :: setUp (void)
-{ 
+{
 }
 
 void xBinUtils :: firstTest (void)
-{ 
+{
 	cout.setf(ios_base::hex,ios_base::basefield);
-	
-	unsigned char Store[12] = {0x11,0x33,0x77,0xFF,0xBB,0x22,0xDD,0x44,0x66,0xEE,0x99,0x66};
-	
+
+	unsigned char Store[16] = {0x11,0x33,0x77,0xFF,0xBB,0x22,0xDD,0x44,0x00,0x00,0xDD,0xAA,0x44,0x22,0xBB,0xAA};
+
 	char a;
 	unsigned char *ptra = (unsigned char *)& a;
 	for (int i = sizeof(char)-1;i>=0;i--)
@@ -45,7 +45,7 @@ void xBinUtils :: firstTest (void)
 		*ptra = Store[i];
 		*ptra++;
 	}
-	
+
 	short b;
 	unsigned char *ptrb = (unsigned char *)& b;
 	for (int i = sizeof(short)-1;i>=0;i--)
@@ -53,7 +53,7 @@ void xBinUtils :: firstTest (void)
 		*ptrb = Store[i];
 		*ptrb++;
 	}
-	
+
 	int c;
 	unsigned char *ptrc = (unsigned char *)& c;
 	for (int i = sizeof(int)-1;i>=0;i--)
@@ -61,7 +61,7 @@ void xBinUtils :: firstTest (void)
 		*ptrc = Store[i];
 		*ptrc++;
 	}
-	
+
 	long d;
 	unsigned char *ptrd = (unsigned char *)& d;
 	for (int i = sizeof(long)-1;i>=0;i--)
@@ -69,7 +69,7 @@ void xBinUtils :: firstTest (void)
 		*ptrd = Store[i];
 		*ptrd++;
 	}
-	
+
 	float e;
 	unsigned char *ptre = (unsigned char *)& e;
 	for (int i = sizeof(float)-1;i>=0;i--)
@@ -77,7 +77,7 @@ void xBinUtils :: firstTest (void)
 		*ptre = Store[i];
 		*ptre++;
 	}
-	
+
 	double f;
 	unsigned char *ptrf = (unsigned char *)& f;
 	for (int i = sizeof(double)-1;i>=0;i--)
@@ -85,7 +85,7 @@ void xBinUtils :: firstTest (void)
 		*ptrf = Store[i];
 		*ptrf++;
 	}
-	
+
 	long double g;
 	unsigned char *ptrg = (unsigned char *)& g;
 	for (int i = sizeof(long double)-1;i>=0;i--)
@@ -101,7 +101,7 @@ void xBinUtils :: firstTest (void)
 		*ptra1 = Store[i];
 		*ptra1++;
 	}
-	
+
 	short b1;
 	unsigned char *ptrb1 = (unsigned char *)& b1;
 	for (int i =0;i<sizeof(short);i++)
@@ -109,7 +109,7 @@ void xBinUtils :: firstTest (void)
 		*ptrb1 = Store[i];
 		*ptrb1++;
 	}
-	
+
 	int c1;
 	unsigned char *ptrc1 = (unsigned char *)& c1;
 	for (int i =0;i<sizeof(int);i++)
@@ -117,7 +117,7 @@ void xBinUtils :: firstTest (void)
 		*ptrc1 = Store[i];
 		*ptrc1++;
 	}
-	
+
 	long d1;
 	unsigned char *ptrd1 = (unsigned char *)& d1;
 	for (int i =0;i<sizeof(long);i++)
@@ -125,7 +125,7 @@ void xBinUtils :: firstTest (void)
 		*ptrd1 = Store[i];
 		*ptrd1++;
 	}
-	
+
 	float e1;
 	unsigned char *ptre1 = (unsigned char *)& e1;
 	for (int i =0;i<sizeof(float);i++)
@@ -133,7 +133,7 @@ void xBinUtils :: firstTest (void)
 		*ptre1 = Store[i];
 		*ptre1++;
 	}
-	
+
 	double f1;
 	unsigned char *ptrf1 = (unsigned char *)& f1;
 	for (int i =0;i<sizeof(double);i++)
@@ -141,7 +141,7 @@ void xBinUtils :: firstTest (void)
 		*ptrf1 = Store[i];
 		*ptrf1++;
 	}
-	
+
 	long double g1;
 	unsigned char *ptrg1 = (unsigned char *)& g1;
 	for (int i =0;i<sizeof(long double);i++)
@@ -149,7 +149,7 @@ void xBinUtils :: firstTest (void)
 		*ptrg1 = Store[i];
 		*ptrg1++;
 	}
-	
+
 	gpstk::BinUtils::twiddle(a);
 	gpstk::BinUtils::twiddle(b);
 	gpstk::BinUtils::twiddle(c);
@@ -157,7 +157,7 @@ void xBinUtils :: firstTest (void)
 	gpstk::BinUtils::twiddle(e);
 	gpstk::BinUtils::twiddle(f);
 	gpstk::BinUtils::twiddle(g);
-	
+
 	CPPUNIT_ASSERT_EQUAL(a1,a);
 	CPPUNIT_ASSERT_EQUAL(b1,b);
 	CPPUNIT_ASSERT_EQUAL(c1,c);
@@ -165,5 +165,5 @@ void xBinUtils :: firstTest (void)
 	CPPUNIT_ASSERT_DOUBLES_EQUAL(e1,e,1e-50);
 	CPPUNIT_ASSERT_DOUBLES_EQUAL(f1,f,1e-50);
 	CPPUNIT_ASSERT_DOUBLES_EQUAL(g1,g,1e-50);
-	
+
 }
