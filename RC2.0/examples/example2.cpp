19bf8f39c3d2e2d064ad0a93457fdff126c3fe91
Dagoberto Salazar <architest@whoknows.com>
2011-07-12 16:43:15 +0000

    Activated compilation of 'examples2'.

--- RC2.0/examples/example2.cpp
+++ RC2.0/examples/example2.cpp
@@ -35,23 +35,26 @@ using namespace gpstk;
 
 int main(void)
 {
-   // Create the input file stream
+
+      // Create the input file stream
    RinexObsStream rin("bahr1620.04o");
 
-   // Create the output file stream
+      // Create the output file stream
    RinexObsStream rout("bahr1620.04o.new", ios::out|ios::trunc);
 
-   // Read the RINEX header
-   RinexObsHeader head; //RINEX header object
+      // Read the RINEX header
+   RinexObsHeader head;    //RINEX header object
    rin >> head;
    rout.header = rin.header;
    rout << rout.header;
 
-   // Loop over all data epochs
-   RinexObsData data; //RINEX data object
-   while (rin >> data) {
+      // Loop over all data epochs
+   RinexObsData data;   //RINEX data object
+   while (rin >> data)
+   {
      rout << data;
    }
 
    exit(0);
+
 }
