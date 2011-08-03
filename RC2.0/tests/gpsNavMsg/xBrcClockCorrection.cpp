3384c2e0de01af5a2a011bf045d3383ba17e2629
Mark Mendiola <mendiolam@whoknows.com>
2011-08-03 15:52:54 +0000

    Adding updated tests for nav messages

--- RC2.0/tests/gpsNavMsg/xBrcClockCorrection.cpp
+++ RC2.0/tests/gpsNavMsg/xBrcClockCorrection.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
 *  $Id:$
 *
-*  Test program from June 2011.  Written to test the BrcKeplerOrbit 
+*  Test program from June 2011. Written to test the BrcClockCorrection 
 *  module as it was being developed.
 *
 *********************************************************************/
@@ -17,29 +17,29 @@ int main( int argc, char * argv[] )
 {
       // Set time to Day 153, 2011 (6/2/2011) at noon
    CivilTime g( 2011, 6, 2, 12, 14, 44.0, TimeSystem::GPS );
-   CommonTime dt = g.convertToCommonTime( );
+   CommonTime dt = g.convertToCommonTime();
 
       // Test data (copied from navdmp output for .....)
       // Generally, we'd load these data from the file
-   char SysID = 'G';
+   std::string SysID = "G";
    ObsID obsID( ObsID::otUndefined, ObsID::cbL1, ObsID::tcCA );
-   short PRNID =                    3;
-   double Toc =              388800.0;
-   short weeknum =               1638;     // By rules of Kepler Orbit, this must be week of Toc
-   double accuracy =            10.61;
-   bool healthy =                true;
-   double af0 =        7.23189674E-04;
-   double af1 =        5.11590770E-12;
-   double af2 =        0.00000000E+00;
+   short PRNID     = 3;
+   double Toc      = 388800.0;
+   short weeknum   = 1638;     // By rules of Clock Correction, this must be week of Toc
+   double accuracy = 10.61;
+   bool healthy    = true;
+   double af0      = 7.23189674E-04;
+   double af1      = 5.11590770E-12;
+   double af2      = 0.00000000E+00;
   
-      //Test Data copied from RINEX file	
-   double rToc =              388800.0;
-   short rweeknum =               1638;     // By rules of Kepler Orbit, this must be week of Toc
-   double raccuracy =            10.61;
-   bool rhealthy =                true;
-   double raf0 =        7.23189674318E-04;
-   double raf1 =        5.11590769747E-12;
-   double raf2 =        0.00000000000E+00;
+      // Test Data copied from RINEX file	
+   double rToc      = 388800.0;
+   short rweeknum   = 1638;     // By rules of Clock Corection, this must be week of Toc
+   double raccuracy = 10.61;
+   bool rhealthy    = true;
+   double raf0      = 7.23189674318E-04;
+   double raf1      = 5.11590769747E-12;
+   double raf2      = 0.00000000000E+00;
 
    long subframe1[10] = { 0x22C2663D, 0x1F0E29B8, 0x2664002B, 0x09FCC1B6, 0x0F60EB8A,
                           0x1299CE93, 0x29CD3DB6, 0x0597BB0F, 0x00000B68, 0x17B28E5C };
@@ -49,50 +49,48 @@ int main( int argc, char * argv[] )
                           0x35A74DFC, 0x065C8B0F, 0x1E4F400A, 0x3FE8966D, 0x05860C44 };
 
 
-      // First test case.  Create an empty CC object, then load the data.
+      // First test case. Create an empty CC object, then load the data.
    cout << "Test Case 1: Creating an empty CC object and loading the data." << endl;
    BrcClockCorrection co1;
    co1.loadData( SysID, obsID, PRNID, Toc, weeknum, accuracy, healthy, 
-		 af0, af1, af2 ); 
+		           af0, af1, af2 ); 
 
    double ClkCorr1 = co1.svClockBias( dt );
    cout.precision(11); 
-   cout << "Clock Correction co1: " << ClkCorr1 << endl;
+   cout << "Clock Correction co1: " << ClkCorr1 << endl << endl;
 
-      // Second test case.  Create an CC object with data available at time of construction.
+      // Second test case. Create an CC object with data available at time of construction.
    cout << "Test Case 2: Creating CC object with data." << endl;
    BrcClockCorrection co2( SysID, obsID, PRNID, Toc, weeknum, accuracy, healthy, 
-		 af0, af1, af2 ); 
+		                     af0, af1, af2 ); 
 
    double ClkCorr2 = co2.svClockBias( dt ); 
-   cout << "ClockCorrection co2: " << ClkCorr2 << endl;
+   cout << "ClockCorrection co2: " << ClkCorr2 << endl << endl;
 
-      // Third test case.  Create a CC object using raw legacy navigation message data
+      // Third test case. Create a CC object using raw legacy navigation message data
    cout << "Test Case 3: Creating CC object with raw legacy nav message data." << endl;
    BrcClockCorrection co3(obsID, PRNID, weeknum, subframe1 );
 
    double ClkCorr3 = co3.svClockBias( dt ); 
-   cout << "Clock Correction co3: " << ClkCorr3 << endl;
+   cout << "Clock Correction co3: " << ClkCorr3 << endl << endl;
 
-      // Fourth test case.  Create a CC object using raw legacy navigation message data
+      // Fourth test case. Create a CC object using raw legacy navigation message data
    cout << "Test Case 4: Creating CC object with raw legacy nav message data." << endl;
    BrcClockCorrection co4;
    co4.loadData(obsID, PRNID, weeknum, subframe1 );
 
    double ClkCorr4 = co4.svClockBias( dt ); 
-   cout << "Clock Correction co4: " << ClkCorr4 << endl;
+   cout << "Clock Correction co4: " << ClkCorr4 << endl << endl;
 
-
-      // Fifth test case.  Create an CC object with data available from RINEX file.
+      // Fifth test case. Create an CC object with data available from RINEX file.
    cout << "Test Case 5: Creating CC object with data from RINEX file." << endl;
    BrcClockCorrection co5( SysID, obsID, PRNID, rToc, rweeknum, raccuracy, rhealthy, 
-		 raf0, raf1, raf2 ); 
+		                     raf0, raf1, raf2 ); 
 
    double ClkCorr5 = co5.svClockBias( dt ); 
-   cout << "Clock Correction co5: " << ClkCorr5 << endl;
+   cout << "Clock Correction co5: " << ClkCorr5 << endl << endl;
 
-
-      // Sixth test case.  Compare against "classic" EngEphemeris
+      // Sixth test case. Compare against "classic" EngEphemeris
    cout << "Test Case 6: Calculated clock correction using 'classic' EngEphemeris." << endl;
    cout<< "Time= "<< g << endl;
    EngEphemeris EE;
@@ -101,8 +99,8 @@ int main( int argc, char * argv[] )
    EE.addSubframe(subframe3, weeknum, 3, 1);
 
    Xvt xvt = EE.svXvt(dt);
-   cout<< "Clock Bias EE:  " << xvt.clkbias <<endl;
-	cout<< "Clock Drift EE: " << xvt.clkdrift<<endl;
+   cout << "Clock Bias EE:  " << xvt.clkbias << endl;
+	cout << "Clock Drift EE: " << xvt.clkdrift << endl;
  
       // Test data (copied from navdmp output for PRN 6 Day 155, 2011)
    long subframeA1[10] = { 0x22C2663D, 0x30A2291C, 0x2664002B, 0x0DB9B68A, 0x12746316,
@@ -122,9 +120,8 @@ int main( int argc, char * argv[] )
    EEA.addSubframe(subframeA3, weeknum, 9, 1);
 
    xvt = EEA.svXvt(dt);
-   cout<< "Clock Bias EE: " << xvt.clkbias <<endl;
-   cout<< "Clock Drift EE: " << xvt.clkdrift <<endl;
-
+   cout << "Clock Bias EE:  "  << xvt.clkbias << endl;
+   cout << "Clock Drift EE: " << xvt.clkdrift << endl;
 
    CivilTime ct3(2011, 6, 5, 1, 0, 0.0, TimeSystem::GPS );
    dt = ct3.convertToCommonTime( );
@@ -132,16 +129,16 @@ int main( int argc, char * argv[] )
    cout << "Time = " << ct3 << endl;
 
    xvt = EEA.svXvt(dt);
-   cout<< "Clock Bias EE:  " << xvt.clkbias <<endl;
-   cout<< "Clock Drift EE: " << xvt.clkdrift <<endl; 
+   cout << "Clock Bias EE:  " << xvt.clkbias << endl;
+   cout << "Clock Drift EE: " << xvt.clkdrift << endl << endl; 
+ 
+   cout << "Dump Output for Test Cases 4 and 6." << endl;
 
-   cout << "Dump Output for Test Cases 4 and 6." <<endl;
+   cout << "dump EE:" << endl;
+   cout << EE << endl;
 
-   cout<<"dump EE:"<<endl;
-   cout<<EE<<endl;
-
-   cout<<"dump co4:"<<endl;
-   cout<<co4<<endl;
+   cout << "dump co4:" << endl;
+   cout << co4 << endl;
 
    return(0);
 }
