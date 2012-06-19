4814621f028b06c7b69b3ac71f3218d457a69eb4
Wei Yan <yanweigps@hotmail.com>
2012-06-19 23:53:23 +0000

    Fix an un-initialize error for msvc2010 on windows.

--- dev/src/Rinex3ObsHeader.cpp
+++ dev/src/Rinex3ObsHeader.cpp
@@ -1819,7 +1819,7 @@ namespace gpstk
       s << "Time of first obs "
          << printTime(firstObs,"%04Y/%02m/%02d %02H:%02M:%06.3f %P") << endl;
 
-      unsigned long allValid;
+      unsigned long allValid(allValid2);
       if     (version == 3.0)   allValid = allValid30;
       else if(version == 3.01)  allValid = allValid301;
 
