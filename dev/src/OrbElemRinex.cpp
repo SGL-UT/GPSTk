f100bc8476409799eb170a6d87ce6e5f4f66eff3
Jonathan Basden <jwbasden@whoknows.com>
2012-08-08 13:51:49 +0000

    Updated loadData methods to be compatible with Rinex3 data.

--- dev/src/OrbElemRinex.cpp
+++ dev/src/OrbElemRinex.cpp
@@ -56,22 +56,23 @@ namespace gpstk
    using namespace std;
   
    OrbElemRinex::OrbElemRinex()
-        :OrbElem(),
-         codeflags(0), health(0), L2Pdata(0),
-         accuracyValue(0.0),
-         IODC(0),
-         Tgd(0.0),
-         fitDuration(0) 
+      :OrbElem(),
+       codeflags(0), health(0), L2Pdata(0),
+       accuracyValue(0.0), IODC(0),
+       Tgd(0.0), fitDuration(0) 
    {}  
     
-
-  
-
    OrbElemRinex::OrbElemRinex( const RinexNavData& rinNav )
       throw( InvalidParameter )
    {
       loadData( rinNav );
    }
+
+   OrbElemRinex::OrbElemRinex( const Rinex3NavData& rinNav )
+     throw( InvalidParameter )
+   {
+      loadData( rinNav );
+   } 
 
      /// Clone method
    OrbElemRinex* OrbElemRinex::clone() const
@@ -81,6 +82,152 @@ namespace gpstk
 
    void OrbElemRinex::loadData( const RinexNavData& rinNav )
       throw( InvalidParameter )
+   {
+      // Fill in the variables unique to OrbElemFIC9
+      codeflags        = rinNav.codeflgs;
+      accuracyValue    = rinNav.accuracy; 
+      health           = rinNav.health;
+      IODC             = rinNav.IODC;
+      L2Pdata          = rinNav.L2Pdata;
+      Tgd              = rinNav.Tgd;
+
+      HOWtime        = rinNav.HOWtime;
+      fitDuration    = rinNav.fitint;
+
+      short fullXmitWeekNum    = rinNav.weeknum;
+
+         // Fill in the variables in the OrbElem parent
+	 // - - - First the simple copies - - -
+      double Toc     = rinNav.Toc;       // OrbElem only stores fully qualified times
+                                         // see notes below.
+      af0            = rinNav.af0;
+      af1            = rinNav.af1;
+      af2            = rinNav.af2;
+
+      Cuc            = rinNav.Cuc;
+      Cus            = rinNav.Cus;
+      Crc            = rinNav.Crc;
+      Crs            = rinNav.Crs;
+      Cic            = rinNav.Cic;
+      Cis            = rinNav.Cis;
+      
+                                         
+      double Toe     = rinNav.Toe;       // Not a member of OrbElem.  See notes below.
+      M0             = rinNav.M0;       // OrbElem only stores fully qualified times
+      dn             = rinNav.dn;       // see notes below.
+      ecc            = rinNav.ecc;
+      double AHalf   = rinNav.Ahalf;
+      OMEGA0         = rinNav.OMEGA0;
+      i0             = rinNav.i0;
+      w              = rinNav.w;
+      OMEGAdot       = rinNav.OMEGAdot;
+      idot           = rinNav.idot;
+
+      // - - - Now work on the things that need to be calculated - - -
+
+	 // The system is assumed (legacy navigation message is from GPS)
+      satID.id = static_cast<short>( rinNav.PRNID );
+
+         // The observation ID has a type of navigation, but the
+         // carrier and code types are undefined.  They could be
+         // L1/L2 C/A, P, Y,.....
+      obsID.type = ObsID::otNavMsg;
+      obsID.band = ObsID::cbUndefined;
+      obsID.code = ObsID::tcUndefined;
+
+	 // Beginning of Validity
+         // New concept.  Admit the following.
+	 //  (a.) The collection system may not capture the data at earliest transmit.
+	 //  (b.) The collection system may not capture the three SFs consecutively.
+	 // Consider a couple of IS-GPS-200 promises,
+	 //  (c.) By definition, beginning of validity == beginning of transmission.
+	 //  (d.) Except for uploads, cutovers will only happen on hour boundaries
+	 //  (e.) Cutovers can be detected by non-even Toc.
+	 //  (f.) Even uploads will cutover on a frame (30s) boundary.
+         // Therefore,
+	 //   1.) If Toc is NOT even two hour interval, pick lowest HOW time,
+	 //   round back to even 30s.  That's the earliest Xmit time we can prove.
+	 //   NOTE: For the case where this is the SECOND SF 1/2/3 after an upload, 
+	 //   this may yield a later time as such a SF 1/2/3 will be on a even 
+	 //   hour boundary.  Unfortunately, we have no way of knowing whether
+	 //   this item is first or second after upload without additional information.
+	 //   2.) If Toc IS even two hour interval, pick time from SF 1, 
+	 //   round back to nearest EVEN two hour boundary.  This assumes collection
+	 //   SOMETIME in first hour of transmission.  Could be more
+	 //   complete by looking at fit interval and IODC to more accurately 
+	 //   determine earliest transmission time. 
+      long longToc = (long) Toc;
+      double XmitSOW = 0.0; 
+      if ( (longToc % 7200) != 0)     // NOT an even two hour change
+      {
+         long Xmit = HOWtime - (HOWtime % 30);
+	 XmitSOW = (double) Xmit;
+      }
+      else
+      {
+         long Xmit = HOWtime - HOWtime % 7200;
+	 XmitSOW = (double) Xmit; 
+      }
+      beginValid = GPSWeekSecond( fullXmitWeekNum, XmitSOW, TimeSystem::GPS ); 
+
+      // Determine Transmit Time
+      // Transmit time is the actual time this
+      // SF 1/2/3 sample was collected	 
+      long Xmit = HOWtime - (HOWtime % 30);
+      transmitTime = GPSWeekSecond( fullXmitWeekNum, (double)Xmit, TimeSystem::GPS );
+
+         // Fully qualified Toe and Toc
+	 // As broadcast, Toe and Toc are in GPS SOW and do not include
+	 // the GPS week number.  OrbElem (rightly) insists on having a
+	 // Toe and Toc in CommonTime objects which implies determining
+	 // the week number. 
+      double timeDiff = Toe - XmitSOW;
+      short epochWeek = fullXmitWeekNum;
+      if (timeDiff < -HALFWEEK) epochWeek++;
+      else if (timeDiff > HALFWEEK) epochWeek--;
+
+      ctToc = GPSWeekSecond(epochWeek, Toc, TimeSystem::GPS);
+      ctToe = GPSWeekSecond(epochWeek, Toe, TimeSystem::GPS);
+
+	 // End of Validity.  
+	 // The end of validity is calculated from the fit interval
+	 // and the Toe.  The fit interval is either trivial
+	 // (if fit interval flag==0, fit interval is 4 hours) 
+	 // or a look-up table based on the IODC. 
+      short fitHours = getLegacyFitInterval(IODC, fitDuration);
+      long endFitSOW = Toe + (fitHours/2)*3600;
+      short endFitWk = epochWeek;
+      if (endFitSOW >= FULLWEEK)
+      {
+         endFitSOW -= FULLWEEK;
+         endFitWk++;
+      }
+      endValid = GPSWeekSecond(endFitWk, endFitSOW, TimeSystem::GPS);   
+
+	 // Semi-major axis and time-rate-of-change of semi-major axis
+	 //    Note: Legacy navigation message (SF 1/2/3) used SQRT(A).
+	 //    The CNAV and CNAV-2 formats use deltaA and Adot.  As a 
+	 //    result, OrbElem uses A and Adot and SQRT(A) and deltaA
+	 //    are converted to A at runtime.
+      A = AHalf * AHalf;
+      Adot = 0.0; 
+         // Legacy nav doesn't have Rate of Chagen to corerction to mean motion, 
+	 // so set it to zero.
+      dndot = 0.0;   
+
+         // Health
+         // OrbElemFIC9 stores the full 8 bits health from the legacy
+	 // navigation message.  OrElemn only stores the true/false, 
+	 // use/don't use based on whether the 8 bit health is 0 or non-zero
+      healthy = (healthy==0);
+
+         // After all this is done, declare that data has been loaded
+	 // into this object (so it may be used). 
+      dataLoadedFlag = true;    
+   }
+
+   void OrbElemRinex::loadData( const Rinex3NavData& rinNav )
+     throw( InvalidParameter )
    {
       // Fill in the variables unique to OrbElemFIC9
       codeflags        = rinNav.codeflgs;
