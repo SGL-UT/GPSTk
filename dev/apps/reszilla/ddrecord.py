7886992a5a731ded3d44e7b7556697df38c62a5c
Jon C Little <littlej@arlut.utexas.edu>
2013-01-10 16:17:56 +0000

    Adding ability to parse stats records

--- dev/apps/reszilla/ddrecord.py
+++ dev/apps/reszilla/ddrecord.py
@@ -28,10 +28,61 @@ class DDRecord(object):
             if len(line)==0: continue
             if line[0] =='#' and line[0:6] != "# Arc:": continue
 
-            for rec in [DDres(), DDcs(), DDarc()]:
+            for rec in [DDres(), DDcs(), DDarc(), DDstat()]:
                 if rec.parse(line):
                     yield rec
 
+class DDstat(DDRecord):
+    """Stores the values from a statistics line output of ddGen"""
+    sre = re.compile(">s +"
+                     "(?P<carrier>\S+) +"
+                     "(?P<code>\S+) +"
+                     "(?P<otype>\S+) +"
+                     "(?P<el1>\d{1,2})-"
+                     "(?P<el2>\d{1,2}) +"
+                     "(?P<mad>[0-9.]+) +"
+                     "(?P<median>[0-9-+.e]+) +"
+                     "(?P<dde>\d+) +"
+                     "(?P<sve>\d+) +"
+                     "(?P<kurt>[0-9.]+) +"
+                     "(?P<jumps>\d+)")
+
+    def __init__(self, time=None, carrier=None, code=None, otype=None, el1=None, el2=None,
+                 mad=None, median=None, dde=None, sve=None, kurt=None, jumps=None):
+        self.carrier = carrier # L1, L2, L5, etc
+        self.code = code       # GPSC/A, GPSY, ...
+        self.otype = otype     # obs type (pseudorange, phase, doppler)
+        self.el1 = el1         # elevation of the reference SV, or start of statistic bin, degrees
+        self.el2 = el2         # elevation of the target SV, or end of statistic bin, degrees
+        self.mad = mad
+        self.median = median
+        self.dde = dde
+        self.sve = sve
+        self.kurt = kurt
+        self.jumps = jumps
+
+    def __str__(self):
+        s = DDRecord.__str__(self)
+        return "stats: {} {:2d}-{:2d} {} {}".format(
+            s, self.el1, self.el2, self.mad, self.dde)
+
+    def parse(self, line):
+        c = DDstat.sre.match(line)
+        if c is None:
+            return False
+        self.carrier = c.group('carrier')
+        self.code = c.group('code')
+        self.otype = c.group('otype')
+        self.el1 = int(c.group('el1'))
+        self.el2 = int(c.group('el2'))
+        self.mad = float(c.group('mad'))
+        self.median = float(c.group('median'))
+        self.dde = int(c.group('dde'))
+        self.sve = int(c.group('sve'))
+        self.kurt = float(c.group('kurt'))
+        self.jumps = int(c.group('jumps'))
+        return True
+#
 
 class DDres(DDRecord):
     """Stores the values from a raw residual line output of ddGen"""
@@ -130,16 +181,17 @@ class DDarc(DDRecord):
                         "bias:(?P<bias>\-?\d+\.\d+) +"
                         "(sdev:(?P<sdev>\d+\.\d+))?")
 
-    def __init__(self, t0=None, t1=None, sv1=None, sv2=None, bias=None):
+    def __init__(self, t0=None, t1=None, sv1=None, sv2=None, bias=None, sdev=None):
         self.t0 = t0
         self.t1 = t1
         self.sv1 = sv1
         self.sv2 = sv2
         self.bias = bias
+        self.sdev = sdev
 
     def __str__(self):
         s = DDRecord.__str__(self)
-        return "bias:{} {}".format(s, self.bias)
+        return "bias:{} {} {}".format(s, self.bias, self.sdev)
 
     def parse(self, line):
         c = DDarc.cre.match(line)
@@ -162,7 +214,7 @@ class DDarc(DDRecord):
 if __name__ == "__main__":
     import sys
     import argparse
-    parser = argparse.ArgumentParser(description="Plots the output of ddGen.")
+    parser = argparse.ArgumentParser(description="Reads the output of ddGen.")
 
     parser.add_argument("input_file", metavar='fn', nargs='?', type=argparse.FileType('r'), default=sys.stdin,
                         help="Input data file, defaults to stdin.",)
