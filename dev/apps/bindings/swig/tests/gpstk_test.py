ef0998fb896d4d444f4734779928a02cf5940b82
Sean Lewis <slewis@arlut.utexas.edu>
2013-09-10 08:32:51 -0500

    Added python binding test runner.

---
+++ dev/apps/bindings/swig/tests/gpstk_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import unittest
+from gpstk_time_test import *
+from gpstk_util_test import *
+from gpstk_store_test import *
+
+
+if __name__ == '__main__':
+    unittest.main()
