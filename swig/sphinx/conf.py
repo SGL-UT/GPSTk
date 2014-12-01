a5ae360e048c838ce83e859b570fa0f996541545
Jason Vestuto <vestuto@arlut.utexas.edu>
2014-12-01 16:15:28 -0600

    Python bindings, documentation, fixed copyright date in conf.py

--- swig/sphinx/conf.py
+++ swig/sphinx/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'The GPS Toolkit'
-copyright = u'2013, ARL:UT'
+copyright = u'2014, ARL:UT'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
