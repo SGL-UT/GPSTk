df67f1cb2e2766ddce5ef4a8d6da36ac982f80e5
Nick Fitzsimmons <nfitz@hpub5.arlut.utexas.edu>
2015-01-09 15:55:59 -0600

    Update for TestUtil.hpp to include the three extra methods to change a string in the output and the passTest and failTest methods. ANSITime_T.cpp has been updated to reflect these changes.

--- core/tests/TimeHandling/ANSITime_T.cpp
+++ core/tests/TimeHandling/ANSITime_T.cpp
@@ -21,27 +21,33 @@ class ANSITime_T
 	int  initializationTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "Constructor(time,TimeSystem)", __FILE__, __func__ );
+		testFramework.init();
 
 	  	ANSITime Compare(13500000,TimeSystem(2)); //Initialize an object
 //--------------ANSITime_initializationTest_1 - Was the time value set to expectation?
 		testFramework.assert(13500000 == (int)Compare.time);
+		testFramework.next();
 
 //--------------ANSITime_initializationTest_2 - Was the time system set to expectation?
 		testFramework.assert(TimeSystem(2) == Compare.getTimeSystem());
+		testFramework.next();
 
 		testFramework.changeSourceMethod("Constructor(ANSITime)");
 		ANSITime Copy(Compare); // Initialize with copy constructor
 //--------------ANSITime_initializationTest_3 - Was the time value set to expectation?
 		testFramework.assert(13500000 == (int)Copy.time);
+		testFramework.next();
 
 //--------------ANSITime_initializationTest_4 - Was the time system set to expectation?
 		testFramework.assert(TimeSystem(2) == Copy.getTimeSystem());
+		testFramework.next();
 
 		testFramework.changeSourceMethod("= Operator");
 		ANSITime Assigned;
 		Assigned = Compare;
 //--------------ANSITime_initializationTest_5 - Was the time value set to expectation?
 		testFramework.assert(13500000 == (int)Assigned.time);
+		testFramework.next();
 
 //--------------ANSITime_initializationTest_6 - Was the time system set to expectation?
 		testFramework.assert(TimeSystem(2) == Assigned.getTimeSystem());
@@ -54,6 +60,7 @@ class ANSITime_T
 	int setFromInfoTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "setFromInfo", __FILE__, __func__ );
+		testFramework.init();
 
 		ANSITime setFromInfo1;
 		ANSITime setFromInfo2;
@@ -63,13 +70,16 @@ class ANSITime_T
 		Id['P'] = "GPS";
 //--------------ANSITime_setFromInfoTest_1 - Does a proper setFromInfo work with all information provided?
 		testFramework.assert(setFromInfo1.setFromInfo(Id));
+		testFramework.next();
 
 //--------------ANSITime_setFromInfoTest_2 - Did the setFromInfo set the proper values?
 		testFramework.assert(Compare == setFromInfo1);
+		testFramework.next();
 
 		Id.erase('K');
 //--------------ANSITime_setFromInfoTest_3 - Does a proper setFromInfo work with missing information?
 		testFramework.assert(setFromInfo2.setFromInfo(Id));
+		testFramework.next();
 
 //--------------ANSITime_setFromInfoTest_4 - Did the previous setFromInfo set the proper values?
 		testFramework.assert(Compare2 == setFromInfo2);		
@@ -82,6 +92,7 @@ class ANSITime_T
 	int operatorTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "== Operator", __FILE__, __func__ );
+		testFramework.init();
 
 		ANSITime Compare(13500000); // Initialize with value
 		ANSITime LessThan(13400000); // Initialize with value
@@ -91,53 +102,68 @@ class ANSITime_T
 
 //--------------ANSITime_operatorTest_1 - Are equivalent objects equivalent?
 		testFramework.assert(Compare == CompareCopy);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_2 - Are equivalent objects equivalent?
 		testFramework.assert(!(Compare == LessThan));
+		testFramework.next();
 
 		testFramework.changeSourceMethod("!= Operator");
 //--------------ANSITime_operatorTest_3 - Are non-equivalent objects not equivalent?
 		testFramework.assert(Compare != LessThan);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_4 - Are non-equivalent objects not equivalent?
 		testFramework.assert(!(Compare != Compare));
+		testFramework.next();
 
 		testFramework.changeSourceMethod("< Operator");
 //--------------ANSITime_operatorTest_5 - Does the < operator function when left_object < right_object?
 		testFramework.assert(LessThan < Compare);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_6 - Does the < operator function when left_object > right_object?
 		testFramework.assert(!(Compare < LessThan));
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_7 - Does the < operator function when left_object = right_object?
 		testFramework.assert(!(Compare < CompareCopy));
+		testFramework.next();
 
 		testFramework.changeSourceMethod("> Operator");
 //--------------ANSITime_operatorTest_8 - Does the > operator function when left_object < right_object?
 		testFramework.assert(!(LessThan > Compare));
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_9 - Does the > operator function when left_object > right_object?
 		testFramework.assert(Compare > LessThan);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_10 - Does the > operator function when left_object = right_object?
 		testFramework.assert(!(Compare > CompareCopy));
+		testFramework.next();
 
 		testFramework.changeSourceMethod("<= Operator");
 //--------------ANSITime_operatorTest_11 - Does the <= operator function when left_object < right_object?
 		testFramework.assert(LessThan <= Compare);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_12 - Does the <= operator function when left_object > right_object?
 		testFramework.assert(!(Compare <= LessThan));
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_13 - Does the <= operator function when left_object = right_object?
 		testFramework.assert(Compare <= CompareCopy);
+		testFramework.next();
 
 		testFramework.changeSourceMethod(">= Operator");
 //--------------ANSITime_operatorTest_14 - Does the >= operator function when left_object < right_object?
 		testFramework.assert(!(LessThan >= Compare));
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_15 - Does the >= operator function when left_object > right_object?
 		testFramework.assert(Compare >= LessThan);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_16 - Does the >= operator function when left_object = right_object?
 		testFramework.assert(Compare >= CompareCopy);
@@ -149,12 +175,14 @@ class ANSITime_T
 	int  resetTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "reset", __FILE__, __func__ );
+		testFramework.init();
 
 	  	ANSITime Compare(13500000,TimeSystem(2)); //Initialize an object
 
 	  	Compare.reset(); // Reset it
 //--------------ANSITime_operatorTest_1 - Was the time value reset to expectation?
 		testFramework.assert(0 == (int)Compare.time);
+		testFramework.next();
 
 //--------------ANSITime_operatorTest_2 - Was the time system reset to expectation?
 		testFramework.assert(TimeSystem(0) == Compare.getTimeSystem());
@@ -166,14 +194,17 @@ class ANSITime_T
 	int  toFromCommonTimeTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "isValid", __FILE__, __func__ );
+		testFramework.init();
 
 	  	ANSITime Compare(13500000,TimeSystem(2)); //Initialize an object
 
 //--------------ANSITime_toFromCommonTimeTest_1 - Is the time after the BEGINNING_OF_TIME?
   		testFramework.assert(Compare.convertToCommonTime() > CommonTime::BEGINNING_OF_TIME);
+		testFramework.next();
 
 //--------------ANSITime_toFromCommonTimeTest_2 - Is the set object vaild?
 		testFramework.assert(Compare.isValid());
+		testFramework.next();
 
   		CommonTime Test = Compare.convertToCommonTime(); //Convert to
 
@@ -183,9 +214,11 @@ class ANSITime_T
 		testFramework.changeSourceMethod("CommonTime Conversion");
 //--------------ANSITime_toFromCommonTimeTest_3 - Is the result of conversion the same?
 		testFramework.assert(Test2 == Compare);
+		testFramework.next();
 
 //--------------ANSITime_toFromCommonTimeTest_4 - Is the time system after conversion what is expected?
 		testFramework.assert(Compare.getTimeSystem()==TimeSystem(2));
+		testFramework.next();
 
 //--------------ANSITime_toFromCommonTimeTest_5 - Is the time after conversion what is expected?
 		testFramework.assert(13500000 == (int)Compare.time);
@@ -197,6 +230,7 @@ class ANSITime_T
 	int  timeSystemTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "Differing TimeSystem == Operator", __FILE__, __func__ );
+		testFramework.init();
 
   		ANSITime GPS1(13500000,TimeSystem(2));
   		ANSITime GPS2(13400000,TimeSystem(2));
@@ -206,37 +240,42 @@ class ANSITime_T
 
 //--------------ANSITime_timeSystemTest_1 - Verify same Time System but different time inequality
 		testFramework.assert(!(GPS1 == GPS2));
+		testFramework.next();
 
 //--------------ANSITime_timeSystemTest_2 - Verify same Time System equality
 		testFramework.assert(GPS1.getTimeSystem() == GPS2.getTimeSystem());
+		testFramework.next();
 
 		testFramework.changeSourceMethod("Differing TimeSystem != Operator");
 //--------------ANSITime_timeSystemTest_3 - Verify different Time System but same time inequality
 		testFramework.assert(GPS1 != UTC1);
+		testFramework.next();
 
 //--------------ANSITime_timeSystemTest_4 - Verify different Time System but same time inequality
 		testFramework.assert(GPS1 != UNKNOWN);
-
-//--------------ANSITime_timeSystemTest_5 - Verify different Time System but same time inequality
-		testFramework.assert(GPS1 != UNKNOWN);
+		testFramework.next();
 
 		testFramework.changeSourceMethod("ANY TimeSystem == Operator");		
-//--------------ANSITime_timeSystemTest_6 - Verify TimeSystem=ANY does not matter in TimeSystem=GPS comparisons 
+//--------------ANSITime_timeSystemTest_5 - Verify TimeSystem=ANY does not matter in TimeSystem=GPS comparisons 
 		testFramework.assert(GPS1 == ANY);
+		testFramework.next();
 
-//--------------ANSITime_timeSystemTest_7 - Verify TimeSystem=ANY does not matter in TimeSystem=UTC comparisons 
+//--------------ANSITime_timeSystemTest_6 - Verify TimeSystem=ANY does not matter in TimeSystem=UTC comparisons 
 		testFramework.assert(UTC1 == ANY);
+		testFramework.next();
 
-//--------------ANSITime_timeSystemTest_8 - Verify TimeSystem=ANY does not matter in TimeSystem=UNKOWN comparisons 
+//--------------ANSITime_timeSystemTest_7 - Verify TimeSystem=ANY does not matter in TimeSystem=UNKOWN comparisons 
 		testFramework.assert(UNKNOWN == ANY);
+		testFramework.next();
 
 		testFramework.changeSourceMethod("ANY TimeSystem < Operator");	
-//--------------ANSITime_timeSystemTest_9 - Verify TimeSystem=ANY does not matter in other operator comparisons 
+//--------------ANSITime_timeSystemTest_8 - Verify TimeSystem=ANY does not matter in other operator comparisons 
 		testFramework.assert(!(GPS2 == ANY) && (GPS2 < ANY));
+		testFramework.next();
 
 		testFramework.changeSourceMethod("setTimeSystem");	
   		UNKNOWN.setTimeSystem(TimeSystem(2)); //Set the Unknown TimeSystem
-//--------------ANSITime_timeSystemTest_10 - Ensure resetting a Time System changes it
+//--------------ANSITime_timeSystemTest_9 - Ensure resetting a Time System changes it
 		testFramework.assert(UNKNOWN.getTimeSystem()==TimeSystem(2));
 
 		return testFramework.countFails();
@@ -245,19 +284,23 @@ class ANSITime_T
 	int  printfTest (void)
 	{
 		TestUtil testFramework( "ANSITime", "printf", __FILE__, __func__ );
+		testFramework.init();
 
   		ANSITime GPS1(13500000,TimeSystem(2));
   		ANSITime UTC1(13500000,TimeSystem(7));
 
 //--------------ANSITime_printfTest_1 - Verify printed output matches expectation
 		testFramework.assert(GPS1.printf("%08K %02P") == (std::string)"13500000 GPS");
+		testFramework.next();
 
 //--------------ANSITime_printfTest_2 - Verify printed output matches expectation
 		testFramework.assert(UTC1.printf("%08K %02P") == (std::string)"13500000 UTC");
+		testFramework.next();
 
 		testFramework.changeSourceMethod("printError");	
 //--------------ANSITime_printfTest_3 - Verify printed error message matches expectation
 		testFramework.assert(GPS1.printError("%08K %02P") == (std::string)"ErrorBadTime ErrorBadTime");
+		testFramework.next();
 
 //--------------ANSITime_printfTest_4 - Verify printed error message matches expectation
 		testFramework.assert(UTC1.printError("%08K %02P") == (std::string)"ErrorBadTime ErrorBadTime");
