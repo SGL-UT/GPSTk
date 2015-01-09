d2fe483298160b217e490519e6fb3a1f8545f254
Jason Vestuto <vestuto@arlut.utexas.edu>
2015-01-09 13:47:22 -0600

    Updates to TimeUtil class, including the following:
    - Fixed constructor class initilization and defaults.
    - Added pass() to have an explict interface element to call for passing tests.
    - Added testCount to store number of tests run.
    - Added countTests() to return testCount.
    - Modified determination of subtestID to depend explicitly on testCount.
    - Modified init() to also initialize testCount and tolerance.
    - Replaced unfail() with undo() to undo results of a call to pass() or fail().
    - Replaced explicit int2str conversion in print() with implicit << conversion.
    - Added call to print() in assert()

--- core/lib/TestFramework/TestUtil.hpp
+++ core/lib/TestFramework/TestUtil.hpp
@@ -41,32 +41,37 @@ class TestUtil
 public:
 
   //----------------------------------------
-  // Method:  TestUtil
+  // Method:  TestUtil::TestUtil()
   // Purpose: Constructor
   // Usage:   To be called at the start of each test method
-  // Inputs:  4
-  //          source_class  = the name of the source class being tested
+  // Inputs:  source_class  = the name of the source class being tested
   //          source_method = the name of the source method being tested
-  //          test_file     = the name of file containing the test code 
-  //          test_method   = the name of test method doing the testing
+  //          test_file     = the name of file containing the test code, e.g., __FILE__
+  //          test_method   = the name of test method doing the testing, e.g. __func__
   // Outputs: none
   //----------------------------------------
-  TestUtil( std::string const& source_class, 
-            std::string const& source_method,
-            std::string const& test_file,
-            std::string const& test_method  )
+  TestUtil( std::string const& sourceClassInput  = "Unknown", 
+            std::string const& sourceMethodInput = "Unknown",
+            std::string const& testFileInput     = "Unknown",
+            std::string const& testMethodInput   = "Unknown"
+           ):
+      outputKeyword( "TestOutput" ), 
+      sourceClass( sourceClassInput  ),
+      sourceMethod( sourceMethodInput ),
+      testFilename( testFileInput ),
+      testMethod( testMethodInput ),
+      failBit( 0 ),
+      failCount( 0 ),
+      testCount( 0 ),
+      subtestID( 1 ),
+      tolerance( 0 )
   {
-    sourceClass   = source_class;
-    sourceMethod  = source_method;
-    testFilename  = test_file;
-    testMethod    = test_method;
 
-    init();
   }
 
   //----------------------------------------
-  // Method:  init
-  // Purpose: Reset the failbit, failCount, and method subtest ID
+  // Method:  TestUtil::init()
+  // Purpose: Reset the failbit, failCount, testCount, and subtestID
   // Usage:   To be called at the beginning of the FIRST subtest in a method
   //          never after that.
   // Inputs:  none
@@ -76,24 +81,26 @@ public:
   {
     failBit   = 0;
     failCount = 0;
+    testCount = 0;
     subtestID = 1;
+    tolerance = 0;
   }
 
   //----------------------------------------
-  // Method:  next()
-  // Purpose: Increment the failCount and test method subtest ID
-  // Usage:   To be called at the beginning of each subtest AFTER the first.
+  // Method:  TestUtil::next()
+  // Purpose: Increment the failCount and reset subtestID based on current testCount
+  // Usage:   To be called at the beginning of each subtest AFTER the first
   // Inputs:  none
   // Outputs: none
   //----------------------------------------
   void next( void )
   {
     failBit = 0;
-    subtestID++;
+    subtestID = countTests() + 1;
   }
 
   //----------------------------------------
-  // Method:  fail()
+  // Method:  TestUtil::fail()
   // Purpose: Fail the test! Record a failure by setting the failBit and
   //           incrementing failCount
   // Usage:   To be called (once!) at the end of any subtest that has failed
@@ -104,24 +111,50 @@ public:
   {
     failBit = 1;
     failCount++;
+    testCount++;
   }
 
   //----------------------------------------
-  // Method:  unfail()
-  // Purpose: unFail the test! Undo a failure by unsetting failBit and
-  //            decrementing failCount
+  // Method:  TestUtil::pass()
+  // Purpose: Pass the test! Record a pass by setting the failBit=0 and
+  //           incrementing the testCount
+  // Usage:   To be called (once!) at the end of any subtest that has passed
+  // Inputs:  none
+  // Outputs: none
+  //----------------------------------------
+  void pass( void )
+  {
+    failBit = 0;
+    testCount++;
+  }
+
+  //----------------------------------------
+  // Method:  TestUtil::undo()
+  // Purpose: undo the test! Undo a pass/fail by unsetting failBit and
+  //           decrementing failCount (only if failed) and 
+  //           decrementing the testCount
   // Usage:   To be called at the end of any subtest that needs to be undone
   // Inputs:  none
   // Outputs: none
   //----------------------------------------
-  void unfail( void )
+  void undo( void )
   {
-    failBit = 0;
-    failCount--;
+    if( failBit==1 )
+      { 
+         failBit = 0;
+         failCount--;
+         testCount--;
+      }
+    else
+      {
+         failBit = 0;
+         testCount--;
+      }
+    next();
   }
 
   //----------------------------------------
-  // Method:  print()
+  // Method:  TestUtil::print()
   // Purpose: print test results and information on classes being tested
   //          to stdout in a common format that is both human-readable
   //          and easy to filter using tools like grep so as to help
@@ -132,31 +165,25 @@ public:
   // STDOUT:  "outputKeyword, sourceClass, sourceMethod, testFilename,
   //             testMethod, subtestID, failBit"
   //----------------------------------------
-  void print( )
+  void print( void )
   {
-
-      // some C++ implementations do not include the std:to_string() method
-      std::ostringstream ss;
-      ss << failBit;
-      std::string failString = ss.str();
-
       // print test description and result to stdout
       std::cout     <<
-      outputKeyword << ", " << \
-      sourceClass   << ", " << \
-      sourceMethod  << ", " << \
-      testFilename  << ", " << \
-      testMethod    << ", " << \
-      subtestID     << ", " << \
-      failString    << std::endl;
+      outputKeyword << ", " << 
+      sourceClass   << ", " << 
+      sourceMethod  << ", " << 
+      testFilename  << ", " << 
+      testMethod    << ", " <<
+      subtestID     << ", " << 
+      failBit       << std::endl;     // implicit conversion from int to string
   }
 
   //----------------------------------------
-  // Method:  assert()
-  // Purpose: Takes a boolean expression, sets fail is assertion is false
-  // Usage:   To be called during any test that needs a bollean expression tested.
-  // Inputs:  1
-  //          boolean testExpression
+  // Method:  TestUtil::assert()
+  // Purpose: Takes a boolean expression, passes or fails the test, depending on
+  //          whether the assertion is true or false, and then prints the result
+  // Usage:   Use when a test can be written as boolean expression.
+  // Inputs:  boolean testExpression
   // Outputs: none
   //----------------------------------------
   void assert( bool testExpression )
@@ -165,50 +192,57 @@ public:
       {
         fail();
       }
+    else
+      {
+        pass();
+      }
+    print();
   }
 
   //----------------------------------------
-  // Method:  countFails()
-  // Purpose: Return the total fail count, the value of failCount
+  // Method:  TestUtil::countFails()
+  // Purpose: Return the number of tests that have failed so far
   // Usage:   To be called at the end of the test method and AFTER all tests
   // Inputs:  none
-  // Outputs: 1
-  //          int failCount
+  // Outputs: int failCount
   //----------------------------------------
-  int countFails( )
+  int countFails( void )
   {
     return( failCount );
   }
 
   //----------------------------------------
-  // Method:  countTests()
-  // Purpose: Return the total test count, the value of last subtestID
+  // Method:  TestUtil::countTests()
+  // Purpose: Return the number of tests that have been run so far
   // Usage:   To be called at the end of the test method and AFTER all tests
   // Inputs:  none
-  // Outputs: 1
-  //          int testCount
+  // Outputs: int testCount
   //----------------------------------------
-  int countTests( )
+  int countTests( void )
   {
-    return( subtestID );
+    return( testCount );
   }
 
 private:
 
   // The following are all used as part of the output from TestUtil::print()
-  // to facilitate filtering of output that is thus printed to stdout.
-  std::string outputKeyword = "TestOutput"; // Identifies a stdout line as from this class.
-  std::string sourceClass   = "Unknown";    // help locate the source class causing a test failure.
-  std::string sourceMethod  = "Unknown";    // help locate the source method causing a test failure.
-  std::string testFilename  = "Unknown";    // help locate the test file that discovered a failure.
-  std::string testMethod    = "Unknown";    // help locate the test method that discovered a failure.
-  int         subtestID     = 1;            // since single test methods may contain multiple subtests.
-  int         failBit       = 0;            // store the result of a test (0=pass, 1=fail)
+  // to facilitate filtering of output that is thus printed to stdout
 
-  // Other stuff
-  double tolerance = 0;
-  int failCount    = 0;
-  // int testCount    = 0;
-  std::ostringstream testOutputStream; // Printed out to screen, but why bother since
-                                  //  we just use std::cout everywhere?
+  std::string outputKeyword; // Identifies a stdout line as from this class
+  std::string sourceClass;   // help locate source class causing a test failure
+  std::string sourceMethod;  // help locate source method causing a test failure
+  std::string testFilename;  // help locate test file that discovered a failure
+  std::string testMethod;    // help locate test method that discovered a failure
+  std::string testLine;      // help locate line number where the test code lives
+  int         failBit;       // store the result of a test (0=pass, 1=fail)
+
+  double      tolerance;     // acceptable difference between test output and 
+                             //  expected or baseline output
+
+  //  since single test methods may contain multiple subtests.
+
+  int subtestID; // ID of the current sub-test, used in TestUtil::print()
+  int failCount; // Count of tests that have fails
+  int testCount; // Count of tests that have been run
+
 };
