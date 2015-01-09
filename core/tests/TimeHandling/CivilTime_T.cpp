71109d71ad113e8a8958a5f4980dd3768e46863d
Bryan Parsons <bparsons@arlut.utexas.edu>
2015-01-09 17:35:12 -0600

    Updated CivilTime_T to utilize new TestUtils class, as well as minor fixes.

--- core/tests/TimeHandling/CivilTime_T.cpp
+++ core/tests/TimeHandling/CivilTime_T.cpp
@@ -18,7 +18,7 @@ class CivilTime_T
 	/* Test to ensure the values in the constructor go to their intended locations */
 	int  initializationTest (void)
 	{
-		TestUtil testFramework( "ANSITime", "Constructor(time,TimeSystem)", __FILE__, __func__ );
+		TestUtil testFramework( "CivilTime", "Constructor(time,TimeSystem)", __FILE__, __func__ );
 	  	CivilTime Compare(2008,8,21,13,30,15.); //Initialize an object
 
 //--------------CivilTime_initializationTest_1 - Was the time system set to expectation?
@@ -97,6 +97,8 @@ class CivilTime_T
 	// Test will check if CivilTime variable can be set from a map.
 	int setFromInfoTest (void)
 	{
+		TestUtil testFramework( "CivilTime", "setFromInfo", __FILE__, __func__ );
+
 		//Create several objects for testing
 		CivilTime setFromInfo1;
 		CivilTime setFromInfo2;
@@ -114,44 +116,51 @@ class CivilTime_T
 		Id['S'] = "00";
 		Id['P'] = "GPS";
 
-		//Can a CivilTime object be set with b,d,Y,H,M,S,P options?
-		if (!(setFromInfo1.setFromInfo(Id))) return 1;
+//--------------CivilTime_setFromInfoTest_1 - Can a CivilTime object be set with b,d,Y,H,M,S,P options?
+		testFramework.assert(setFromInfo1.setFromInfo(Id));
 
-		//Is the set object what is expected?
-    		CivilTime Check(2008,12,31,12,0,0,TimeSystem::GPS);
-    		if (setFromInfo1 != Check) return 2; 
+    	CivilTime Check(2008,12,31,12,0,0,TimeSystem::GPS);
+//--------------CivilTime_setFromInfoTest_2 - Is the set object what is expected?
+		testFramework.assert(setFromInfo1 == Check); 
 
 		Id.erase('b');
 		Id.erase('Y');
 		Id['m'] = "12";
 		Id['y'] = "06";
 
-		//Can a CivilTime object be set with d,m,y,H,M,S,P options?
-		if (!(setFromInfo2.setFromInfo(Id))) return 2;
-    		CivilTime Check2(2006,12,31,12,0,0,TimeSystem::GPS);
-    		if (setFromInfo2 != Check2) return 3; 
+//--------------CivilTime_setFromInfoTest_3 - Can a CivilTime object be set with d,m,y,H,M,S,P options?
+		testFramework.assert(setFromInfo2.setFromInfo(Id));
+    	
+    	CivilTime Check2(2006,12,31,12,0,0,TimeSystem::GPS);
+
+//--------------CivilTime_setFromInfoTest_4 - Is the set object what is expected?
+    	testFramework.assert(setFromInfo2 == Check2); 
+		
 		Id.erase('y');
 		Id['y'] = "006";
 
-		//Can a CivilTime object be set with a 3 digit year?
-		// Answer should be no. 'y' option is for 2 digit years.
-		if (!(setFromInfo3.setFromInfo(Id))) return 4;
-    		if (setFromInfo3 == Check2) return 5; 
-		//Can a CivilTime object be set without a year?
+//--------------CivilTime_setFromInfoTest_5 - Can a CivilTime object be set with a 3 digit year? Answer should be no. 'y' option is for 2 digit years.
+		testFramework.assert(setFromInfo3.setFromInfo(Id));
+//--------------CivilTime_setFromInfoTest_6 - Is the set object what is expected?
+    	testFramework.assert(setFromInfo3 != Check2); 
+
 		Id.erase('y');
-		if (!(setFromInfo4.setFromInfo(Id))) return 6;
+//--------------CivilTime_setFromInfoTest_7 - Can a CivilTime object be set without a year?
+		testFramework.assert(setFromInfo4.setFromInfo(Id));
 		Id.erase('m');
 		Id['b'] = "AAA";
-		//Can a CivilTime object be set with an improper month?
-		if (setFromInfo5.setFromInfo(Id)) return 7;
+//--------------CivilTime_setFromInfoTest_8 - Can a CivilTime object be set with an improper month?
+		testFramework.assert(!(setFromInfo5.setFromInfo(Id)));
 
-		return 0;
+		return testFramework.countFails();
 	}
 
 	/* Test will check if the ways to initialize and set an CivilTime object.
 	   Test also tests whether the comparison operators and isValid method function. */
 	int operatorTest (void)
 	{
+		TestUtil testFramework( "CivilTime", "== Operator", __FILE__, __func__ );
+		
 		CivilTime Zero; //Use default constructor
 
 		CivilTime Aug21(2008,8,21,13,30,15.); //Reference time
@@ -167,61 +176,104 @@ class CivilTime_T
 
 		CivilTime Aug21Copy2 = Aug21Copy; //Use the = set operator.
 		Aug21Copy2 = Aug21Copy;
-		//Equality Assertion
-		if (!(Aug21 == Aug21Copy)) return 1;
-		//Non-equality Assertion
-		if (!(Aug21 != LessThanYear)) return 2;
-		//Less than assertions
-		if (!(LessThanYear < Aug21)) return 3;
-		if ((Aug21 < LessThanYear)) return 4;
-		if (!(LessThanMonth < Aug21)) return 5;
-		if ((Aug21 < LessThanMonth)) return 6;
-		if (!(LessThanDay < Aug21)) return 7;
-		if ((Aug21 < LessThanDay)) return 8;
-		if (!(LessThanHour < Aug21)) return 9;
-		if ((Aug21 < LessThanHour)) return 10;
-		if (!(LessThanMinute < Aug21)) return 11;
-		if ((Aug21 < LessThanMinute)) return 12;
-		if (!(LessThanSecond < Aug21)) return 13;
-		if ((Aug21 < LessThanSecond)) return 14;
-		//Greater than assertions
-		if (!(Aug21 > LessThanYear)) return 15;
-		//Less than equals assertion
-		if (!(LessThanYear <= Aug21)) return 16;
-		if (!(Aug21Copy <= Aug21)) return 17;
-		//Greater than equals assertion
-		if (!(Aug21 >= LessThanYear)) return 18;
-		if (!(Aug21 >= Aug21Copy)) return 19;
-		// isValid assertions
-		if (!(Aug21.isValid())) return 20;
-		if ((Zero.isValid())) return 21;
-		return 0;
+//--------------CivilTime_operatorTest_1 - Are equivalent objects equivalent?
+		testFramework.assert(Aug21 == Aug21Copy);
+
+		testFramework.changeSourceMethod("!= Operator");
+//--------------CivilTime_operatorTest_2 - Are non-equivalent objects equivalent?
+		testFramework.assert(Aug21 != LessThanYear);
+
+		testFramework.changeSourceMethod("< Operator");
+//--------------CivilTime_operatorTest_3 - Does the < operator function when left_year < right_year?
+		testFramework.assert(LessThanYear < Aug21);
+//--------------CivilTime_operatorTest_4 - Does the < operator function when left_year > right_year?
+		testFramework.assert(!(Aug21 < LessThanYear);
+//--------------CivilTime_operatorTest_5 - Does the < operator function when left_month < right_month?
+		testFramework.assert(LessThanMonth < Aug21);
+//--------------CivilTime_operatorTest_6 - Does the < operator function when left_month > right_month?
+		testFramework.assert(!(Aug21 < LessThanMonth));
+//--------------CivilTime_operatorTest_7 - Does the < operator function when left_day < right_day?
+		testFramework.assert(LessThanDay < Aug21);
+//--------------CivilTime_operatorTest_8 - Does the < operator function when left_day > right_day?
+		testFramework.assert(!(Aug21 < LessThanDay));
+//--------------CivilTime_operatorTest_9 - Does the < operator function when left_hour < right_hour?
+		testFramework.assert(LessThanHour < Aug21);
+//--------------CivilTime_operatorTest_10 - Does the < operator function when left_hour > right_hour?
+		testFramework.assert(!(Aug21 < LessThanHour));
+//--------------CivilTime_operatorTest_11 - Does the < operator function when left_minute < right_minute?
+		testFramework.assert(LessThanMinute < Aug21);
+//--------------CivilTime_operatorTest_12 - Does the < operator function when left_minute > right_minute?
+		testFramework.assert(!(Aug21 < LessThanMinute));
+//--------------CivilTime_operatorTest_13 - Does the < operator function when left_second < right_second?
+		testFramework.assert(LessThanSecond < Aug21);
+//--------------CivilTime_operatorTest_14 - Does the < operator function when left_second > right_second?
+		testFramework.assert(!(Aug21 < LessThanSecond));
+
+		testFramework.changeSourceMethod("> Operator");
+//--------------CivilTime_operatorTest_15 - Does the > operator function when left_year > right_year?
+		testFramework.assert(Aug21 > LessThanYear);
+
+		testFramework.changeSourceMethod("<= Operator");
+//--------------CivilTime_operatorTest_16 - Does the <= operator function when left_year < right_year?
+		testFramework.assert(LessThanYear <= Aug21);
+//--------------CivilTime_operatorTest_17 - Does the <= operator function when left_year = right_year?
+		testFramework.assert(Aug21Copy <= Aug21);
+
+		testFramework.changeSourceMethod(">= Operator");
+//--------------CivilTime_operatorTest_18 - Does the >= operator function when left_year > right_year?
+		testFramework.assert(Aug21 >= LessThanYear);
+//--------------CivilTime_operatorTest_19 - Does the >= operator function when left_year = right_year?
+		testFramework.assert(Aug21 >= Aug21Copy);
+
+//--------------CivilTime_operatorTest_20 - Is the set object valid?
+		testFramework.assert(Aug21.isValid());
+//--------------CivilTime_operatorTest_21 - Is the default constructor set object valid?
+		testFramework.assert(!(Zero.isValid()));
+
+		return testFramework.countFails();
 	}
 
 	/* Test will check converting to/from CommonTime. */
 	int  toFromCommonTimeTest (void)
 	{
+  		TestUtil testFramework( "CivilTime", "isValid", __FILE__, __func__ );
+
   		CivilTime Aug21(2008,8,21,13,30,15.,TimeSystem::GPS);
 
 		// Perform comparisons to start of CommonTime
   		//if (GPS1.convertToCommonTime() < CommonTime::BEGINNING_OF_TIME) return 11;
   		//if (CommonTime::BEGINNING_OF_TIME > GPS1) return 12;
 
+//--------------CivilTime_toFromCommonTimeTest_1 - Is the time after the BEGINNING_OF_TIME?
+  		testFramework.assert(Aug21.convertToCommonTime() > CommonTime::BEGINNING_OF_TIME);
+
+//--------------CivilTime_toFromCommonTimeTest_2 - Is the set object valid?
+		testFramework.assert(Aug21.isValid());
+  		
   		CommonTime Test = Aug21.convertToCommonTime();
 
   		CivilTime Test2;
   		Test2.convertFromCommonTime(Test);
 
-  		if (!(Test2 == Aug21)) return 1; // Converting to then from yields original
+		testFramework.changeSourceMethod("CommonTime Conversion");
+//--------------CivilTime_toFromCommonTimeTest_3 - Is the result of conversion the same?
+		testFramework.assert(Test2 == Aug21);
+//--------------CivilTime_toFromCommonTimeTest_4 - Is the time system after conversion what is expected?
+		testFramework.assert(Aug21.getTimeSystem()==TimeSystem(2));
+//--------------CivilTime_toFromCommonTimeTest_5 - Is the year after conversion what is expected?
+  		testFramework.assert(2008 == (int)Aug21.year);
+//--------------CivilTime_toFromCommonTimeTest_6 - Is the month after conversion what is expected?
+  		testFramework.assert(8 == (int)Aug21.month);
+//--------------CivilTime_toFromCommonTimeTest_7 - Is the day after conversion what is expected?
+  		testFramework.assert(21 == (int)Aug21.day);
+//--------------CivilTime_toFromCommonTimeTest_8 - Is the hour after conversion what is expected?
+  		testFramework.assert(13 == (int)Aug21.hour);
+//--------------CivilTime_toFromCommonTimeTest_9 - Is the minute after conversion what is expected?
+  		testFramework.assert(30 == (int)Aug21.minute);
+//--------------CivilTime_toFromCommonTimeTest_10 - Is the second after conversion what is expected?
+  		testFramework.assert(15 == (int)Aug21.second);		
 
-  		if (!(Aug21.getTimeSystem()==TimeSystem(2))) return 2; // Recheck TimeSystem
-  		if (2008 != (int)Aug21.year) return 3; // Recheck year value
-  		if (8 != (int)Aug21.month) return 4; // Recheck month value
-  		if (21 != (int)Aug21.day) return 5; // Recheck day value
-  		if (13 != (int)Aug21.hour) return 6; // Recheck hour value
-  		if (30 != (int)Aug21.minute) return 7; // Recheck minute value
-  		if (15 != (int)Aug21.second) return 8; // Recheck second value		
-		return 0;
+		return testFramework.countFails();
 	}
 
 
@@ -229,62 +281,96 @@ class CivilTime_T
 	/* Test will check the reset method. */
 	int resetTest (void)
 	{
+		TestUtil testFramework( "CivilTime", "reset", __FILE__, __func__ );
 		//Initialize a time
   		CivilTime Aug21(2008,8,21,13,30,15.,TimeSystem::GPS);
 
   		Aug21.reset();
-  		if (!(Aug21.getTimeSystem()==TimeSystem(0))) return 8; // Check TimeSystem
-  		if (0 != (int)Aug21.year) return 9; // Check year value reset
-  		if (1 != (int)Aug21.month) return 10; // Check month value reset
-  		if (1 != (int)Aug21.day) return 11; // Check day value reset
-  		if (0 != (int)Aug21.hour) return 12; // Check hour value reset
-  		if (0 != (int)Aug21.minute) return 13; // Check minute value reset
-  		if (0 != (int)Aug21.second) return 14; // Check second value reset	
-		return 0;
+//--------------CivilTime_operatorTest_1 - Was the time system reset to expectation?
+  		testFramework.assert(Aug21.getTimeSystem()==TimeSystem(0));
+//--------------CivilTime_operatorTest_2 - Was the year reset to expectation?
+  		testFramework.assert(0 == (int)Aug21.year);
+//--------------CivilTime_operatorTest_3 - Was the month reset to expectation?
+  		testFramework.assert(1 == (int)Aug21.month);
+//--------------CivilTime_operatorTest_4 - Was the day reset to expectation?
+  		testFramework.assert(1 == (int)Aug21.day);
+//--------------CivilTime_operatorTest_5 - Was the hour reset to expectation?
+  		testFramework.assert(0 == (int)Aug21.hour);
+//--------------CivilTime_operatorTest_6 - Was the minute reset to expectation?
+  		testFramework.assert(0 == (int)Aug21.minute);
+//--------------CivilTime_operatorTest_7 - Was the second reset to expectation?
+  		testFramework.assert(0 == (int)Aug21.second);
+
+		return testFramework.countFails();
 	}
 
 	/* Test to check the TimeSystem comparisons when using the comparison operators. */
 	int timeSystemTest (void)
 	{
+  		TestUtil testFramework( "CivilTime", "Differing TimeSystem == Operator", __FILE__, __func__ );
+
   		CivilTime GPS1(   2008,8,21,13,30,15.,TimeSystem::GPS);
   		CivilTime GPS2(   2005,8,21,13,30,15.,TimeSystem::GPS); // Time is less than the others
   		CivilTime UTC1(   2008,8,21,13,30,15.,TimeSystem::UTC);
   		CivilTime UNKNOWN(2008,8,21,13,30,15.,TimeSystem::Unknown);
   		CivilTime ANY(    2008,8,21,13,30,15.,TimeSystem::Any);
 
-  		if (GPS1 == GPS2) return 1; // GPS1 and GPS2 should have different times
-  		if (GPS1.getTimeSystem() != GPS2.getTimeSystem()) return 2; // Should have the same time system
-  		if (GPS1 == UTC1) return 3; //Should have different time systems
-  		if (GPS1 == UNKNOWN) return 4;
-		
-		// Make TimeSystem part not matter and perform comparisons
-		// which solely depend on the time value.
-  		if (GPS1 != ANY) return 5; 
-  		if (UTC1 != ANY) return 6;
-  		if (UNKNOWN != ANY) return 7;
-  		if (GPS2 == ANY) return 8;
-  		if (GPS2 > GPS1) return 9;
-  		if (GPS2 > ANY) return 10;
+//--------------CivilTime_timeSystemTest_1 - Verify same Time System but different time inequality
+		testFramework.assert(!(GPS1 == GPS2));
+//--------------CivilTime_timeSystemTest_2 - Verify same Time System equality
+		testFramework.assert(GPS1.getTimeSystem() == GPS2.getTimeSystem());
 
+		testFramework.changeSourceMethod("Differing TimeSystem != Operator");
+//--------------CivilTime_timeSystemTest_3 - Verify different Time System but same time inequality
+		testFramework.assert(GPS1 != UTC1);
+
+//--------------CivilTime_timeSystemTest_4 - Verify different Time System but same time inequality
+		testFramework.assert(GPS1 != UNKNOWN);
+		
+		testFramework.changeSourceMethod("ANY TimeSystem == Operator");		
+//--------------CivilTime_timeSystemTest_5 - Verify TimeSystem=ANY does not matter in TimeSystem=GPS comparisons 
+		testFramework.assert(GPS1 == ANY);
+//--------------CivilTime_timeSystemTest_6 - Verify TimeSystem=ANY does not matter in TimeSystem=UTC comparisons 
+		testFramework.assert(UTC1 == ANY);
+//--------------CivilTime_timeSystemTest_7 - Verify TimeSystem=ANY does not matter in TimeSystem=UNKOWN comparisons 
+		testFramework.assert(UNKNOWN == ANY);
+
+		testFramework.changeSourceMethod("ANY TimeSystem < Operator");	
+//--------------CivilTime_timeSystemTest_8 - Verify TimeSystem=ANY does not matter in other operator comparisons 
+		testFramework.assert(!(GPS2 == ANY) && (GPS2 < ANY));
+
+		testFramework.changeSourceMethod("setTimeSystem");	
   		UNKNOWN.setTimeSystem(TimeSystem(2)); //Set the Unknown TimeSystem
-  		if (UNKNOWN.getTimeSystem()!=TimeSystem(2)) return 13;
-		return 0;
+//--------------CivilTime_timeSystemTest_9 - Ensure resetting a Time System changes it
+		testFramework.assert(UNKNOWN.getTimeSystem()==TimeSystem(2));
+
+		return testFramework.countFails();
+
 	}
 
 	int printfTest (void)
 	{
+		TestUtil testFramework( "CivilTime", "printf", __FILE__, __func__ );
+
   		CivilTime GPS1(2008,8,21,13,30,15.,TimeSystem::GPS);
   		CivilTime UTC1(2008,8,21,13,30,15.,TimeSystem::UTC);
 
-  		if (GPS1.printf("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") !=
-                       (std::string)"2008 08 08 Aug 21 13 30 15 15.000000 GPS") return 1;
-  		if (UTC1.printf("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") !=
-                       (std::string)"2008 08 08 Aug 21 13 30 15 15.000000 UTC") return 2;
-  		if (GPS1.printError("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") !=
-                       (std::string)"ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime") return 3;
-  		if (UTC1.printError("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") !=
-                       (std::string)"ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime")return 4;
-		return 0;
+//--------------CivilTime_printfTest_1 - Verify printed output matches expectation
+		testFramework.assert("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") ==
+                       (std::string)"2008 08 08 Aug 21 13 30 15 15.000000 GPS");
+//--------------CivilTime_printfTest_2 - Verify printed output matches expectation
+		testFramework.assert("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") ==
+                       (std::string)"2008 08 08 Aug 21 13 30 15 15.000000 UTC");
+
+		testFramework.changeSourceMethod("printError");	
+//--------------CivilTime_printfTest_3 - Verify printed error message matches expectation
+		testFramework.assert(GPS1.printError("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") ==
+                       (std::string)"ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime");
+//--------------CivilTime_printfTest_4 - Verify printed error message matches expectation
+		testFramework.assert("%04Y %02y %02m %02b %02d %02H %02M %02S %02f %02P") ==
+                       (std::string)"ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime ErrorBadTime");
+		
+		return testFramework.countFails();
 	}
 
 	private:
