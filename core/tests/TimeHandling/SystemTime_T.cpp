07d3afb2ae529005fdc4bd85687a960f4cfc7b42
Nick Fitzsimmons <nfitz@hpub5.arlut.utexas.edu>
2015-01-28 15:06:14 -0600

    Adding the test for SystemTime.

--- core/tests/TimeHandling/SystemTime_T.cpp
+++ core/tests/TimeHandling/SystemTime_T.cpp
@@ -35,17 +35,73 @@
 //=============================================================================
 
 #include "TestUtil.hpp"
+#include "SystemTime.hpp"
+#include "ANSITime.hpp"
+#include "CommonTime.hpp"
+#include "UnixTime.hpp"
 #include <iostream>
+#include <ctime>
 
-class SystemTimeTest
+using namespace std;
+using namespace gpstk;
+
+class SystemTime_T
 {
         public: 
-		SystemTimeTest(){}// Default Constructor, set the precision value
-		~SystemTimeTest() {} // Default Desructor
+		SystemTime_T(){}// Default Constructor, set the precision value
+		~SystemTime_T() {} // Default Desructor
+
+		/* SystemTime inherits from UnixTime. The only added ability is to read the time from the machine.
+                   This one test will read the system time and compare it with the ctime value. However, since
+                   this test will need to read the time twice no guarantees can be made for strict tolerances.
+                   In fact, the tolerance will be set so that the time will be accurate to the second only. */
+		int obtainTimeFromSystemTest ( void )
+		{
+			TestUtil testFramework( "CommonTime", "set", __FILE__, __LINE__ );
+			testFramework.init();
+
+			long day, day2;
+			long sod, sod2;
+			double fsod, fsod2;
+			
+			SystemTime timeFrom_SystemTime;
+			time_t t;
+			time( &t );
+			ANSITime   timeFrom_ctime( t );
+
+			CommonTime commonTime_SystemTime = timeFrom_SystemTime.convertToCommonTime();
+			CommonTime commonTime_ctime      = timeFrom_ctime.convertToCommonTime();
+
+			commonTime_SystemTime.get(day,sod,fsod);
+			commonTime_ctime.get(day2,sod2,fsod2);
+
+//--------------SystemTime_ _1 - Is the day value accurate?
+			testFramework.assert( day == day2 );
+			testFramework.next();
+
+//--------------SystemTime_ _2 - Is the second value accurate?
+			testFramework.assert( sod == sod2 );
+			testFramework.next();
+
+//--------------SystemTime_ _3 - Is the TimeSystem set to UTC
+			testFramework.assert( commonTime_SystemTime.getTimeSystem() == TimeSystem(5) );
+			testFramework.next();
+
+			return testFramework.countFails();
+		}
+
 };
 
 
 int main() //Main function to initialize and run all tests above
 {
-	return 0; //Return the total number of errors
+	int check, errorCounter = 0;
+	SystemTime_T testClass;
+
+	check = testClass.obtainTimeFromSystemTest();
+	errorCounter += check;
+
+	std::cout << "Total Failures for " << __FILE__ << ": " << errorCounter << std::endl;
+
+	return errorCounter; //Return the total number of errors
 }
