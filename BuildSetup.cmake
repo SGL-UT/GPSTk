6d9e08b290c11053637b5c931465dd6a3b14bf03
Jason Vestuto <vestuto@arlut.utexas.edu>
2015-03-11 10:26:25 -0500

    Build, additional debug message() calls added to the BuildSetup
    - Added more platform and compiler specific debug messages.
    - Repositioned all message() calls in file after values are tested.

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -1,33 +1,6 @@
 
-
-
-#----------------------------------------
-# Debug Messages
-#
-# The CMake command-line tool, message( STATUS ) displays 
-# messages on stdout and all other message types on stderr. 
-#----------------------------------------
-
 if( DEBUG_SWITCH )
     message( STATUS "DEBUG: Included CMake file BuildSetup.cmake" )
-    message( STATUS "DEBUG: CMAKE_SYSTEM             = ${CMAKE_SYSTEM}" )      # e.g., Linux-3.2.0
-    message( STATUS "DEBUG: CMAKE_SYSTEM_NAME        = ${CMAKE_SYSTEM_NAME}" ) # e.g., Linux
-    message( STATUS "DEBUG: CMAKE_COMMAND            = ${CMAKE_COMMAND}" )     # e.g., /usr/bin/cmake
-    message( STATUS "DEBUG: CMAKE_VERSION            = ${CMAKE_VERSION}" )     # e.g., 2.8.9
-    message( STATUS "DEBUG: CMAKE_BUILD_TOOL         = ${CMAKE_BUILD_TOOL}" )  # e.g., /usr/bin/make
-    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_DIR   = ${CMAKE_CURRENT_LIST_DIR}" )    # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_FILE  = ${CMAKE_CURRENT_LIST_FILE}" )   # e.g., $HOME/git/gpstk/CMakeLists.txt
-    message( STATUS "DEBUG: CMAKE_SOURCE_DIR         = ${CMAKE_SOURCE_DIR}" )          # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_SOURCE_DIR = ${CMAKE_CURRENT_SOURCE_DIR}" )  # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_BINARY_DIR = ${CMAKE_CURRENT_BINARY_DIR}" )  # e.g., $HOME/git/gpstk/build
-    message( STATUS "DEBUG: PROJECT_NAME             = ${PROJECT_NAME}" )              # e.g., gpstk
-    message( STATUS "DEBUG: PROJECT_BINARY_DIR       = ${PROJECT_BINARY_DIR}" )        # e.g., $HOME/git/gpstk/build
-    message( STATUS "DEBUG: PROJECT_SOURCE_DIR       = ${PROJECT_SOURCE_DIR}" )        # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_INSTALL_DIR        = ${CMAKE_INSTALL_DIR}" )         # e.g., /usr
-    message( STATUS "DEBUG: CMAKE_INSTALL_PREFIX     = ${CMAKE_INSTALL_PREFIX}" )      # e.g., $HOME/.local/gpstk
-    if( BUILD_PYTHON )
-        message( STATUS "DEBUG: PYTHON_INSTALL_PREFIX    = ${PYTHON_INSTALL_PREFIX}" )     # e.g., $HOME/.local
-    endif()
 endif()
 
 #----------------------------------------
@@ -97,6 +70,47 @@ if( "${isSystemDir}" STREQUAL "-1" )
    set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}:$ORIGIN/../lib" )
 endif( "${isSystemDir}" STREQUAL "-1" )
 
+
+#----------------------------------------
+# Debug Messages
+#
+# The CMake command-line tool, message( STATUS ) displays 
+# messages on stdout and all other message types on stderr. 
+#----------------------------------------
+
+if( DEBUG_SWITCH )
+    message( STATUS "DEBUG: CMAKE_SYSTEM              = ${CMAKE_SYSTEM}" )      # e.g., Linux-3.2.0
+    message( STATUS "DEBUG: CMAKE_SYSTEM_NAME         = ${CMAKE_SYSTEM_NAME}" ) # e.g., Linux
+    message( STATUS "DEBUG: UNIX                      = ${UNIX}" ) # e.g., is the platform UNIX ?
+    message( STATUS "DEBUG: APPLE                     = ${APPLE}" ) # e.g., is the platform OSX?
+    message( STATUS "DEBUG: WIN32                     = ${WIN32}" ) # e.g., is the platform Windows?
+    message( STATUS "DEBUG: MSVC11                    = ${MSVC11}" ) # e.g., is the platform VisualStudio?
+    message( STATUS "DEBUG: MSVC12                    = ${MSVC12}" ) # e.g., is the platform VisualStudio?
+    message( STATUS "DEBUG: CMAKE_COMMAND             = ${CMAKE_COMMAND}" )     # e.g., /usr/bin/cmake
+    message( STATUS "DEBUG: CMAKE_VERSION             = ${CMAKE_VERSION}" )     # e.g., 2.8.9
+    message( STATUS "DEBUG: CMAKE_BUILD_TOOL          = ${CMAKE_BUILD_TOOL}" )  # e.g., /usr/bin/make
+    message( STATUS "DEBUG: STADYN                    = ${STADYN}" )
+    message( STATUS "DEBUG: CMAKE_COMPILER_IS_GNUCC   = ${CMAKE_COMPILER_IS_GNUCC}" )
+    message( STATUS "DEBUG: CMAKE_CXX_FLAGS           = ${CMAKE_CXX_FLAGS}" )
+    message( STATUS "DEBUG: CMAKE_SHARED_LINKER_FLAGS = ${CMAKE_SHARED_LINKER_FLAGS}" )
+    message( STATUS "DEBUG: CMAKE_SKIP_BUILD_RPATH    = ${CMAKE_SKIP_BUILD_RPATH}" )
+    message( STATUS "DEBUG: CMAKE_INSTALL_RPATH       = ${CMAKE_INSTALL_RPATH}" )
+    message( STATUS "DEBUG: CMAKE_BUILD_WITH_INSTALL_RPATH = ${CMAKE_BUILD_WITH_INSTALL_RPATH}" )
+    message( STATUS "DEBUG: CMAKE_INSTALL_RPATH_USE_LINK_PATH = ${CMAKE_INSTALL_RPATH_USE_LINK_PATH}" )
+    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_DIR   = ${CMAKE_CURRENT_LIST_DIR}" )    # e.g., $HOME/git/gpstk
+    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_FILE  = ${CMAKE_CURRENT_LIST_FILE}" )   # e.g., $HOME/git/gpstk/CMakeLists.txt
+    message( STATUS "DEBUG: CMAKE_SOURCE_DIR         = ${CMAKE_SOURCE_DIR}" )          # e.g., $HOME/git/gpstk
+    message( STATUS "DEBUG: CMAKE_CURRENT_SOURCE_DIR = ${CMAKE_CURRENT_SOURCE_DIR}" )  # e.g., $HOME/git/gpstk
+    message( STATUS "DEBUG: CMAKE_CURRENT_BINARY_DIR = ${CMAKE_CURRENT_BINARY_DIR}" )  # e.g., $HOME/git/gpstk/build
+    message( STATUS "DEBUG: PROJECT_NAME             = ${PROJECT_NAME}" )              # e.g., gpstk
+    message( STATUS "DEBUG: PROJECT_BINARY_DIR       = ${PROJECT_BINARY_DIR}" )        # e.g., $HOME/git/gpstk/build
+    message( STATUS "DEBUG: PROJECT_SOURCE_DIR       = ${PROJECT_SOURCE_DIR}" )        # e.g., $HOME/git/gpstk
+    message( STATUS "DEBUG: CMAKE_INSTALL_DIR        = ${CMAKE_INSTALL_DIR}" )         # e.g., /usr
+    message( STATUS "DEBUG: CMAKE_INSTALL_PREFIX     = ${CMAKE_INSTALL_PREFIX}" )      # e.g., $HOME/.local/gpstk
+    if( BUILD_PYTHON )
+        message( STATUS "DEBUG: PYTHON_INSTALL_PREFIX    = ${PYTHON_INSTALL_PREFIX}" )     # e.g., $HOME/.local
+    endif()
+endif()
 
 #----------------------------------------
 # Debug Verbose: print all cmake variables
