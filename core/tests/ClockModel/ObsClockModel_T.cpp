28b3e6fa2dd00111a23463102af729863faf3c30
Jason Vestuto <vestuto@arlut.utexas.edu>
2015-04-14 11:30:35 -0500

    Tests, added basic structure for test class ObsClockModel_T
    - Previous version of this template contained only the test class name.
    - Added declarations for test methods for each ObcClockModel method
    - Added definitions for test methods for each ObcClockModel method with all initialization needed for test framework
    - Definitions have function signature, return handling, and TestUtil initialization, but no test algorithm implemetation.
    - Added full implementation for main function

--- core/tests/ClockModel/ObsClockModel_T.cpp
+++ core/tests/ClockModel/ObsClockModel_T.cpp
@@ -1,15 +1,121 @@
-#include "TestUtil.hpp"
-#include <iostream>
 
+
+#include "TestUtil.hpp"
+#include "ObsClockModel.hpp"
+
+using namespace std;
+using namespace gpstk;
+
+//------------------------------------------------------------
+// Declarations for test class
+//------------------------------------------------------------
 class ObsClockModel_T
 {
-        public: 
-		ObsClockModel_T(){}// Default Constructor, set the precision value
-		~ObsClockModel_T() {} // Default Desructor
+    public: 
+        ObsClockModel_T(){}
+        ~ObsClockModel_T(){}
+         int test_constructor();
+         int test_getSvStatus();
+         int test_setSvStatus();
+         int test_setSvMap();
+         int test_getSvMode();
+         int test_simpleOrdClock();
 };
+//------------------------------------------------------------
+// Definitions for test class
+//------------------------------------------------------------
 
-
-int main() //Main function to initialize and run all tests above
+int ObsClockModel_T :: test_constructor( void )
 {
-	return 0; //Return the total number of errors
+    TestUtil test_util( "ObsClockModel", "constructor", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel objects are created with default constructor";
+    std::string test_fail = "class constructor failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+int ObsClockModel_T :: test_getSvStatus( void )
+{
+    TestUtil test_util( "ObsClockModel", "getSvStatus", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel method getSvStatus is tested here";
+    std::string test_fail = "test failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+int ObsClockModel_T :: test_setSvStatus( void )
+{
+    TestUtil test_util( "ObsClockModel", "setSvStatus", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel method setSvStatus is tested here";
+    std::string test_fail = "test failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+int ObsClockModel_T :: test_setSvMap( void )
+{
+    TestUtil test_util( "ObsClockModel", "setSvMap", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel method setSvMap is tested here";
+    std::string test_fail = "test failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+int ObsClockModel_T :: test_getSvMode( void )
+{
+    TestUtil test_util( "ObsClockModel", "getSvMode", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel method getSvMode is tested here";
+    std::string test_fail = "test failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+int ObsClockModel_T :: test_simpleOrdClock( void )
+{
+    TestUtil test_util( "ObsClockModel", "simpleOrdClock", __FILE__, __LINE__ );
+    std::string test_desc = "ObsClockModel method simpleOrdClock is tested here";
+    std::string test_fail = "test failed";
+
+    // do stuff
+
+    return( test_util.countFails() );
+}
+
+
+
+
+//------------------------------------------------------------
+// main() for test application to be executed by CMake/CTest
+//------------------------------------------------------------
+int main( )
+{
+   int  errorTotal = 0;
+
+   ObsClockModel_T  testClass;
+
+   errorTotal += testClass.test_constructor();
+   errorTotal += testClass.test_getSvStatus();
+   errorTotal += testClass.test_setSvStatus();
+   errorTotal += testClass.test_setSvMap();
+   errorTotal += testClass.test_getSvMode();
+   errorTotal += testClass.test_simpleOrdClock();
+
+   cout << "Total Failures for " << __FILE__ << ": " << errorTotal << endl;
+
+   return( errorTotal );
 }
