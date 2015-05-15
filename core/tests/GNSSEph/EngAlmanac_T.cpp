69ce4c6f68a02256859b7f758e9be0767f85170f
Joseph Voss <voss@arlut.utexas.edu>
2015-05-15 10:25:04 -0500

    Refactored the get method tests

--- core/tests/GNSSEph/EngAlmanac_T.cpp
+++ core/tests/GNSSEph/EngAlmanac_T.cpp
@@ -46,7 +46,7 @@ class EngAlmanac_T
     //Reads in AlmanacData object to form comparison data types
 	EngAlmanac_T(AlmanacData iAData, AlmanacSubframes iASubframes)
 	{ 
-		eps = 1E-6; //Low precision, data is extracted from binary subframes, some have 16 bit precision
+		eps = 1E-6; //Low precision, data is extracted from binary subframes, some have only 16 bit precision
 		aData = iAData;
 		aSubframes = iASubframes;
 
@@ -54,48 +54,12 @@ class EngAlmanac_T
 	
 	~EngAlmanac_T() {} // Default Desructor
 
+	gpstk::EngAlmanac dataStore;
 
 	int addSubframeTest(void)
 	{
 		TestUtil testFramework("EngAlmanac", "addSubframe", __FILE__, __LINE__);
 
-		gpstk::EngAlmanac dataStore;
-		//No parity on these subframes, EngAlmanac doesn't check for a parity
-
-//Wrote a python script that reads in almanac data and converts to binary subframe strings
-//Started porting to c++, will finish if time allows.
-		const long subframe50[10] = {0x22c000e4, 0x00000598, 0x10488780, 0x13832280, 0x3f520000, 0x28433640, 0x303131c0, 0x03d0a340, 0x21541e00, 0x3fc00200};
-		const long subframe51[10] = {0x22c000e4, 0x00000598, 0x109e0140, 0x13bfa740, 0x3f4e0000, 0x28437f00, 0x2fd06180, 0x28d89900, 0x25d33fc0, 0x12800800};
-		const long subframe52[10] = {0x22c000e4, 0x00000598, 0x10c1de40, 0x1382c300, 0x3f52c000, 0x28434500, 0x3acb52c0, 0x24b44bc0, 0x355610c0, 0x0a401f00};
-		const long subframe53[10] = {0x22c000e4, 0x00000598, 0x1116f780, 0x13bf7fc0, 0x3f4cc000, 0x28434380, 0x2ff84900, 0x0ac3bbc0, 0x1ebc60c0, 0x3f800200};
-		const long subframe54[10] = {0x22c000e4, 0x00000598, 0x11487380, 0x1380ae00, 0x3f4fc000, 0x2842f9c0, 0x3ab9bb80, 0x03d9f840, 0x3edf6c80, 0x37c00f00};
-		const long subframe55[10] = {0x22c000e4, 0x00000598, 0x11805fc0, 0x13831e00, 0x3f52c000, 0x28435e40, 0x301afdc0, 0x23307140, 0x324c86c0, 0x05001900};
-		const long subframe56[10] = {0x22c000e4, 0x00000598, 0x11d0f140, 0x138484c0, 0x3f4ec000, 0x28435740, 0x10617ec0, 0x23ec1440, 0x1ad457c0, 0x0f000b00};
-		const long subframe57[10] = {0x22c000e4, 0x00000598, 0x1240e540, 0x13828700, 0x3f570000, 0x28431fc0, 0x056a95c0, 0x1a38c600, 0x3022d0c0, 0x3b3ff100};
-		const long subframe58[10] = {0x22c000e4, 0x00000598, 0x129e0d00, 0x13bfd540, 0x3f4f8000, 0x28433dc0, 0x3ae16300, 0x09760e80, 0x02756400, 0x3a400200};
-		const long subframe59[10] = {0x22c000e4, 0x00000598, 0x12e02d80, 0x13b7e300, 0x3f454000, 0x28435300, 0x2ca23580, 0x0e440800, 0x1b728840, 0x2d3ffc00};
-		const long subframe5a[10] = {0x22c000e4, 0x00000598, 0x130b6800, 0x1387b5c0, 0x3f550000, 0x28434b80, 0x1b4456c0, 0x05ed6740, 0x15678180, 0x09800900};
-		const long subframe5b[10] = {0x22c000e4, 0x00000598, 0x134aa300, 0x13853e40, 0x3f5a0000, 0x284349c0, 0x0695d780, 0x153042c0, 0x22856740, 0x3c7ffa00};
-		const long subframe5c[10] = {0x22c000e4, 0x00000598, 0x13907d80, 0x13840600, 0x3f584000, 0x28431940, 0x063c0240, 0x2bdf5600, 0x35c4eb40, 0x023ffd00};
-		const long subframe5d[10] = {0x22c000e4, 0x00000598, 0x13ce9c40, 0x13be9980, 0x3f524000, 0x28432b80, 0x04fa3f00, 0x03fa5140, 0x2f366e40, 0x37fffc00};
-		const long subframe5e[10] = {0x22c000e4, 0x00000598, 0x1410c640, 0x1387cd40, 0x3f554000, 0x284302c0, 0x1b750500, 0x02f72d40, 0x3fe93bc0, 0x3b800e00};
-		const long subframe5f[10] = {0x22c000e4, 0x00000598, 0x1454b280, 0x1384e0c0, 0x3f5a4000, 0x28436f80, 0x25e93fc0, 0x2b858e00, 0x369e6800, 0x3a400600};
-		const long subframe510[10] = {0x22c000e4, 0x00000598, 0x14a11280, 0x13bd2fc0, 0x3f4b4000, 0x284311c0, 0x3a98a5c0, 0x2bf76240, 0x05c43640, 0x0d000b00};
-		const long subframe511[10] = {0x22c000e4, 0x00000598, 0x14d72e00, 0x13846540, 0x3f5a8000, 0x28435d40, 0x266abc00, 0x057cb3c0, 0x2ea2a900, 0x2f400200};
-		const long subframe512[10] = {0x22c000e4, 0x00000598, 0x150b97c0, 0x13bd5f40, 0x3f4b4000, 0x28477840, 0x3a0fdd40, 0x0c65af40, 0x37788bc0, 0x0a400d00};
-		const long subframe513[10] = {0x22c000e4, 0x00000598, 0x156e3080, 0x13be8580, 0x3f4cc000, 0x28430300, 0x2ff6e3c0, 0x2c837f80, 0x110142c0, 0x317fff00};
-		const long subframe514[10] = {0x22c000e4, 0x00000598, 0x15902180, 0x13bcd480, 0x3f4b0000, 0x28433f40, 0x3a9bcb40, 0x2b1769c0, 0x015ca080, 0x0b400c00};
-		const long subframe515[10] = {0x22c000e4, 0x00000598, 0x15d4ad80, 0x138110c0, 0x3f55c000, 0x28432840, 0x05818a00, 0x249cb240, 0x2a83af80, 0x3cbff800};
-		const long subframe516[10] = {0x22c000e4, 0x00000598, 0x1606a140, 0x1381c8c0, 0x3f4b0000, 0x28433e80, 0x0ff7c740, 0x03ae2340, 0x22dbf140, 0x3e400600};
-//start as 4 here, prn 8 is kabut	
-		const long subframe42[10] = {0x22c000e4, 0x0000042c, 0x16496500, 0x1385d540, 0x3f520000, 0x28434780, 0x1acb6b00, 0x0740b7c0, 0x0e851e80, 0x3f3ffc00};
-		const long subframe43[10] = {0x22c000e4, 0x0000042c, 0x16809280, 0x1382eb40, 0x3f4e0000, 0x28430780, 0x1ac5a340, 0x0bf5a480, 0x3bef1600, 0x00c00d00};
-		const long subframe44[10] = {0x22c000e4, 0x0000042c, 0x16c4e700, 0x13840c80, 0x3f598000, 0x28434c40, 0x256d1000, 0x01e84c40, 0x37842980, 0x02400900};
-		const long subframe45[10] = {0x22c000e4, 0x0000042c, 0x1727fcc0, 0x13879340, 0x3f558000, 0x28434e00, 0x1b830500, 0x2f0b2980, 0x3f1ed780, 0x0e400b00};
-		const long subframe47[10] = {0x22c000e4, 0x0000042c, 0x1742b180, 0x13850380, 0x3f5a4000, 0x28437440, 0x26019d00, 0x39657600, 0x11fdfa80, 0x13c00e00};
-		const long subframe48[10] = {0x22c000e4, 0x0000042c, 0x1782b580, 0x13823140, 0x3f4b4000, 0x28436240, 0x10e2f4c0, 0x1d069c80, 0x1c886f00, 0x3dc00e00};
-		const long subframe49[10] = {0x22c000e4, 0x0000042c, 0x17d0a280, 0x13855080, 0x3f4fc000, 0x28437440, 0x10757bc0, 0x3abe3d00, 0x19242480, 0x0a400400};
-		const long subframe4a[10] = {0x22c000e4, 0x0000042c, 0x18171000, 0x1380c400, 0x3f510000, 0x28436f40, 0x3ba16dc0, 0x00701380, 0x1fafa080, 0x3ac01a00};
 
 		//Subframe and week number mod 1024
 		testMesg = "Adding subframe 5 page 1 failed";
@@ -286,13 +250,21 @@ class EngAlmanac_T
 */
 		const long subframe456[10] = {0x22c000e4, 0x0000042c, 0x2e37ab40, 0x2fbbf780, 0x2b703780,
 									  0x2eb76ac0, 0x32ac2c00, 0x2d5b9680, 0x037fa800, 0x267fff00};
-		std::cout<<"adding page18"<<std::endl;
 		testMesg = "Adding subframe 4 page 18 failed";
 		testFramework.assert(dataStore.addSubframe(subframe456, 819), testMesg, __LINE__);
 
-		int x;
+
+		return testFramework.countFails();
+	} 
+
+	int getTest(void)
+	{
+		int x, failCount;
+		TestUtil testFramework("EngAlmanac", "getTest", __FILE__, __LINE__);
 
 		//GPSTK documentation should really say what units these return as
+		// For loop which tests all the data values for the almanac. Commented out, too many tests
+/*
 		for(int i=0; i<31; i++)
 		{
 			if ( i >= 7) x = i + 2; //switch to allow for skipped prn 8
@@ -324,7 +296,163 @@ class EngAlmanac_T
 			testMesg = "getAf1 returned the wrong value";
 			testFramework.assert(std::abs(dataStore.getAf1(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.af1[i]) < eps, testMesg, __LINE__);
 		}
+*/
 
+		//getEcc test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getEcc(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.ecc[i])*1E2 < eps))
+				failCount++;
+		}
+		testMesg = "getEcc returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getIOffset test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getIOffset(gpstk::SatID(x, gpstk::SatID::systemGPS)) - (aData.oi[i] - .3*M_PI)) < eps))
+				failCount++;
+		}
+		testMesg = "getIOffset returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getOmegadot test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getOmegadot(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.rora[i])*1E8 < eps))
+				failCount++;
+		}
+		testMesg = "getOmegadot returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//get6bitHealth test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(dataStore.get6bitHealth(gpstk::SatID(x, gpstk::SatID::systemGPS)) == 0))
+				failCount++;
+		}
+		testMesg = "get6bitHealth returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);	
+
+		//getSVHealth test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(dataStore.getSVHealth(gpstk::SatID(x, gpstk::SatID::systemGPS)) == 0))
+				failCount++;
+		}
+		testMesg = "getSVHealth returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);	
+
+		//getSVConfig test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(dataStore.getSVConfig(gpstk::SatID(x, gpstk::SatID::systemGPS)) == 9))
+				failCount++;
+		}
+		testMesg = "getSVConfig returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);	
+
+		//getAhalf test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getAhalf(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.sqrta[i])*1E-4 < eps))
+				failCount++;
+		}
+		testMesg = "getAhalf returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);	
+
+		//getA test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getA(gpstk::SatID(x, gpstk::SatID::systemGPS)) - pow(aData.sqrta[i],2))*1E-8 < eps))
+				failCount++;
+		}
+		testMesg = "getA returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getOmega0 test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getOmega0(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.raaw[i])*1E-1 < eps))
+				failCount++;
+		}
+		testMesg = "getOmega0 returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getW test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getW(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.aop[i]) < eps))
+				failCount++;
+		}
+		testMesg = "getW returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getM0 test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getM0(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.ma[i])*1E-1 < eps))
+				failCount++;
+		}
+		testMesg = "getM0 returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getAf0 test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getAf0(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.af0[i])*1E5 < eps))
+				failCount++;
+		}
+		testMesg = "getAf0 returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);
+
+		//getAf1 test
+		failCount = 0;
+		for (int i=0; i<31; i++)
+		{
+			if ( i >= 7) x = i + 2;
+			else x = i + 1;
+			if (!(std::abs(dataStore.getAf1(gpstk::SatID(x, gpstk::SatID::systemGPS)) - aData.af1[i]) < eps))
+				failCount++;
+		}
+		testMesg = "getAf1 returned the wrong value";
+		testFramework.assert(failCount == 0, testMesg, __LINE__);			
 
 		dataStore.check(std::cout);
 
@@ -365,23 +493,22 @@ class EngAlmanac_T
 		testFramework.assert(std::abs(a0 + 0.8329656822606921) < eps, testMesg, __LINE__);
 		testMesg = "getUTC returned an incorrect value for A1";
 		testFramework.assert(std::abs(a1 + 4.0241348031599955e-09) < eps, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for Tot";
+		testMesg = "getUTC returned an incorrect value for deltaTLS";
 		testFramework.assert(std::abs(deltaTLS - 13) < eps, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for W";
+		testMesg = "getUTC returned an incorrect value for deltaTLSF";
 		testFramework.assert(std::abs(deltaTLSF + 103) < eps, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for WNLSF";
+		testMesg = "getUTC returned an incorrect value for Tot";
 		testFramework.assert( tot == 450560, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for DN";
+		testMesg = "getUTC returned an incorrect value for WNt";
 		testFramework.assert( WNt == 90, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for A0";
+		testMesg = "getUTC returned an incorrect value for WNLSF";
 		testFramework.assert( WNLSF == 254, testMesg, __LINE__);
-		testMesg = "getUTC returned an incorrect value for A0";
+		testMesg = "getUTC returned an incorrect value for DN";
 		testFramework.assert( DN == 5, testMesg, __LINE__);
 
 		return testFramework.countFails();
-	} 
 
-	int getTest(void);
+	}
 
 	int isDataTest(void);
 
@@ -400,14 +527,17 @@ class EngAlmanac_T
 
 int main() //Main function to initialize and run all tests above
 {
-	std::ifstream iAlmanac("./data/test_input_gps_almanac.txt");
-	AlmanacData iAData(iAlmanac);
-	AlmanacSubframes iASubframes(iAData);
+	std::ifstream iAlmanac("./data/test_input_gps_almanac.txt"); // Reads in almanac data from file
+	AlmanacData iAData(iAlmanac); // Parses file into data objects
+	AlmanacSubframes iASubframes(iAData); // Takes data objects and generates the subframes needed
 
 	EngAlmanac_T testClass(iAData, iASubframes);
 	int check, errorCounter = 0;
 
-	check = testClass.addSubframeTest();
+	check = testClass.addSubframeTest(); // Tests adding the subframes to a class member of EngAlmanac
+	errorCounter += check;
+
+	check = testClass.getTest(); // Tests the get methods and if subframes stored the correct data
 	errorCounter += check;
 
 	std::cout << "Total Failures for " << __FILE__ << ": " << errorCounter << std::endl;
