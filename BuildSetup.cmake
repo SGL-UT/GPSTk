f48a487fc63b095409dd69c870a90cc9f624ecf7
Jon Little <littlej@arlut.utexas.edu>
2015-10-09 12:33:43 -0500

    Now build through packages on Linux

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -1,10 +1,7 @@
 
-if( DEBUG_SWITCH )
-    message( STATUS "DEBUG: Included CMake file BuildSetup.cmake" )
-endif()
-
 #========================================
-# Set Platform-Dependent Options
+# Try to keep all the platform dependent
+# options in this file
 #========================================
 
 #----------------------------------------
@@ -33,7 +30,7 @@ elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
 elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
 
 else()
-    message( ERROR "ERROR: CMAKE_SYSTEM_NAME = ${CMAKE_SYSTEM_NAME}, not supported. Currently supported: Linux, Darwin, SunOS, " )
+    message( ERROR "CMAKE_SYSTEM_NAME = ${CMAKE_SYSTEM_NAME}, not supported. Currently supported: Linux, Darwin, SunOS, Windows" )
 endif()
 
 #----------------------------------------
@@ -77,95 +74,80 @@ if( "${isSystemDir}" STREQUAL "-1" )
 endif( "${isSystemDir}" STREQUAL "-1" )
 
 
-#----------------------------------------
-# Debug Messages
-#
-# The CMake command-line tool, message( STATUS ) displays 
-# messages on stdout and all other message types on stderr. 
-#----------------------------------------
-
-if( DEBUG_SWITCH )
-    message( STATUS "DEBUG: CMAKE_SYSTEM              = ${CMAKE_SYSTEM}" )      # e.g., Linux-3.2.0
-    message( STATUS "DEBUG: CMAKE_SYSTEM_NAME         = ${CMAKE_SYSTEM_NAME}" ) # e.g., Linux
-    message( STATUS "DEBUG: UNIX                      = ${UNIX}" ) # e.g., is the platform UNIX ?
-    message( STATUS "DEBUG: APPLE                     = ${APPLE}" ) # e.g., is the platform OSX?
-    message( STATUS "DEBUG: WIN32                     = ${WIN32}" ) # e.g., is the platform Windows?
-    message( STATUS "DEBUG: MSVC11                    = ${MSVC11}" ) # e.g., is the platform VisualStudio?
-    message( STATUS "DEBUG: MSVC12                    = ${MSVC12}" ) # e.g., is the platform VisualStudio?
-    message( STATUS "DEBUG: CMAKE_COMMAND             = ${CMAKE_COMMAND}" )     # e.g., /usr/bin/cmake
-    message( STATUS "DEBUG: CMAKE_VERSION             = ${CMAKE_VERSION}" )     # e.g., 2.8.9
-    message( STATUS "DEBUG: CMAKE_BUILD_TOOL          = ${CMAKE_BUILD_TOOL}" )  # e.g., /usr/bin/make
-    message( STATUS "DEBUG: STADYN                    = ${STADYN}" )
-    message( STATUS "DEBUG: CMAKE_COMPILER_IS_GNUCC   = ${CMAKE_COMPILER_IS_GNUCC}" )
-    message( STATUS "DEBUG: CMAKE_CXX_FLAGS           = ${CMAKE_CXX_FLAGS}" )
-    message( STATUS "DEBUG: CMAKE_SHARED_LINKER_FLAGS = ${CMAKE_SHARED_LINKER_FLAGS}" )
-    message( STATUS "DEBUG: CMAKE_SKIP_BUILD_RPATH    = ${CMAKE_SKIP_BUILD_RPATH}" )
-    message( STATUS "DEBUG: CMAKE_INSTALL_RPATH       = ${CMAKE_INSTALL_RPATH}" )
-    message( STATUS "DEBUG: CMAKE_BUILD_WITH_INSTALL_RPATH = ${CMAKE_BUILD_WITH_INSTALL_RPATH}" )
-    message( STATUS "DEBUG: CMAKE_INSTALL_RPATH_USE_LINK_PATH = ${CMAKE_INSTALL_RPATH_USE_LINK_PATH}" )
-    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_DIR   = ${CMAKE_CURRENT_LIST_DIR}" )    # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_LIST_FILE  = ${CMAKE_CURRENT_LIST_FILE}" )   # e.g., $HOME/git/gpstk/CMakeLists.txt
-    message( STATUS "DEBUG: CMAKE_SOURCE_DIR         = ${CMAKE_SOURCE_DIR}" )          # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_SOURCE_DIR = ${CMAKE_CURRENT_SOURCE_DIR}" )  # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_CURRENT_BINARY_DIR = ${CMAKE_CURRENT_BINARY_DIR}" )  # e.g., $HOME/git/gpstk/build
-    message( STATUS "DEBUG: PROJECT_NAME             = ${PROJECT_NAME}" )              # e.g., gpstk
-    message( STATUS "DEBUG: PROJECT_BINARY_DIR       = ${PROJECT_BINARY_DIR}" )        # e.g., $HOME/git/gpstk/build
-    message( STATUS "DEBUG: PROJECT_SOURCE_DIR       = ${PROJECT_SOURCE_DIR}" )        # e.g., $HOME/git/gpstk
-    message( STATUS "DEBUG: CMAKE_INSTALL_DIR        = ${CMAKE_INSTALL_DIR}" )         # e.g., /usr
-    message( STATUS "DEBUG: CMAKE_INSTALL_PREFIX     = ${CMAKE_INSTALL_PREFIX}" )      # e.g., $HOME/.local/gpstk
+if( DEBUG_SWITCH AND NOT DEBUG_VERBOSE )
+    message( STATUS "CMAKE_SYSTEM              = ${CMAKE_SYSTEM}" )      # e.g., Linux-3.2.0
+    message( STATUS "CMAKE_SYSTEM_NAME         = ${CMAKE_SYSTEM_NAME}" ) # e.g., Linux
+    message( STATUS "UNIX                      = ${UNIX}" ) # e.g., is the platform UNIX ?
+    message( STATUS "APPLE                     = ${APPLE}" ) # e.g., is the platform OSX?
+    message( STATUS "WIN32                     = ${WIN32}" ) # e.g., is the platform Windows?
+    message( STATUS "MSVC11                    = ${MSVC11}" ) # e.g., is the platform VisualStudio?
+    message( STATUS "MSVC12                    = ${MSVC12}" ) # e.g., is the platform VisualStudio?
+    message( STATUS "CMAKE_COMMAND             = ${CMAKE_COMMAND}" )     # e.g., /usr/bin/cmake
+    message( STATUS "CMAKE_VERSION             = ${CMAKE_VERSION}" )     # e.g., 2.8.9
+    message( STATUS "CMAKE_BUILD_TOOL          = ${CMAKE_BUILD_TOOL}" )  # e.g., /usr/bin/make
+    message( STATUS "STADYN                    = ${STADYN}" )
+    message( STATUS "CMAKE_COMPILER_IS_GNUCC   = ${CMAKE_COMPILER_IS_GNUCC}" )
+    message( STATUS "CMAKE_CXX_FLAGS           = ${CMAKE_CXX_FLAGS}" )
+    message( STATUS "CMAKE_SHARED_LINKER_FLAGS = ${CMAKE_SHARED_LINKER_FLAGS}" )
+    message( STATUS "CMAKE_SKIP_BUILD_RPATH    = ${CMAKE_SKIP_BUILD_RPATH}" )
+    message( STATUS "CMAKE_INSTALL_RPATH       = ${CMAKE_INSTALL_RPATH}" )
+    message( STATUS "CMAKE_BUILD_WITH_INSTALL_RPATH = ${CMAKE_BUILD_WITH_INSTALL_RPATH}" )
+    message( STATUS "CMAKE_INSTALL_RPATH_USE_LINK_PATH = ${CMAKE_INSTALL_RPATH_USE_LINK_PATH}" )
+    message( STATUS "CMAKE_CURRENT_LIST_DIR   = ${CMAKE_CURRENT_LIST_DIR}" )    # e.g., $HOME/git/gpstk
+    message( STATUS "CMAKE_CURRENT_LIST_FILE  = ${CMAKE_CURRENT_LIST_FILE}" )   # e.g., $HOME/git/gpstk/CMakeLists.txt
+    message( STATUS "CMAKE_SOURCE_DIR         = ${CMAKE_SOURCE_DIR}" )          # e.g., $HOME/git/gpstk
+    message( STATUS "CMAKE_CURRENT_SOURCE_DIR = ${CMAKE_CURRENT_SOURCE_DIR}" )  # e.g., $HOME/git/gpstk
+    message( STATUS "CMAKE_CURRENT_BINARY_DIR = ${CMAKE_CURRENT_BINARY_DIR}" )  # e.g., $HOME/git/gpstk/build
+    message( STATUS "PROJECT_NAME             = ${PROJECT_NAME}" )              # e.g., gpstk
+    message( STATUS "PROJECT_BINARY_DIR       = ${PROJECT_BINARY_DIR}" )        # e.g., $HOME/git/gpstk/build
+    message( STATUS "PROJECT_SOURCE_DIR       = ${PROJECT_SOURCE_DIR}" )        # e.g., $HOME/git/gpstk
+    message( STATUS "CMAKE_INSTALL_DIR        = ${CMAKE_INSTALL_DIR}" )         # e.g., /usr
+    message( STATUS "CMAKE_INSTALL_PREFIX     = ${CMAKE_INSTALL_PREFIX}" )      # e.g., $HOME/.local/gpstk
 endif()
 
 #----------------------------------------
 # Debug Verbose: print all cmake variables
 #----------------------------------------
-
 if( DEBUG_VERBOSE )
     get_cmake_property( _variableNames VARIABLES )
     foreach( _variableName ${_variableNames} )
-        message( STATUS "---- DEBUG VERBOSE: ${_variableName} = ${${_variableName}}" )
+        message( STATUS "---- ${_variableName} = ${${_variableName}}" )
     endforeach()
 endif()
 		
 #----------------------------------------
 # Get CMake vars into C++
 #----------------------------------------
-
 configure_file( "${PROJECT_SOURCE_DIR}/build_config.h.in" "${PROJECT_BINARY_DIR}/generated/build_config.h" )
 include_directories( "${PROJECT_BINARY_DIR}/generated/" ) 
 install( FILES "${PROJECT_BINARY_DIR}/generated/build_config.h" DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}" )
 
 
-#-----------------------------------------
-# Build debs, tgz packages for binaries 
-# Usage: "$ make package" 
-#-----------------------------------------
-	
+if( ${CMAKE_SYSTEM_NAME} MATCHES "SunOS" )
+    set( CPACK_GENERATOR "TGZ" )
+elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
+    set( CPACK_GENERATOR "Bundle;TGZ" )
+elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
+    set( CPACK_GENERATOR "DEB;TGZ" )
+elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Windows" )
+    set( CPACK_GENERATOR "ZIP" )
+endif()
 
 set( CPACK_PACKAGE_DESCRIPTION_SUMMARY "GPSTk libraries and applications for GNSS processing.") 
 set( CPACK_PACKAGE_VENDOR "ARL:UT SGL" )
 set( CPACK_PACKAGE_CONTACT "Bryan Parsons" )
 set( CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.txt" )
-set( CPACK_PACKAGE_VERSION_MAJOR "${gpstk_VERSION_MAJOR}" )
-set( CPACK_PACKAGE_VERSION_MINOR "${gpstk_VERSION_MINOR}" )
-set( CPACK_PACKAGE_VERSION_PATCH "${gpstk_VERSION_PATCH}" )
+set( CPACK_PACKAGE_VERSION_MAJOR "${GPSTK_VERSION_MAJOR}" )
+set( CPACK_PACKAGE_VERSION_MINOR "${GPSTK_VERSION_MINOR}" )
+set( CPACK_PACKAGE_VERSION_PATCH "${GPSTK_VERSION_PATCH}" )
 set( CPACK_INCLUDE_TOPLEVEL_DIRECTORY "OFF" )
-
+set( CPACK_PACKAGE_INSTALL_DIRECTORY "gpstk")
+set( CPACK_TOPLEVEL_TAG "gpstk" ) 
 set( CPACK_DEBIAN_PACKAGE_DEPENDS "libc6 (>= 2.13)" )
 set( CPACK_DEBIAN_SECTION "stable" )
 set( CPACK_DEBIAN_PACKAGE_SECTION "science" )
 
-set( CPACK_GENERATOR "DEB;TGZ" )
-
-#-----------------------------------------
-# Build tgz packages for source release
-# Usage: "$ make package_source" 
-#-----------------------------------------
-
-set( CPACK_SOURCE_IGNORE_FILES "/build/")
+set( CPACK_SOURCE_IGNORE_FILES "${PROJECT_BINARY_DIR}" "build-*/" "/.git" )
 set( CPACK_SOURCE_GENERATOR "TGZ")
 
 include( CPack )
-
-#----------------------------------------
-# The End
-#----------------------------------------
