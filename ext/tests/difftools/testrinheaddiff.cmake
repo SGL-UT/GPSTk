b38503f75391d9710d81fba8a2cce4a2828005cf
johnk <johnk@arlut.utexas.edu>
2015-11-06 14:34:23 -0600

    Add RINEX 3 obs tests for rinheaddiff

---
+++ ext/tests/difftools/testrinheaddiff.cmake
@@ -0,0 +1,16 @@
+# tests for rinheaddiff
+
+execute_process(COMMAND ${TEST_PROG} ${IXOPTS} ${SOURCEDIR}/${FILE1} ${SOURCEDIR}/${FILE2}
+                OUTPUT_FILE ${TARGETDIR}/${TESTBASE}.out
+                RESULT_VARIABLE HAD_ERROR)
+# files are expected to be different
+if(!HAD_ERROR)
+    message(FATAL_ERROR "Test failed")
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E compare_files
+    ${SOURCEDIR}/${TESTBASE}.exp ${TARGETDIR}/${TESTBASE}.out
+    RESULT_VARIABLE DIFFERENT)
+if(DIFFERENT)
+    message(FATAL_ERROR "Test failed - files differ")
+endif()
--- ext/tests/testhelp.cmake
+++ ext/tests/testhelp.cmake
@@ -5,12 +5,14 @@
 # help alone
 
 execute_process(COMMAND ${TEST_PROG} -h
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with short help option")
 endif()
 
 execute_process(COMMAND ${TEST_PROG} --help
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with long help option")
@@ -19,12 +21,14 @@ endif()
 # help and debug
 
 execute_process(COMMAND ${TEST_PROG} -h -d
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with short debug option")
 endif()
 
 execute_process(COMMAND ${TEST_PROG} -h -ddd
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with multiple short debug options")
@@ -33,12 +37,14 @@ endif()
 # help and long debug
 
 execute_process(COMMAND ${TEST_PROG} -h --debug
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with long debug option")
 endif()
 
 execute_process(COMMAND ${TEST_PROG} -h --debug --debug --debug
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with multiple long debug options")
@@ -47,12 +53,14 @@ endif()
 # help and verbose
 
 execute_process(COMMAND ${TEST_PROG} -h -v
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with short verbose option")
 endif()
 
 execute_process(COMMAND ${TEST_PROG} -h -vvv
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with multiple short verbose options")
@@ -61,12 +69,14 @@ endif()
 # help and long verbose
 
 execute_process(COMMAND ${TEST_PROG} -h --verbose
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with long verbose option")
 endif()
 
 execute_process(COMMAND ${TEST_PROG} -h --verbose --verbose --verbose
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(HAD_ERROR)
     message(FATAL_ERROR "Test failed with multiple long verbose options")
@@ -75,6 +85,7 @@ endif()
 # unimplemented option
 
 execute_process(COMMAND ${TEST_PROG} -h --whyUimplementDis
+                OUTPUT_QUIET
                 RESULT_VARIABLE HAD_ERROR)
 if(!HAD_ERROR)
     message(FATAL_ERROR "Test failed with undefined option")
