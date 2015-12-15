73fcce4e614c602b2614efd7fb54f2898b1f854d
johnk <johnk@arlut.utexas.edu>
2015-12-15 14:29:32 -0600

    Reformatting

--- ext/lib/Rxio/NovatelData.hpp
+++ ext/lib/Rxio/NovatelData.hpp
@@ -22,12 +22,12 @@
 
 //============================================================================
 //
-//This software developed by Applied Research Laboratories at the University of
-//Texas at Austin, under contract to an agency or agencies within the U.S. 
-//Department of Defense. The U.S. Government retains all rights to use,
-//duplicate, distribute, disclose, or release this software. 
+// This software developed by Applied Research Laboratories at the University of
+// Texas at Austin, under contract to an agency or agencies within the U.S. 
+// Department of Defense. The U.S. Government retains all rights to use,
+// duplicate, distribute, disclose, or release this software. 
 //
-//Pursuant to DoD Directive 523024 
+// Pursuant to DoD Directive 523024 
 //
 // DISTRIBUTION STATEMENT A: This software has been approved for public 
 //                           release, distribution is unlimited.
@@ -36,7 +36,8 @@
 
 /**
  * @file NovatelData.hpp
- * gpstk::NovatelData - container for Novatel data, with methods to convert to Rinex
+ * gpstk::NovatelData - container for Novatel data, with methods to
+ * convert to Rinex
  */
 
 #ifndef GPSTK_NOVATEL_DATA_HPP
@@ -51,14 +52,15 @@
 
 namespace gpstk
 {
-   /** @addtogroup NovatelGroup */
-   //@{
+      /** @addtogroup NovatelGroup */
+      //@{
 
-   /// Read Novatel binary records and parse into Rinex. Currently supports
-   /// the basic data records of OEM2 and OEM4 files only; others are yet to be
-   /// implemented.
-   /// Ref. (OEM2) 'GPSCard Command Descriptions' Manual (Rev 3)
-   /// Ref. 'OEM4 Family of Receivers,' Users Manual Volume 2 (OM-20000047 Rev 12)
+      /** Read Novatel binary records and parse into Rinex. Currently
+       * supports the basic data records of OEM2 and OEM4 files only;
+       * others are yet to be implemented.
+       * Ref. (OEM2) 'GPSCard Command Descriptions' Manual (Rev 3)
+       * Ref. 'OEM4 Family of Receivers,' Users Manual Volume 2
+       *   (OM-20000047 Rev 12) */
    class NovatelData : public FFData
    {
    public:
@@ -83,13 +85,14 @@ namespace gpstk
 
          /// Default constructor
       NovatelData(void) : rectype(Unknown), datasize(0), headersize(0), gpsWeek(-1)
-         {}
+      {}
 
          /// Destructor
       virtual ~NovatelData(void) {}
 
-         /// Return true if this is a valid Novatel record.
-         /// Test the validity of the record with this before further processing.
+         /** Return true if this is a valid Novatel record.  Test the
+          * validity of the record with this before further
+          * processing. */
       bool isValid(void) const;
 
          /// This class is not header
@@ -98,16 +101,18 @@ namespace gpstk
          /// This class is data
       virtual bool isData(void) const { return true; }
 
-         /// True if this record is an ephemeris record
-         /// Test the identity of the record with this before casting into Rinex.
+         /** True if this record is an ephemeris record.  Test the
+          * identity of the record with this before casting into
+          * Rinex. */
       bool isNav(void) const;
 
-         /// True if this record is observation data
-         /// Test the identity of the record with this before casting into Rinex.
+         /** True if this record is observation data.  Test the
+          * identity of the record with this before casting into
+          * Rinex. */
       bool isObs(void) const;
 
-         /// True if this record is auxiliary data (not Nav, not Obs)
-         /// (only one of isNav(), isObs() and isAux() is true
+         /** True if this record is auxiliary data (not Nav, not Obs).
+          * (only one of isNav(), isObs() and isAux() is true */
       bool isAux(void) const;
 
          /// True if this record belongs to OEM2 receivers
@@ -123,12 +128,14 @@ namespace gpstk
          /// OEM2 nav records that are processed before any obs records
       void setWeek(long& gpsweek) { gpsWeek = gpsweek; }
 
-         /// cast *this into an gpstk::RinexNavData.
-         /// @throw if the record is invalid or not an ephemeris (isNav()==false)
+         /** Cast *this into an gpstk::RinexNavData.
+          * @throw if the record is invalid or not an ephemeris (isNav()==false)
+          */
       operator RinexNavData() throw(gpstk::Exception);
 
-         /// cast *this into a gpstk::RinexObsData
-         /// @throw if the record is invalid or not an observation (isObs()==false)
+         /** Cast *this into a gpstk::RinexObsData.
+          * @throw if the record is invalid or not an observation
+          *   (isObs()==false) */
       operator RinexObsData() throw(gpstk::Exception);
 
          /// public data members
@@ -156,21 +163,21 @@ namespace gpstk
                gpstk::FFStreamError);
 
    private:
-         /// private data members
-         /// header length(28) + data length(max 65536) = 65564
+         // header length(28) + data length(max 65536) = 65564
       unsigned char buffer[65564];   ///< buffer for raw data
 
-         /// Reference GPS week, for OEM2, where the nav records require a GPS week,
-         /// but only the obs records have one, and then it is 10-bit.
-         /// This epoch will be used to remove the ambiguity in the 10-bit week
-         /// number of the obs records, and then will provide a week number for
-         /// for the nav records. If not set by the user, it will be set by the
-         /// system time, and then by the first obs record.
+         /** Reference GPS week, for OEM2, where the nav records
+          * require a GPS week, but only the obs records have one, and
+          * then it is 10-bit.  This epoch will be used to remove the
+          * ambiguity in the 10-bit week number of the obs records,
+          * and then will provide a week number for for the nav
+          * records. If not set by the user, it will be set by the
+          * system time, and then by the first obs record. */
       long gpsWeek;
       
    }; // end class NovatelData
 
-   //@}
+      //@}
 
 }  // end namespace gpstk
 
