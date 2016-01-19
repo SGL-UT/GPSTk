e358f4b15be249822c1d0b9852ffb46b62e0bf1d
johnk <johnk@arlut.utexas.edu>
2016-01-19 12:45:48 -0600

    Correct a comment

--- ext/lib/NavFilter/LNavFilterData.hpp
+++ ext/lib/NavFilter/LNavFilterData.hpp
@@ -11,11 +11,11 @@ namespace gpstk
    {
    public:
       LNavFilterData();
-         /** Must be set this before use in a filter.  The data is
-          * expected to be an array of 10 uint32_t values where each
-          * array index corresponds to a subframe word (sf[0] is word
-          * 1, and so on).  The words are right-aligned meaning that
-          * the LSB of word 1 is in the LSB of sf[0].
+         /** Must be set before use in a filter.  The data is expected
+          * to be an array of 10 uint32_t values where each array
+          * index corresponds to a subframe word (sf[0] is word 1, and
+          * so on).  The words are right-aligned meaning that the LSB
+          * of word 1 is in the LSB of sf[0].
           *
           * This is stored as a pointer rather than array so that data
           * need not be moved in order to perform the filtering. */
