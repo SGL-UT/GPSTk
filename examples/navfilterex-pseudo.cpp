f243c0f4e3bb8cb61e7bcc0a548e17ec67a59d1e
johnk <johnk@arlut.utexas.edu>
2016-01-20 13:11:16 -0600

    Fix up example

--- examples/navfilterex-pseudo.cpp
+++ examples/navfilterex-pseudo.cpp
@@ -4,12 +4,12 @@
 void filterEX()
 {
       // Filter manager, where the work is done
-   NavFilterMgr mgr;
+   gpstk::NavFilterMgr mgr;
       // Individual filters being applied
-   LNavCookFilter filtCook;
-   LNavParityFilter filtParity;
+   gpstk::LNavCookFilter filtCook;
+   gpstk::LNavParityFilter filtParity;
       // Data being passed to the filter
-   LNavFilterData navFiltData;
+   gpstk::LNavFilterData navFiltData;
       // Generic LNAV message storage
    std::vector<uint32_t> subframe(10,0);
 
@@ -21,13 +21,13 @@ void filterEX()
    navFiltData.sf = &subframe[0];
 
       // validate the subframe
-   NavFilter::NavMsgList l = mgr.validate(&navFiltData);
+   gpstk::NavFilter::NavMsgList l = mgr.validate(&navFiltData);
 
       // process the results
-   NavFilter::NavMsgList::const_iterator nmli;
+   gpstk::NavFilter::NavMsgList::const_iterator nmli;
    for (nmli = l.begin(); nmli != l.end(); nmli++)
    {
-      LNavFilterData *fd = dynamic_cast<LNavFilterData*>(*nmli);
+      gpstk::LNavFilterData *fd = dynamic_cast<gpstk::LNavFilterData*>(*nmli);
          // do something with fd (nav message that passed the filters)
    }
 }
