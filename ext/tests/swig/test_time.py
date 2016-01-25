21aa40eab386624d9c4fd131ec0415762f0d2a44
Jon Little <littlej@arlut.utexas.edu>
2016-01-25 10:48:55 -0600

    adding testing of time classes

--- ext/tests/swig/test_time.py
+++ ext/tests/swig/test_time.py
@@ -13,7 +13,8 @@ class TestCommonTime(unittest.TestCase):
     """CommonTime tests"""
     def test_CommonTime(self):
         """CommonTime constructor tests"""
-        t=gpstk.CommonTime(1980, 1, 6, 0, 0, 0)
+        t=gpstk.CommonTime(1980, 1, 6, 0, 0, 0, gpstk.TimeSystem(gpstk.TimeSystem.GPS))
+
 
         systime = gpstk.SystemTime()
         comtime = systime.toCommonTime()
