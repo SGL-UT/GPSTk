f87dd842ae98d03b378bbd38339cf6be8e976c23
Anthony Hughes <anthony@arlut.utexas.edu>
2016-02-15 12:31:11 -0600

    Added a more correct and efficient addSubframe() variant that accepts the subframe as an array of uint32_t, thus avoiding several array copies.

--- core/lib/GNSSEph/EngAlmanac.hpp
+++ core/lib/GNSSEph/EngAlmanac.hpp
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
-//  
+//
 //  Copyright 2004, The University of Texas at Austin
 //
 //============================================================================
@@ -23,13 +23,13 @@
 //============================================================================
 //
 //This software developed by Applied Research Laboratories at the University of
-//Texas at Austin, under contract to an agency or agencies within the U.S. 
+//Texas at Austin, under contract to an agency or agencies within the U.S.
 //Department of Defense. The U.S. Government retains all rights to use,
-//duplicate, distribute, disclose, or release this software. 
+//duplicate, distribute, disclose, or release this software.
 //
-//Pursuant to DoD Directive 523024 
+//Pursuant to DoD Directive 523024
 //
-// DISTRIBUTION STATEMENT A: This software has been approved for public 
+// DISTRIBUTION STATEMENT A: This software has been approved for public
 //                           release, distribution is unlimited.
 //
 //=============================================================================
@@ -88,6 +88,17 @@ namespace gpstk
           * @throw InvalidParameter if subframe is valid but not subframe 4-5.
           */
       bool addSubframe(const long subframe[10], const int gpsWeek)
+         throw(gpstk::InvalidParameter);
+
+         /**
+          * Store a subframe in this object.
+          * @param subframe ten word navigation subframe stored in the
+          * 30 least-significant bits of each array index.
+          * @param gpsWeek full GPS week number.
+          * @return true if successful.
+          * @throw InvalidParameter if subframe is valid but not subframe 4-5.
+          */
+      bool addSubframe(const uint32_t subframe[10], const short gpsWeek)
          throw(gpstk::InvalidParameter);
 
          /** This function returns true if data is available for a given
