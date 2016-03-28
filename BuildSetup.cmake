aa0394c944675ed94376baf072d938b5213bf5d1
Bryan Parsons <bparsons@arlut.utexas.edu>
2016-03-28 13:43:29 -0500

    Adding warning flag back to silence SunOS warnings.

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -17,7 +17,7 @@ endif()
 # Platform-dependent Compiler flags
 #----------------------------------------
 if( ${CMAKE_SYSTEM_NAME} MATCHES "SunOS" )
-    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -erroff=hidevf,badargtype2w" )
+    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -erroff=hidevf,wvarhidemem,badargtype2w" )
     # add -DCMAKE_CXX_FLAGS=-std=c++03 or =-std=c++11 on the CMAKE invocation
     set( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -mt -shared" )
 elseif( ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" )
