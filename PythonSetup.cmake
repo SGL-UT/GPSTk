d5b153f6ec474af96d148550f2ca0211caba80ee
Jon C. Little <littlej@arlut.utexas.edu>
2016-04-01 07:21:33 -0500

    Fixing bug where the wrong python libraries were bein found

--- PythonSetup.cmake
+++ PythonSetup.cmake
@@ -26,6 +26,7 @@
 #------------------------------------------------------------
 # If the user provides a custom Python configuration, use it
 #------------------------------------------------------------
+
 include( CustomPythonSetup.cmake 
          OPTIONAL
          RESULT_VARIABLE PYTHON_CUSTOM_CONFIG )
@@ -34,8 +35,18 @@ include( CustomPythonSetup.cmake
 # If a user-speccified python configuration is not found, let CMake try to find the system python
 #------------------------------------------------------------
 if( ${PYTHON_CUSTOM_CONFIG} MATCHES "NOTFOUND" )
-    find_package( PythonInterp 2.7 )
-    find_package( PythonLibs ${PYTHON_VERSION_STRING} REQUIRED )
+  find_package( PythonInterp )
+  
+  # It looks like the find for PythonLibs gets the 'first' Python.h it can find,
+  # which does not necessiarly match what the executable found by PythonInterp
+  # will be copacetic with. So, we set CMAKE_INCLUDE_PATH to what is retured
+  # by the found python-config
+  execute_process( COMMAND "${PYTHON_EXECUTABLE}-config" "--includes" OUTPUT_VARIABLE PYTHON_INCLUDES)
+  string(REGEX MATCH "^-I(.*) " _python_include ${PYTHON_INCLUDES})
+  string(STRIP ${_python_include} _python_include)
+  string(SUBSTRING ${_python_include} 2 -1 _python_include) # strip the "-I"
+  set(CMAKE_INCLUDE_PATH ${_python_include})
+  find_package( PythonLibs ${PYTHON_VERSION_STRING} REQUIRED )
 endif()
 
 
@@ -43,23 +54,24 @@ endif()
 # Debug messaging
 #------------------------------------------------------------
 if( DEBUG_SWITCH OR NOT PYTHONLIBS_FOUND)
-    message( STATUS "PYTHONINTERP_FOUND        = ${PYTHONINTERP_FOUND}" )
-    message( STATUS "PYTHON_EXECUTABLE         = ${PYTHON_EXECUTABLE}" )
-    message( STATUS "PYTHON_VERSION_STRING     = ${PYTHON_VERSION_STRING}" )
-    message( STATUS "PYTHONLIBS_FOUND          = ${PYTHONLIBS_FOUND}" )
-    message( STATUS "PYTHON_LIBRARIES          = ${PYTHON_LIBRARIES}" )
-    message( STATUS "PYTHON_INCLUDE_DIRS       = ${PYTHON_INCLUDE_DIRS}" )
-    message( STATUS "PYTHONLIBS_VERSION_STRING = ${PYTHONLIBS_VERSION_STRING}" )
-    message( STATUS "PYTHON_INSTALL_PREFIX     = ${PYTHON_INSTALL_PREFIX}" ) 
+  message( STATUS "PYTHONINTERP_FOUND        = ${PYTHONINTERP_FOUND}" )
+  message( STATUS "PYTHON_EXECUTABLE         = ${PYTHON_EXECUTABLE}" )
+  message( STATUS "PYTHON_VERSION_STRING     = ${PYTHON_VERSION_STRING}" )
+  message( STATUS "PYTHONLIBS_FOUND          = ${PYTHONLIBS_FOUND}" )
+  message( STATUS "PYTHON_LIBRARIES          = ${PYTHON_LIBRARIES}" )
+  message( STATUS "PYTHON_INCLUDE_DIR        = ${PYTHON_INCLUDE_DIRS}" )
+  message( STATUS "PYTHON_INCLUDE_DIRS       = ${PYTHON_INCLUDE_DIRS}" )
+  message( STATUS "PYTHONLIBS_VERSION_STRING = ${PYTHONLIBS_VERSION_STRING}" )
+  message( STATUS "PYTHON_INSTALL_PREFIX     = ${PYTHON_INSTALL_PREFIX}" ) 
 endif()
 
 #------------------------------------------------------------
 # Consistent python library and headers could not be found
 #------------------------------------------------------------
 if( NOT PYTHONLIBS_FOUND )
-    message( STATUS "Cannot find requested version of PYTHONLIBS on your system." )
-    message( STATUS "Cannot build swig bindings without the right python libraries." )
-    message( STATUS "PYTHON_LIBRARY and PYTHON_INCLUDE_DIR versions must match PYTHON_EXECUTABLE." )
-    message( FATAL_ERROR "Cannot find PYTHONLIBS. Cannot proceed. Exiting now!" )
-    return()
+  message( STATUS "Cannot find requested version of PYTHONLIBS on your system." )
+  message( STATUS "Cannot build swig bindings without the right python libraries." )
+  message( STATUS "PYTHON_LIBRARY and PYTHON_INCLUDE_DIR versions must match PYTHON_EXECUTABLE." )
+  message( FATAL_ERROR "Cannot find PYTHONLIBS. Cannot proceed. Exiting now!" )
+  return()
 endif()
