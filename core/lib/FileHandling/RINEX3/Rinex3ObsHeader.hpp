27391a776f944b987d8795e7630469495dd6010a
Jon C. Little <littlej@arlut.utexas.edu>
2016-09-16 11:42:06 -0500

    Fixing several files that got overwritten in the previous merge of master in to RinEdit_Galileo

--- core/lib/FileHandling/RINEX3/Rinex3ObsHeader.hpp
+++ core/lib/FileHandling/RINEX3/Rinex3ObsHeader.hpp
@@ -386,6 +386,9 @@ namespace gpstk
          /// Map P to Y code observations in RINEX 2 files
       bool PisY;
 
+         /// Used to help debug this class
+      static int debug;
+
          /// Destructor
       virtual ~Rinex3ObsHeader()
       {}
