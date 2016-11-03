0a05675bca72ec709b48a87eeff3541794c58155
Jon C. Little <littlej@arlut.utexas.edu>
2016-11-03 07:33:22 -0500

    Fixing return code to be non-zero when there are uncaught exceptions thrown during tests

--- swig/gpstk/test_utils.py
+++ swig/gpstk/test_utils.py
@@ -31,4 +31,4 @@ def run_unit_tests():
     isuite = unittest.TestLoader().discover(dir, pattern=script)
 
     rc = runner.run(isuite)
-    sys.exit(len(rc.failures))
+    sys.exit(len(rc.failures) + len(rc.errors))
