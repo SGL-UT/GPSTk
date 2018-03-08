810d69e0525b63b8b23829d6afc3c54d5b9178e4
John H. Price <jhprice@arlut.utexas.edu>
2018-03-08 11:25:36 -0600

    Use newer rinex classes to allow RINEX 3 file checking

--- core/apps/checktools/rnwcheck.cpp
+++ core/apps/checktools/rnwcheck.cpp
@@ -36,8 +36,8 @@
 
 #include "CheckFrame.hpp"
 
-#include "RinexNavStream.hpp"
-#include "RinexNavData.hpp"
+#include "Rinex3NavStream.hpp"
+#include "Rinex3NavData.hpp"
 
 using namespace std;
 using namespace gpstk;
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
 {
    try
    {
-      CheckFrame<RinexNavStream, RinexNavData> cf(argv[0],
+      CheckFrame<Rinex3NavStream, Rinex3NavData> cf(argv[0],
                                                   std::string("Rinex Nav"));
       
       if (!cf.initialize(argc, argv))
