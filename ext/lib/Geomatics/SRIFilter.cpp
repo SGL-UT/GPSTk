1758d5a94a54ca4409650b5a9e5f68bb26653411
Brian Tolman <btolman@arlut.utexas.edu>
2019-06-12 10:26:29 -0500

    trivial changes, mostly to doc

--- ext/lib/Geomatics/SRIFilter.cpp
+++ ext/lib/Geomatics/SRIFilter.cpp
@@ -1,3 +1,10 @@
+/// @file SRIFilter.cpp  Implementation of class SRIFilter.
+/// class SRIFilter implements the square root information matrix form of the
+/// Kalman filter.
+///
+/// Reference: "Factorization Methods for Discrete Sequential Estimation,"
+///             G.J. Bierman, Academic Press, 1977.
+
 //============================================================================
 //
 //  This file is part of GPSTk, the GPS Toolkit.
@@ -33,13 +40,6 @@
 //                           release, distribution is unlimited.
 //
 //=============================================================================
-
-/// @file SRIFilter.cpp  Implementation of class SRIFilter.
-/// class SRIFilter implements the square root information matrix form of the
-/// Kalman filter.
-///
-/// Reference: "Factorization Methods for Discrete Sequential Estimation,"
-///             G.J. Bierman, Academic Press, 1977.
 
 //------------------------------------------------------------------------------------
 #include "SRIFilter.hpp"
@@ -148,8 +148,9 @@ void SRIFilter::measurementUpdate(const Matrix<double>& H, Vector<double>& D,
    try {
          // whiten partials and data
       Matrix<double> P(H);
-      Matrix<double> CHL(lowerCholesky(CM));
+      Matrix<double> CHL;
       if(&CM != &SRINullMatrix) {
+         CHL = lowerCholesky(CM);
          Matrix<double> L(inverseLT(CHL));
          P = L * P;
          D = L * D;
@@ -159,9 +160,8 @@ void SRIFilter::measurementUpdate(const Matrix<double>& H, Vector<double>& D,
       SrifMU(R, Z, P, D);
 
          // un-whiten residuals
-      if(&CM != &SRINullMatrix) {         // same if above creates CHL
+      if(&CM != &SRINullMatrix)           // same if above creates CHL
          D = CHL * D;
-      }
    }
    catch(MatrixException& me) { GPSTK_RETHROW(me); }
    catch(VectorException& ve) { GPSTK_RETHROW(ve); }
@@ -333,8 +333,8 @@ void SRIFilter::Reset(const int N)
 //    There are three ways to handle non-zero process noise covariance.
 // (1) If Q is the (known) a priori process noise covariance Q, then
 // set Q=Rw(-1)*Rw(-T), and G=1.
-// (2) Transform process noise covariance matrix to UDU form, Q=UDU,
-// then set G=U  and Rw = (D)**-1/2.
+// (2) Transform process noise covariance matrix to UDU form, Q=UDU^T,
+// then set G=U  and Rw = (D)^-1/2.
 // (3) Take the sqrt of process noise covariance matrix Q, then set
 // G=this sqrt and Rw = 1.  [2 and 3 have been tested.]
 //    The routine applies a Householder transformation to a large
