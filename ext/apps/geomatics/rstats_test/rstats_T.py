69290cdcac4f03ac6c8d7d84856b6f2b5811fe86
Brian Tolman <btolman@arlut.utexas.edu>
2019-06-21 10:07:20 -0500

    make test python2-compatible

--- ext/apps/geomatics/rstats_test/rstats_T.py
+++ ext/apps/geomatics/rstats_test/rstats_T.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # Test rstats
 
@@ -14,12 +14,13 @@ def RunCmd(cmd1,cmd2,cmd3):
     #print("full cmd is {}".format(cmd))
 
     proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    try:
-        outs,errs = proc.communicate(timeout=300)
-    except TimeoutError:
-        printf("Failed - timeout")
-        proc.kill()
-        outs,errs = proc.communicate()
+    #try:                                               # python 3
+    #    outs,errs = proc.communicate(timeout=300)      # python 3
+    #except TimeoutError:                               # python 3
+    #    print("Failed - timeout")                      # python 3
+    #    proc.kill()                                    # python 3
+    #    outs,errs = proc.communicate()                 # python 3
+    outs,errs = proc.communicate()                    # python 2
 
     outstr = outs.decode("utf-8").rstrip("\r\n")
     errstr = errs.decode("utf-8").rstrip("\r\n")
@@ -52,67 +53,96 @@ print("Command exe with data path is "+cmdexe+cmdpath)
 # -------------------------------------------------------------------------------------------
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -bc -br -b2 -bw")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 -bc --beg 66600 --end 69000")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 -br --min 10. --max 40.")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --KS")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -bw")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 --brief")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -brw")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -br")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -bw")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -bc")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --wt 3 -b2")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --outs 0.5")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --plot")
 outstr = outstr.replace(cmdpath,"")   # remove input file path
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --qplot --stdout")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --bin 30")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --sum")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --sum+")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --fit 5,65250,67700 --stdout")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --seq --label SEQS")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 4 -x 1 -y 2 --disc 0.8,0")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --fdf 0.8")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --fdf2 0.8,0.02,2")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --wind 10")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --xwind 10")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --fix 4")
 print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"SDexam01.txt -q -p 5 -x 1 -y 2 --wnj 1.e-4")
+# remove last 2 columns in KMU output (residuals - subject to numerical noise)
 lines = outstr.split("\n")
 for line in lines:
     line = line.rstrip("\r\n")
     if re.match(r'^KMU',line):
         F = line.split()
-        for i in range(0,10):
-            print(F[i],end="")
+        #print("KMU",end="")                         # python 3
+        print("KMU"),                              # python 2
+        for i in range(1,10):
+            #print(" "+F[i],end="")                  # python 3
+            print(F[i]),                           # python 2
         print("")
     else:
         print(line)
-#print(outstr)
+
 outstr=RunCmd(cmdexe,cmdpath,"testfft.data -q -x 1 -y 9 -p 8 --fft 0.0034722")
 print(outstr)
 
